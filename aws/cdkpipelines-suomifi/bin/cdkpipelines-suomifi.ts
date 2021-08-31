5a50b45b4b132b50dbd1fa4d518d4fbbca93a089
kettunenj <juhani.kettunen@cgi.com>
2021-08-31 14:01:04 +0300

    last initial commit to main, changing branch workflow

--- aws/cdkpipelines-suomifi/bin/cdkpipelines-suomifi.ts
+++ aws/cdkpipelines-suomifi/bin/cdkpipelines-suomifi.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkpipelinesSuomifiStack } from '../lib/cdkpipelines-suomifi-stack';
+import { CdkpipelinesSuomifiPipelineStack } from '../lib/cdkpipelines-suomifi-pipeline-stack';
 
 const app = new cdk.App();
-new CdkpipelinesSuomifiStack(app, 'CdkpipelinesSuomifiStack', {
+new CdkpipelinesSuomifiPipelineStack(app, 'CdkpipelinesSuomfiPipelineStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new CdkpipelinesSuomifiStack(app, 'CdkpipelinesSuomifiStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '283563576583', region: 'eu-west-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- aws/cdkpipelines-suomifi/lib/cdkpipelines-suomifi-stack.ts
+++ aws/cdkpipelines-suomifi/lib/cdkpipelines-suomifi-stack.ts
@@ -1,7 +1,11 @@
 import * as apigw from '@aws-cdk/aws-apigateway';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as loadbalance from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ssm from '@aws-cdk/aws-ssm';
 import { CfnOutput, Construct, Stack, StackProps } from '@aws-cdk/core';
 import * as path from 'path';
+import { AlarmBase } from '@aws-cdk/aws-cloudwatch';
 
 /**
  * A stack for our simple Lambda-powered web service
@@ -14,6 +18,39 @@ export class CdkpipelinesSuomifiStack extends Stack {
  
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // 0. Get HASSU VPC
+    const vpcId = ssm.StringParameter.fromStringParameterAttributes(this, 'vpc-ssm-parameter', {
+      parameterName: 'HassuVpcId'
+    }).stringValue
+    
+    const vpc = ec2.Vpc.fromLookup(this, 'Vpc', {
+      vpcId: vpcId
+    });
+
+    // SG
+    const sgId = ssm.StringParameter.fromStringParameterAttributes(this, 'sg-ssm-parameter', {
+      parameterName: 'HassuSgId'
+    }).stringValue;
+
+    const securityGroup = ec2.SecurityGroup.fromLookup(this, 'HassuSG', sgId);
+
+    // 1. ALB
+    const alb = new loadbalance.ApplicationLoadBalancer(this, 'LoadBalancer', {
+      vpc,
+      internetFacing: false,
+      vpcSubnets: {onePerAz: true},
+      http2Enabled: true,
+      securityGroup
+    });
+    
+    // 2. ECS Cluster, Service, Task
+
+    // 3. RDS
+
+    // 4. AppMesh
+
+    // 5. CloudMap
 
     // The Lambda function that contains the functionality
     const handler = new lambda.Function(this, 'Lambda', {
