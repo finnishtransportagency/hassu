1ca43e3c0a1ebed87953a32a8e31b76e9817ea19
kettunenj <juhani.kettunen@cgi.com>
2021-09-02 20:18:20 +0300

    alb-ecs cdk

--- aws/cdkpipelines-suomifi/lib/cdkpipelines-suomifi-stack.ts
+++ aws/cdkpipelines-suomifi/lib/cdkpipelines-suomifi-stack.ts
@@ -3,6 +3,8 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import * as loadbalance from '@aws-cdk/aws-elasticloadbalancingv2';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ssm from '@aws-cdk/aws-ssm';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as logs from '@aws-cdk/aws-logs';
 import { CfnOutput, Construct, Stack, StackProps } from '@aws-cdk/core';
 import * as path from 'path';
 import { AlarmBase } from '@aws-cdk/aws-cloudwatch';
@@ -19,7 +21,7 @@ export class CdkpipelinesSuomifiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // 0. Get HASSU VPC
+    // 1. Get HASSU VPC
     const vpcId = ssm.StringParameter.fromStringParameterAttributes(this, 'vpc-ssm-parameter', {
       parameterName: 'HassuVpcId'
     }).stringValue
@@ -28,14 +30,13 @@ export class CdkpipelinesSuomifiStack extends Stack {
       vpcId: vpcId
     });
 
-    // SG
+    // 2. ALB
     const sgId = ssm.StringParameter.fromStringParameterAttributes(this, 'sg-ssm-parameter', {
       parameterName: 'HassuSgId'
     }).stringValue;
 
     const securityGroup = ec2.SecurityGroup.fromLookup(this, 'HassuSG', sgId);
 
-    // 1. ALB
     const alb = new loadbalance.ApplicationLoadBalancer(this, 'LoadBalancer', {
       vpc,
       internetFacing: false,
@@ -44,7 +45,61 @@ export class CdkpipelinesSuomifiStack extends Stack {
       securityGroup
     });
     
-    // 2. ECS Cluster, Service, Task
+    // 3. ECS Cluster, Service, Task, Container, LogGroup
+    const cluster = new ecs.Cluster(this, 'ECSCluster', {
+      vpc: vpc
+    });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDefinition', {
+      memoryLimitMiB: 512,
+      cpu: 256
+    });
+
+    const keycloakUserParam = ssm.StringParameter.fromSecureStringParameterAttributes(this, 'KeycloakUserParam', {
+      parameterName: 'HassuKeycloakUser',
+      version: 1
+    });
+
+    const keycloakPasswordParam = ssm.StringParameter.fromSecureStringParameterAttributes(this, 'KeycloakPasswordParam', {
+      parameterName: 'HassuKeycloakPassword',
+      version: 1
+    });
+
+    const logGroup = new logs.LogGroup(this, 'LogGroup', {
+      logGroupName: '/ecs/hassu-suomifi',
+      retention: logs.RetentionDays.TWO_WEEKS
+    });
+    
+    const container = taskDefinition.addContainer('KeycloakContainer', {
+      image: ecs.ContainerImage.fromRegistry("jboss/keycloak"),
+      environment: {
+        ENV: 'dev',
+        FOO: 'bar'
+      },
+      secrets: {
+        KEYClOAK_USER: ecs.Secret.fromSsmParameter(keycloakUserParam),
+        KEUCLOAK_PASSWORD: ecs.Secret.fromSsmParameter(keycloakPasswordParam)
+      },
+      portMappings:[{ containerPort: 8080 }],
+      logging: ecs.LogDrivers.awsLogs({
+        streamPrefix: '/ecs/hassu-suomifi/'
+      })
+    });
+
+    const service = new ecs.FargateService(this, 'Service', {
+      cluster,
+      taskDefinition,
+      desiredCount: 2
+    });
+
+    const listener = alb.addListener('Listener', { port: 80});
+    const targetGroup = listener.addTargets('ECS', {
+      port: 80,
+      targets: [service.loadBalancerTarget({
+        containerName: 'KeycloakContainer',
+        containerPort: 8080
+      })]
+    })
 
     // 3. RDS
 
