551569b3a27fe885631535020d383ece8c815f1c
kettunenj <juhani.kettunen@cgi.com>
2021-09-03 19:26:30 +0300

    fix app

--- aws/cdkpipelines-suomifi/bin/cdkpipelines-suomifi.ts
+++ aws/cdkpipelines-suomifi/bin/cdkpipelines-suomifi.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkpipelinesSuomifiStack } from '../lib/cdkpipelines-suomifi-stack';
+import { CdkpipelinesSuomifiPipelineStack } from '../lib/cdkpipelines-suomifi-pipeline-stack';
 
 const app = new cdk.App();
-new CdkpipelinesSuomifiStack(app, 'CdkpipelinesSuomfiStack', {
+new CdkpipelinesSuomifiPipelineStack(app, 'CdkpipelinesSuomifiPipelineStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
