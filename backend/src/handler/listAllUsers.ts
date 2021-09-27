4fc1ad4b1b36712097d122d651f924e3d5d18c70
Mikko Haapamäki <mikko.haapamaki@cgi.com>
2021-09-27 09:56:37 +0300

    fix: lisää puuttuva käyttöoikeusrajaus

--- backend/src/handler/listAllUsers.ts
+++ backend/src/handler/listAllUsers.ts
@@ -1,5 +1,7 @@
 import * as userService from "../service/userService";
+import { requireVaylaUser } from "../service/userService";
 
 export async function listAllUsers() {
+  requireVaylaUser();
   return userService.listAllUsers();
 }
