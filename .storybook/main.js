eacba1ea5572ec04ac9eda9fcb8cccc60ece72dd
Tomi Korkalainen <tomi.v.korkalainen@gmail.com>
2021-11-17 11:11:43 +0200

    change how storybook postcss addon is applied

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,18 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-postcss"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@styles": path.resolve(__dirname, "../src/styles/"),
