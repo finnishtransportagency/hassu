6072db54b61515ec0f6408244592faf791b0327f
Tomi Korkalainen <77731851+tkork@users.noreply.github.com>
2021-12-10 15:56:05 +0200

    feat: Lisää kuulutuspäiväkenttä aloituskuulutussivulle
    * feat: Lisää kuulutuspäiväkenttä aloituskuulutussivulle
    
    * lisää validointia aloituskuulutuskenttään

--- src/pages/yllapito/projekti/[oid]/aloituskuulutus.tsx
+++ src/pages/yllapito/projekti/[oid]/aloituskuulutus.tsx
@@ -14,12 +14,50 @@ import log from "loglevel";
 import ButtonLink from "@components/button/ButtonLink";
 import HassuLink from "@components/HassuLink";
 import { PageProps } from "@pages/_app";
+import DatePicker from "@components/form/DatePicker";
 
 type FormValues = Pick<TallennaProjektiInput, "oid"> & {
-  aloitusKuulutus: Pick<AloitusKuulutusInput, "hankkeenKuvaus">;
+  aloitusKuulutus: Pick<AloitusKuulutusInput, "hankkeenKuvaus" | "kuulutusPaiva">;
 };
 
 const maxAloituskuulutusLength = 2000;
+
+const draftValidationSchema: SchemaOf<FormValues> = Yup.object().shape({
+  oid: Yup.string().required(),
+  aloitusKuulutus: Yup.object().shape({
+    hankkeenKuvaus: Yup.string().max(
+      maxAloituskuulutusLength,
+      `Aloituskuulutukseen voidaan kirjoittaa maksimissaan ${maxAloituskuulutusLength} merkkiä`
+    ),
+    kuulutusPaiva: Yup.string()
+      .test("is-valid-date", "Virheellinen päivämäärä", (dateString) => {
+        // KuulutusPaiva is not required when saved as a draft.
+        // This test doesn't throw errors if date is not set.
+        if (!dateString) {
+          return true;
+        }
+        let validDate = false;
+        try {
+          const dateString2 = new Date(dateString!).toISOString().split("T")[0];
+          if (dateString2 === dateString) {
+            validDate = true;
+          }
+        } catch {
+          validDate = false;
+        }
+        return validDate;
+      })
+      .test("not-in-past", "Aloituskuulutusta ei voida asettaa menneisyyteen", (dateString) => {
+        // KuulutusPaiva is not required when saved as a draft.
+        // This test doesn't throw errors if date is not set.
+        if (!dateString) {
+          return true;
+        }
+        const todayISODate = new Date().toISOString().split("T")[0];
+        return dateString >= todayISODate;
+      }),
+  }),
+});
 
 export default function Aloituskuulutus({ setRouteLabels }: PageProps): ReactElement {
   const [isFormSubmitting, setIsFormSubmitting] = useState(false);
@@ -29,6 +67,7 @@ export default function Aloituskuulutus({ setRouteLabels }: PageProps): ReactEle
   const projektiError = projektiLoadError || !projekti?.tallennettu;
   const isLoadingProjekti = !projekti && !projektiLoadError;
   const disableFormEdit = projektiError || isLoadingProjekti || isFormSubmitting;
+  const today = new Date().toISOString().split("T")[0];
 
   useEffect(() => {
     if (router.isReady) {
@@ -44,20 +83,8 @@ export default function Aloituskuulutus({ setRouteLabels }: PageProps): ReactEle
     }
   }, [router.isReady, oid, projekti, setRouteLabels]);
 
-  const validationSchema: SchemaOf<FormValues> = Yup.object().shape({
-    oid: Yup.string().required(),
-    aloitusKuulutus: Yup.object().shape({
-      hankkeenKuvaus: Yup.string()
-        .required("Aloituskuulutus on täytettävä.")
-        .max(
-          maxAloituskuulutusLength,
-          `Aloituskuulutukseen voidaan kirjoittaa maksimissaan ${maxAloituskuulutusLength} merkkiä.`
-        ),
-    }),
-  });
-
   const formOptions: UseFormProps<FormValues> = {
-    resolver: yupResolver(validationSchema, { abortEarly: false, recursive: true }),
+    resolver: yupResolver(draftValidationSchema, { abortEarly: false, recursive: true }),
     defaultValues: { aloitusKuulutus: { hankkeenKuvaus: "" } },
     mode: "onChange",
     reValidateMode: "onChange",
@@ -68,19 +95,25 @@ export default function Aloituskuulutus({ setRouteLabels }: PageProps): ReactEle
     handleSubmit,
     formState: { errors },
     reset,
+    watch,
   } = useForm<FormValues>(formOptions);
+
+  const watchKuulutusPaiva = watch("aloitusKuulutus.kuulutusPaiva");
 
   useEffect(() => {
     if (projekti && projekti.oid) {
       const tallentamisTiedot: FormValues = {
         oid: projekti.oid,
-        aloitusKuulutus: { hankkeenKuvaus: projekti?.aloitusKuulutus?.hankkeenKuvaus },
+        aloitusKuulutus: {
+          hankkeenKuvaus: projekti?.aloitusKuulutus?.hankkeenKuvaus,
+          kuulutusPaiva: projekti?.aloitusKuulutus?.kuulutusPaiva,
+        },
       };
       reset(tallentamisTiedot);
     }
   }, [projekti, reset]);
 
-  const onSubmit = async (formData: FormValues) => {
+  const saveDraft = async (formData: FormValues) => {
     setIsFormSubmitting(true);
     try {
       await api.tallennaProjekti(formData);
@@ -89,6 +122,11 @@ export default function Aloituskuulutus({ setRouteLabels }: PageProps): ReactEle
       log.log("OnSubmit Error", e);
     }
     setIsFormSubmitting(false);
+  };
+
+  const sendToManager = async (formData: FormValues) => {
+    log.log(formData);
+    alert("Lähetetään projektipäällikölle...");
   };
 
   return (
@@ -111,6 +149,44 @@ export default function Aloituskuulutus({ setRouteLabels }: PageProps): ReactEle
             )}
           </Notification>
         )}
+        <Notification type={NotificationType.WARN}>
+          Aloituskuulutusta ei ole vielä julkaistu palvelun julkisella puolella.{" "}
+          {watchKuulutusPaiva && !errors.aloitusKuulutus?.kuulutusPaiva
+            ? `Kuulutuspäivä on ${new Date(watchKuulutusPaiva).toLocaleDateString("fi")}`
+            : "Kuulutuspäivää ei ole asetettu"}
+          . Voit edelleen tehdä muutoksia projektin tietoihin. Tallennetut muutokset huomioidaan kuulutuksessa.
+        </Notification>
+        <h3 className="vayla-title">Suunnittelun aloittamisesta kuuluttaminen</h3>
+        <p>
+          Kun suunnitelman aloittamisesta kuulutetaan, projektista julkaistaan aloituskuulutustiedot tämän palvelun
+          julkisella puolella. Aloituskuulutuksen näkyvilläoloaika määräytyy annetun kuulutuspäivän mukaan. Projekti
+          siirtyy määräajan jälkeen automaattisesti suunnitteluvaiheeseen.
+        </p>
+        <Notification type={NotificationType.INFO} hideIcon>
+          <div>
+            <h3 className="vayla-small-title">Ohjeet</h3>
+            <ul className="list-disc block pl-5">
+              <li>
+                Anna päivämäärä, jolloin suunnittelun aloittamisesta kuulutetaan tämän palvelun julkisella puolella.
+              </li>
+              <li>
+                Kuvaa aloituskuulutuksessa esitettävään sisällönkuvauskenttään lyhyesti suunnittelukohteen alueellinen
+                rajaus (maantiealue ja vaikutusalue), suunnittelun tavoitteet, vaikutukset ja toimenpiteet
+                pääpiirteittäin karkealla tasolla. Älä lisää tekstiin linkkejä.
+              </li>
+            </ul>
+          </div>
+        </Notification>
+        <div className="lg:flex md:gap-x-8">
+          <DatePicker
+            label="Kuuluuspäivä *"
+            {...register("aloitusKuulutus.kuulutusPaiva")}
+            disabled={disableFormEdit}
+            min={today}
+            error={errors.aloitusKuulutus?.kuulutusPaiva}
+          />
+          <DatePicker disabled label="Siirtyy suunnitteluvaiheeseen" />
+        </div>
         <Textarea
           label="Suunnitelman aloituskuulutus *"
           {...register("aloitusKuulutus.hankkeenKuvaus")}
@@ -142,10 +218,10 @@ export default function Aloituskuulutus({ setRouteLabels }: PageProps): ReactEle
       </form>
       <hr />
       <div className="flex gap-6 justify-between flex-wrap">
-        <Button onClick={handleSubmit(onSubmit)} disabled={disableFormEdit}>
+        <Button onClick={handleSubmit(saveDraft)} disabled={disableFormEdit}>
           Tallenna Keskeneräisenä
         </Button>
-        <Button primary disabled>
+        <Button primary onClick={handleSubmit(sendToManager)} disabled>
           Lähetä Hyväksyttäväksi
         </Button>
       </div>
