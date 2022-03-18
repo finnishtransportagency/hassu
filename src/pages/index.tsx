9f6d8f5917a1c8daaad9fc9e23cf4ce8555ec92d
Tomi Korkalainen <77731851+tkork@users.noreply.github.com>
2022-03-18 14:20:46 +0200

    fix: Palauta etusivulle kaiken tyyppiset suunnitelmat
    * lisää kaiken tyyppiset projektit etusivulle näkyviin
    
    * korjaa kansalaisen etusivu
    
    * poista admin propsien anto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { ProjektiTyyppi } from "@services/api";
 import { ProjektiListaus } from "../components/projektiListaus";
 
 const App = () => {
   // const [refreshListCounter, setRefreshListCounter] = useState(0)
 
-  return <ProjektiListaus key="1" />;
+  return (
+    <>
+      <h2 className="mt-4">Tiesuunnitelmat</h2>
+      <ProjektiListaus projektiTyyppi={ProjektiTyyppi.TIE} />
+      <h2 className="mt-4">Ratasuunnitelmat</h2>
+      <ProjektiListaus projektiTyyppi={ProjektiTyyppi.RATA} />
+      <h2 className="mt-4">Yleissuunnitelmat</h2>
+      <ProjektiListaus projektiTyyppi={ProjektiTyyppi.YLEINEN} />
+    </>
+  );
 };
 
 export default App;
