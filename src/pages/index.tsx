ffb576421540e3a74217c4ac0ae65db16623b567
Tomi Korkalainen <tomi.korkalainen@cgi.com>
2022-03-18 12:02:07 +0000

    korjaa kansalaisen etusivu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { ProjektiTyyppi } from "@services/api";
 import { ProjektiListaus } from "../components/projektiListaus";
 
 const App = () => {
   // const [refreshListCounter, setRefreshListCounter] = useState(0)
 
-  return <ProjektiListaus key="1" />;
+  return (
+    <>
+      <h2 className="mt-4">Tiesuunnitelmat</h2>
+      <ProjektiListaus admin projektiTyyppi={ProjektiTyyppi.TIE} />
+      <h2 className="mt-4">Ratasuunnitelmat</h2>
+      <ProjektiListaus admin projektiTyyppi={ProjektiTyyppi.RATA} />
+      <h2 className="mt-4">Yleissuunnitelmat</h2>
+      <ProjektiListaus admin projektiTyyppi={ProjektiTyyppi.YLEINEN} />
+    </>
+  );
 };
 
 export default App;
