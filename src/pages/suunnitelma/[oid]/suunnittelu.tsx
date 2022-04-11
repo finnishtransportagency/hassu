63b75c528e57d167de407b5e5a76b7f6894636ce
kettunenj <juhani.kettunen@cgi.com>
2022-04-11 22:49:12 +0300

    julkaisu ja julkinen puoli

--- src/pages/suunnitelma/[oid]/suunnittelu.tsx
+++ src/pages/suunnitelma/[oid]/suunnittelu.tsx
@@ -3,6 +3,7 @@ import ProjektiJulkinenPageLayout from "@components/projekti/kansalaisnakyma/Pro
 import Section from "@components/layout/Section";
 import { useProjektiJulkinen } from "src/hooks/useProjektiJulkinen";
 import { useRouter } from "next/router";
+import SectionContent from "@components/layout/SectionContent";
 
 export default function Suunnittelu(): ReactElement {
   const router = useRouter();
@@ -12,7 +13,31 @@ export default function Suunnittelu(): ReactElement {
   return (
     <ProjektiJulkinenPageLayout selectedStep={1} title="Tutustu hankkeeseen ja vuorovaikuta">
       <>
-        <Section>Hanke siirtynyt suunnitteluvaiheeseen {projekti?.aloitusKuulutusJulkaisut ? projekti.aloitusKuulutusJulkaisut[0].siirtyySuunnitteluVaiheeseen : ""}</Section>
+        <Section>
+          <SectionContent>
+            <h5 className="vayla-small-title">Suunnitteluhankkeen kuvaus</h5>
+            <p>{projekti?.suunnitteluVaihe?.hankkeenKuvaus?.SUOMI}</p>
+          </SectionContent>
+          <SectionContent>
+            <h5 className="vayla-small-title">Suunnittelun eteneminen</h5>
+            <p>{projekti?.suunnitteluVaihe?.suunnittelunEteneminenJaKesto}</p>
+          </SectionContent>
+          <SectionContent>
+            <h5 className="vayla-small-title">Arvio seuraavan vaiheen alkamisesta</h5>
+            <p>{projekti?.suunnitteluVaihe?.arvioSeuraavanVaiheenAlkamisesta}</p>
+          </SectionContent>
+        </Section>
+        <Section>
+          <SectionContent>
+            <h5 className="vayla-small-title">Osallistumisen ja vaikuttamisen mahdollisuudet ja aikataulut</h5>
+            <p>
+              Voit osallistua vuorovaikutustilaisuuksiin, tutustua suunnittelu- ja esittelyaineistoihin sekä jättää
+              palautteen tai kysyä hankkeesta. Osallistumalla sinulla on mahdollisuus vaikuttaa hankkeen suunnitteluun.
+              Suunnitelmaluonnokset ja esittelyaineistot ovat tutustuttavissa xx.xx.xxxx asti. Siirry aineistoihin.
+              Kysymykset ja palautteet toivotaan esitettävän xx.xx.xxxx mennessä. Siirry lomakkeelle.
+            </p>
+          </SectionContent>
+        </Section>
       </>
     </ProjektiJulkinenPageLayout>
   );
