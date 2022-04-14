ae716816c557989777bee3c6e45d6a0f85f16297
kettunenj <juhani.kettunen@cgi.com>
2022-04-14 15:08:12 +0300

    dialogin rautalankaa

--- src/components/projekti/suunnitteluvaihe/SuunniteluvaiheenVuorovaikuttaminen.tsx
+++ src/components/projekti/suunnitteluvaihe/SuunniteluvaiheenVuorovaikuttaminen.tsx
@@ -4,7 +4,8 @@ import SectionContent from "@components/layout/SectionContent";
 import { TallennaProjektiInput, Projekti, api, VuorovaikutusInput } from "@services/api";
 import Section from "@components/layout/Section";
 import { ReactElement, useEffect, useState } from "react";
-import { Stack } from "@mui/material";
+import { Badge, Chip, chipClasses, Stack } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import Button from "@components/button/Button";
 import useSnackbars from "src/hooks/useSnackbars";
 import log from "loglevel";
@@ -15,6 +16,14 @@ import DatePicker from "@components/form/DatePicker";
 import dayjs from "dayjs";
 import { vuorovaikutusSchema } from "src/schemas/vuorovaikutus";
 import HassuStack from "@components/layout/HassuStack";
+import HassuDialog from "@components/HassuDialog";
+import WindowCloseButton from "@components/button/WindowCloseButton";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import TextInput from "@components/form/TextInput";
+import Select from "@components/form/Select";
+import HassuGrid from "@components/HassuGrid";
 
 type ProjektiFields = Pick<TallennaProjektiInput, "oid">;
 type RequiredProjektiFields = Required<{
@@ -51,9 +60,20 @@ export default function SuunniteluvaiheenVuorovaikuttaminen({
   isDirtyHandler,
   vuorovaikutusnro,
 }: Props): ReactElement {
+  const [openVuorovaikutustilaisuus, setOpenVuorovaikutustilaisuus] = useState(false);
   const [isFormSubmitting, setIsFormSubmitting] = useState(false);
   const { showSuccessMessage, showErrorMessage } = useSnackbars();
   const today = dayjs().format();
+
+  const HassuBadge = styled(Badge)(() => ({
+    [`&.${chipClasses.deleteIcon}`]: {
+      marginLeft: "0.5rem",
+      marginRight: "1rem",
+    },
+    [`&.${chipClasses.label}`]: {
+      paddingRight: "0px",
+    },
+  }));
 
   const formOptions: UseFormProps<FormValues> = {
     resolver: yupResolver(vuorovaikutusSchema, { abortEarly: false, recursive: true }),
@@ -156,7 +176,12 @@ export default function SuunniteluvaiheenVuorovaikuttaminen({
           <Section>
             <h5 className="vayla-small-title">Vuorovaikutusmahdollisuudet palautteiden ja kysymyksien lisäksi</h5>
             <SectionContent>
-              <Button type="submit" onClick={() => console.log("tilaisuuden lisäys")} disabled>
+              <Button
+                onClick={(e) => {
+                  setOpenVuorovaikutustilaisuus(true);
+                  e.preventDefault();
+                }}
+              >
                 Lisää tilaisuus
               </Button>
             </SectionContent>
@@ -221,6 +246,102 @@ export default function SuunniteluvaiheenVuorovaikuttaminen({
           </Button>
         </Stack>
       </Section>
+      <div>
+        <HassuDialog
+          open={openVuorovaikutustilaisuus}
+          onClose={() => setOpenVuorovaikutustilaisuus(false)}
+          maxWidth={"lg"}
+        >
+          <Section noDivider smallGaps>
+            <SectionContent>
+              <div className="vayla-dialog-title flex">
+                <div className="flex-grow">Vuorovaikutustilaisuuden lisääminen</div>
+                <div className="justify-end">
+                  <WindowCloseButton onClick={() => setOpenVuorovaikutustilaisuus(false)}></WindowCloseButton>
+                </div>
+              </div>
+            </SectionContent>
+            <SectionContent>
+              <div className="vayla-dialog-content">
+                <form>
+                  <HassuStack>
+                    <p>Voit valita saman vuorovaikutustavan useammin kuin yhden kerran.</p>
+                    <HassuStack direction={["column", "column", "row"]}>
+                      <Chip
+                        icon={<HeadphonesIcon />}
+                        clickable
+                        label="Live-tilaisuus verkossa"
+                        variant="outlined"
+                        onDelete={() => console.log("lul")}
+                        deleteIcon={<HassuBadge badgeContent={7} color={"primary"} />}
+                      />
+                      <Chip
+                        icon={<LocationCityIcon />}
+                        clickable
+                        label="Fyysinen tilaisuus"
+                        variant="outlined"
+                        onDelete={() => console.log("lul")}
+                        deleteIcon={<HassuBadge badgeContent={2} color={"primary"} />}
+                      />
+                      <Chip
+                        icon={<LocalPhoneIcon />}
+                        clickable
+                        label="Soittoaika"
+                        variant="outlined"
+                        onDelete={() => console.log("lul")}
+                        deleteIcon={<HassuBadge badgeContent={1} color={"primary"} />}
+                      />
+                    </HassuStack>
+                    <Section>
+                      <TextInput
+                        label="Tilaisuuden nimi *"
+                        // {...register2("syy", { required: "Palautuksen syy täytyy antaa" })}
+                        // error={errors2.syy}
+                        maxLength={200}
+                      ></TextInput>
+                      <HassuStack direction={["column", "column", "row"]}>
+                        <DatePicker label="Päivämäärä *"></DatePicker>
+                        <TextInput label="Alkaa *" maxLength={6}></TextInput>
+                        <TextInput label="Päättyy *" maxLength={6}></TextInput>
+                      </HassuStack>
+                      <HassuGrid cols={{ lg: 3 }}>
+                        <Select
+                          addEmptyOption
+                          options={[
+                            { label: "Teams", value: "TEAMS" },
+                            { label: "Zoom", value: "ZOOM" },
+                          ]}
+                          label="Käytettävä palvelu *"
+                        />
+                      </HassuGrid>
+                      <TextInput label="Linkki tilaisuuteen *" maxLength={200}></TextInput>
+                      <p>Linkki tilaisuuteen julkaistaan palvelun julkisella puolella kaksi (2) tuntia ennen tilaisuuden alkamista.</p>
+                      <Button>Poista</Button>
+                    </Section>
+                  </HassuStack>
+                  <HassuStack
+                    direction={["column", "column", "row"]}
+                    justifyContent={[undefined, undefined, "flex-end"]}
+                    paddingTop={"1rem"}
+                  >
+                    <Button primary onClick={() => console.log("tallenna")}>
+                      Tallenna
+                    </Button>
+                    <Button
+                      onClick={(e) => {
+                        setOpenVuorovaikutustilaisuus(false);
+                        e.preventDefault();
+                      }}
+                    >
+                      Peruuta
+                    </Button>
+                  </HassuStack>
+                </form>
+              </div>
+            </SectionContent>
+          </Section>
+        </HassuDialog>
+      </div>
       <HassuSpinner open={isFormSubmitting} />
     </>
   );
