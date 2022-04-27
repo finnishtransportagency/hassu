fc59742860478edc9844625a831bbca9f6af761a
kettunenj <juhani.kettunen@cgi.com>
2022-04-27 19:19:20 +0300

    initial commit

--- src/pages/suunnitelma/[oid]/suunnittelu.tsx
+++ src/pages/suunnitelma/[oid]/suunnittelu.tsx
@@ -6,6 +6,14 @@ import { useRouter } from "next/router";
 import SectionContent from "@components/layout/SectionContent";
 import useTranslation from "next-translate/useTranslation";
 import { YhteystietoInput } from "@services/api";
+import { formatDate, formatDayOfWeek } from "src/util/dateUtils";
+import dayjs from "dayjs";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import { VuorovaikutusTilaisuusTyyppi } from "@services/api";
+import capitalize from "lodash/capitalize";
 
 export default function Suunnittelu(): ReactElement {
   const router = useRouter();
@@ -39,32 +47,118 @@ export default function Suunnittelu(): ReactElement {
 
   const yhteystiedotListana = mockVuorovaikutusYhteystiedot.map((yhteystieto) => t("common:yhteystieto", yhteystieto));
 
+  if (!projekti || !projekti.suunnitteluVaihe) {
+    return <></>;
+  }
+
+  const vuorovaikutus = projekti.suunnitteluVaihe.vuorovaikutukset?.[0]; //TODO voiko olla useita, ja mika niista naytetaan
+
+  const getIcon = (tyyppi: VuorovaikutusTilaisuusTyyppi) => {
+    switch (tyyppi) {
+      case VuorovaikutusTilaisuusTyyppi.PAIKALLA:
+        return <LocationCityIcon />;
+      case VuorovaikutusTilaisuusTyyppi.SOITTOAIKA:
+        return <LocalPhoneIcon />;
+      case VuorovaikutusTilaisuusTyyppi.VERKOSSA:
+        return <HeadphonesIcon />;
+      default:
+        return <QuestionMarkIcon />;
+    }
+  };
+
   return (
     <ProjektiJulkinenPageLayout selectedStep={1} title="Tutustu hankkeeseen ja vuorovaikuta">
       <>
         <Section>
           <SectionContent>
             <h5 className="vayla-small-title">Suunnitteluhankkeen kuvaus</h5>
-            <p>{projekti?.suunnitteluVaihe?.hankkeenKuvaus?.SUOMI}</p>
+            <p>{projekti.suunnitteluVaihe.hankkeenKuvaus?.SUOMI}</p>
           </SectionContent>
           <SectionContent>
             <h5 className="vayla-small-title">Suunnittelun eteneminen</h5>
-            <p>{projekti?.suunnitteluVaihe?.suunnittelunEteneminenJaKesto}</p>
+            <p>{projekti.suunnitteluVaihe.suunnittelunEteneminenJaKesto}</p>
           </SectionContent>
           <SectionContent>
             <h5 className="vayla-small-title">Arvio seuraavan vaiheen alkamisesta</h5>
-            <p>{projekti?.suunnitteluVaihe?.arvioSeuraavanVaiheenAlkamisesta}</p>
+            <p>{projekti.suunnitteluVaihe.arvioSeuraavanVaiheenAlkamisesta}</p>
           </SectionContent>
         </Section>
         <Section>
           <SectionContent>
             <h5 className="vayla-small-title">Osallistumisen ja vaikuttamisen mahdollisuudet ja aikataulut</h5>
-            <p>
-              Voit osallistua vuorovaikutustilaisuuksiin, tutustua suunnittelu- ja esittelyaineistoihin sekä jättää
-              palautteen tai kysyä hankkeesta. Osallistumalla sinulla on mahdollisuus vaikuttaa hankkeen suunnitteluun.
-              Suunnitelmaluonnokset ja esittelyaineistot ovat tutustuttavissa xx.xx.xxxx asti. Siirry aineistoihin.
-              Kysymykset ja palautteet toivotaan esitettävän xx.xx.xxxx mennessä. Siirry lomakkeelle.
-            </p>
+            {!vuorovaikutus && (
+              <p>
+                Voit osallistua vuorovaikutustilaisuuksiin, tutustua suunnittelu- ja esittelyaineistoihin sekä jättää
+                palautteen tai kysyä hankkeesta. Osallistumalla sinulla on mahdollisuus vaikuttaa hankkeen
+                suunnitteluun.
+              </p>
+            )}
+            {vuorovaikutus && (
+              <>
+                <p>
+                  Voit osallistua vuorovaikutustilaisuuksiin, tutustua suunnittelu- ja esittelyaineistoihin sekä jättää
+                  palautteen tai kysyä hankkeesta. Osallistumalla sinulla on mahdollisuus vaikuttaa hankkeen
+                  suunnitteluun.
+                </p>
+                <p>
+                  Suunnitelmaluonnokset ja esittelyaineistot ovat tutustuttavissa sivun alareunassa. Siirry
+                  aineistoihin.
+                </p>
+                <p>
+                  Kysymykset ja palautteet toivotaan esitettävän{" "}
+                  {formatDate(vuorovaikutus.kysymyksetJaPalautteetViimeistaan)} mennessä. Siirry lomakkeelle.
+                </p>
+              </>
+            )}
+          </SectionContent>
+          <SectionContent>
+            <h5 className="vayla-small-title">Tulevat vuorovaikutustilaisuudet</h5>
+            {!vuorovaikutus && <p>Vuorovaikutustilaisuudet julkaistaan mahdollisimman pian.</p>}
+            {vuorovaikutus && (
+              <div className="vayla-tilaisuus-list">
+                {vuorovaikutus.vuorovaikutusTilaisuudet
+                  ?.sort((a, b) => {
+                    if (dayjs(a.paivamaara).isBefore(dayjs(b.paivamaara))) {
+                      return -1;
+                    }
+                    if (dayjs(a.paivamaara).isAfter(dayjs(b.paivamaara))) {
+                      return 1;
+                    }
+                    return 0;
+                  })
+                  .map((tilaisuus, index) => {
+                    return (
+                      <div key={index} className="vayla-tilaisuus-item">
+                        <div className="flex flex-cols gap-5">
+                          {getIcon(tilaisuus.tyyppi)}
+                          <p>
+                            <b>
+                              {formatDayOfWeek(tilaisuus.paivamaara)} {formatDate(tilaisuus.paivamaara)} klo{" "}
+                              {tilaisuus.alkamisAika}-{tilaisuus.paattymisAika}, {capitalize(tilaisuus.nimi)}
+                            </b>
+                          </p>
+                        </div>
+                        <div>
+                          <p>Yleisötilaisuus järjestetään suorana verkkotapahtumana.</p>
+                          <p>
+                            Tilaisuus toteutetaan Teamsin välityksellä. Teams-sovelluksen asentamista omalle laitteelle
+                            ei edellytetä. Liittymislinkki toimii Internet-selaimella tietokoneella tai
+                            mobiililaitteella.{" "}
+                          </p>
+                          <p>
+                            Liity tilaisuuteen: Tilaisuuden liittymislinkki julkaistaan tässä kaksi (2) tuntia ennen
+                            tilaisuuden alkua ja poistetaan tilaisuuden jälkeen.
+                          </p>
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
+            )}
+          </SectionContent>
+          <SectionContent>
+            <h5 className="vayla-small-title">Esittelyaineisto ja suunnitelmaluonnokset</h5>
+            {!vuorovaikutus && <p>Aineistot ja luonnokset julkaistaan lähempänä vuorovaikutustilaisuutta.</p>}
           </SectionContent>
         </Section>
         <Section>
