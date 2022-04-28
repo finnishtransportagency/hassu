7eccf5172ba82d7b6b58db91d6af45fcade01e19
Valhe Kouneli <valhe.kouneli@gmail.com>
2022-04-28 10:40:37 +0300

    Translate footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,8 +1,12 @@
 // import Image from "next/image";
 import { Container } from "@mui/material";
 import React from "react";
+import useTranslation from "next-translate/useTranslation";
 
 export const Footer = ({}) => {
+
+  const { t } = useTranslation("projekti");
+
   return (
     <div className="bg-gray-lightest w-full self-start mt-auto">
       <Container sx={{ marginTop: 4 }} component="footer">
@@ -11,20 +15,17 @@ export const Footer = ({}) => {
             {/* <Image src="/vayla-600px.jpg" alt="Väylä" width="100" height="100" /> */}
             {/* <Image src="/ely-400px.png" alt="ELY" width="100" height="100" /> */}
             <p>
-              Hankesuunnitelmista lorem ipsum-teksti tähän, joka kertoo lyhyesti mitä tämä sivusto pitää sisällään.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dicta voluptatem fugit quos praesentium
-              architecto accusamus non earum fuga veritatis, quia facilis est repellendus et iste similique beatae
-              perferendis totam?
+              {t("info.hankesuunnitelmista")}
             </p>
-            <a href="#">Hankesuunnittelusivu</a>
-            <h3 className="mt-3">Oikopolut</h3>
-            <a href="#">Etäkäyttö (Extranet-palvelut, VPN-etäyhteys, intra)</a>
+            <a href="#">{t("ui-linkkitekstit.hankesuunnittelusivu")}</a>
+            <h3 className="mt-3">{t("ui-otsikot.oikopolut")}</h3>
+            <a href="#">{t("ui-linkkitekstit.etakaytto")}</a>
           </div>
           <div className="col-span-8 flex md:items-end md:space-x-4 place-content-end flex-col md:flex-row">
-            <a href="#">Saavutettavuus</a>
-            <a href="#">Tietoa sivustosta</a>
-            <a href="#">Tietosuoja</a>
-            <a href="#">Palautelomake</a>
+            <a href="#">{t("ui-linkkitekstit.saavutettavuus")}</a>
+            <a href="#">{t("ui-linkkitekstit.tietoa_sivustosta")}</a>
+            <a href="#">{t("ui-linkkitekstit.tietosuoja")}</a>
+            <a href="#">{t("ui-linkkitekstit.palautelomake")}</a>
           </div>
         </div>
       </Container>
