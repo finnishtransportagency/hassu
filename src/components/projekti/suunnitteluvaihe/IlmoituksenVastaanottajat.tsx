4d6dbea73c5ca0eec9a29ed9982810c7f4ee6879
Valhe Kouneli <valhe.kouneli@cgi.com>
2022-05-20 13:59:27 +0300

    Näytä viranomaisvastaanottajat niin kuin UI-kuvassa.

--- src/components/projekti/suunnitteluvaihe/IlmoituksenVastaanottajat.tsx
+++ src/components/projekti/suunnitteluvaihe/IlmoituksenVastaanottajat.tsx
@@ -89,23 +89,26 @@ export default function IlmoituksenVastaanottajat({
             </p>
           </SectionContent>
           <SectionContent>
-            <h6 className="font-bold">Viranomaiset</h6>
-            <div className="content grid grid-cols-4 mb-4">
-              <p className="vayla-table-header">Kunta</p>
-              <p className="vayla-table-header">Sähköpostiosoite</p>
-              <p className="vayla-table-header">Ilmoituksen tila</p>
-              <p className="vayla-table-header">Lähetysaika</p>
-              {vuorovaikutus?.ilmoituksenVastaanottajat?.viranomaiset?.map((viranomainen, index) => (
+            <div className="grid grid-cols-4 gap-x-6 mb-4">
+              <h6 className="font-bold">Viranomaiset</h6>
+              <p></p>
+              <p style={{ color: "#7A7A7A" }}>Ilmoituksen tila</p>
+              <p style={{ color: "#7A7A7A" }}>Lähetysaika</p>
+
+              {vuorovaikutus?.ilmoituksenVastaanottajat?.viranomaiset?.map((viranomainen) => (
                 <>
-                  <p className={getStyleForRow(index)}>{viranomainen.nimi}</p>
-                  <p className={getStyleForRow(index)}>{viranomainen.sahkoposti}</p>
-                  <p className={getStyleForRow(index)}>{viranomainen.lahetetty ? "Lahetetty" : "Ei lähetetty"}</p>
-                  <p className={getStyleForRow(index)}>
+                  <p className="odd:bg-white even:bg-grey col-span-2">
+                    {t(`viranomainen.${viranomainen.nimi}`)}, {viranomainen.sahkoposti}
+                  </p>
+                  <p className="odd:bg-white even:bg-grey">{viranomainen.lahetetty ? "Lähetetty" : "Ei lähetetty"}</p>
+                  <p className="odd:bg-white even:bg-grey">
                     {viranomainen.lahetetty ? dayjs(viranomainen.lahetetty).format("DD.MM.YYYY HH:mm") : null}
                   </p>
                 </>
               ))}
             </div>
+          </SectionContent>
+          <SectionContent>
             <h6 className="font-bold">Kunnat</h6>
             <div className="content grid grid-cols-4 mb-4">
               <p className="vayla-table-header">Kunta</p>
