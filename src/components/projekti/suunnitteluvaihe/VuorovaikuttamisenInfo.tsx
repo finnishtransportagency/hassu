ca3cff83a0bc7f709d59c471fe303b5987937e27
Valhe Kouneli <valhe.kouneli@cgi.com>
2022-06-02 12:30:45 +0300

    Lisää puuttuva teksti varoitukseen.

--- src/components/projekti/suunnitteluvaihe/VuorovaikuttamisenInfo.tsx
+++ src/components/projekti/suunnitteluvaihe/VuorovaikuttamisenInfo.tsx
@@ -1,6 +1,4 @@
-import {
-  Vuorovaikutus
-} from "@services/api";
+import { Vuorovaikutus } from "@services/api";
 import React, { ReactElement } from "react";
 import Notification, { NotificationType } from "@components/notification/Notification";
 import { examineJulkaisuPaiva } from "src/util/dateUtils";
@@ -9,19 +7,15 @@ interface Props {
   vuorovaikutus: Vuorovaikutus | undefined;
 }
 
-
-export default function SuunnitteluvaiheenVuorovaikuttaminen({
-  vuorovaikutus
-}: Props): ReactElement {
-
+export default function SuunnitteluvaiheenVuorovaikuttaminen({ vuorovaikutus }: Props): ReactElement {
   const julkinen = !!vuorovaikutus?.julkinen;
 
   if (!julkinen) {
     return (
       <p>
         Kansalainen pääsee vaikuttamaan väylähankkeen tai väylän suunnitteluun siinä vaiheessa. kun tehdään
-        yleissuunnitelmaa ja kun edetään tie- tai ratasuunnitelmaan. Kaikista suunnittelun vaiheista
-        kuulutetaan tai ilmoitetaan, jotta asianosaisilla on mahdollisuus kommentoida suunnitelmia.
+        yleissuunnitelmaa ja kun edetään tie- tai ratasuunnitelmaan. Kaikista suunnittelun vaiheista kuulutetaan tai
+        ilmoitetaan, jotta asianosaisilla on mahdollisuus kommentoida suunnitelmia.
       </p>
     );
   }
@@ -29,16 +23,13 @@ export default function SuunnitteluvaiheenVuorovaikuttaminen({
   let { julkaisuPaiva, published } = examineJulkaisuPaiva(julkinen, vuorovaikutus.vuorovaikutusJulkaisuPaiva);
 
   if (published) {
-    return (
-      <Notification type={NotificationType.INFO_GREEN}>
-        Vuorovaikutus on julkaistu {julkaisuPaiva}.
-      </Notification>
-    );
+    return <Notification type={NotificationType.INFO_GREEN}>Vuorovaikutus on julkaistu {julkaisuPaiva}.</Notification>;
   }
 
   return (
     <Notification type={NotificationType.WARN}>
       Vuorovaikutusta ei ole vielä julkaistu palvelun julkisella puolella. Julkaisu {julkaisuPaiva}.
+      {!vuorovaikutus.aineistot ? " Huomaathan, että suunnitelma-aineistot tulee vielä lisätä." : ""}
     </Notification>
   );
 }
