b0c09b367f536d7fb385e97b8ac3242c6c43feff
Mikko Haapamäki <haapamakim@users.noreply.github.com>
2022-06-06 14:13:55 +0300

    feat: virheilmoitusten välittäminen frontendiin backendistä (#245)

--- backend/src/error/IllegalAccessError.ts
+++ backend/src/error/IllegalAccessError.ts
@@ -1,6 +1,8 @@
-export class IllegalAccessError extends Error {
+import { ClientError } from "./ClientError";
+
+export class IllegalAccessError extends ClientError {
   constructor(m?: string) {
-    super(m ? m : "");
+    super("IllegalAccessError", m);
 
     // Set the prototype explicitly.
     Object.setPrototypeOf(this, IllegalAccessError.prototype);
--- backend/src/error/velhoError.ts
+++ backend/src/error/velhoError.ts
@@ -1,6 +1,8 @@
-export class VelhoError extends Error {
+import { SystemError } from "./SystemError";
+
+export class VelhoError extends SystemError {
   constructor(m?: string, error?: any) {
-    super(m ? m + (error?.response?.data ? JSON.stringify(error.response?.data) : "") : "");
+    super("VelhoError", m ? m + (error?.response?.data ? JSON.stringify(error.response?.data) : "") : "");
 
     // Make the log show VelhoError instead of Error in the stack trace
     Object.setPrototypeOf(this, VelhoError.prototype);
