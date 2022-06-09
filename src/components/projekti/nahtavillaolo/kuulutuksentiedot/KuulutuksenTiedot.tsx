f1101db15f07908131196a64071547976bcbdb66
Valhe Kouneli <valhe.kouneli@cgi.com>
2022-06-09 09:40:59 +0300

    UI tehty niin pitkälle kuin lman BE:tä mahdollista.

--- src/components/projekti/nahtavillaolo/kuulutuksentiedot/KuulutuksenTiedot.tsx
+++ src/components/projekti/nahtavillaolo/kuulutuksentiedot/KuulutuksenTiedot.tsx
@@ -3,13 +3,12 @@ import { TallennaProjektiInput } from "@services/api";
 import React, { useEffect } from "react";
 import { UseFormProps, useForm, FormProvider } from "react-hook-form";
 import { useProjektiRoute } from "src/hooks/useProjektiRoute";
-import { nahtavillaoloAineistotSchema } from "src/schemas/nahtavillaoloAineistot";
+import { nahtavillaoloKuulutusSchema } from "src/schemas/nahtavillaoloKuulutus";
 import NahtavillaoloPainikkeet from "../NahtavillaoloPainikkeet";
 import HankkeenSisallonKuvaus from "./HankkeenSisallonKuvaus";
 import KuulutuksenJaIlmoituksenEsikatselu from "./KuulutuksenJaIlmoituksenEsikatselu";
 import KuulutuksessaEsitettavatYhteystiedot from "./KuulutuksessaEsitettavatYhteystiedot";
 import KuulutusJaJulkaisuPaiva from "./KuulutusJaJulkaisuPaiva";
-import MuistutustenAntaminen from "./MuistutustenAntaminen";
 
 type Props = {};
 
@@ -18,7 +17,7 @@ export type KuulutuksenTiedotFormValues = Pick<TallennaProjektiInput, "oid">;
 export default function NahtavilleAsetettavatAineistot({}: Props) {
   const { data: projekti } = useProjektiRoute();
   const formOptions: UseFormProps<KuulutuksenTiedotFormValues> = {
-    resolver: yupResolver(nahtavillaoloAineistotSchema, { abortEarly: false, recursive: true }),
+    resolver: yupResolver(nahtavillaoloKuulutusSchema, { abortEarly: false, recursive: true }),
     mode: "onChange",
     reValidateMode: "onChange",
   };
@@ -39,7 +38,6 @@ export default function NahtavilleAsetettavatAineistot({}: Props) {
     <FormProvider {...useFormReturn}>
       <form>
         <KuulutusJaJulkaisuPaiva />
-        <MuistutustenAntaminen />
         <HankkeenSisallonKuvaus />
         <KuulutuksessaEsitettavatYhteystiedot />
         <KuulutuksenJaIlmoituksenEsikatselu />
--- src/components/projekti/nahtavillaolo/kuulutuksentiedot/KuulutusJaJulkaisuPaiva.tsx
+++ src/components/projekti/nahtavillaolo/kuulutuksentiedot/KuulutusJaJulkaisuPaiva.tsx
@@ -1,16 +1,94 @@
 import Section from "@components/layout/Section";
-import React from "react";
+import SectionContent from "@components/layout/SectionContent";
+import React, { useCallback } from "react";
+import { useFormContext } from "react-hook-form";
+import { KuulutuksenTiedotFormValues } from "./KuulutuksenTiedot";
+import DatePicker from "@components/form/DatePicker";
+import dayjs from "dayjs";
+import log from "loglevel";
+// import { api, LaskuriTyyppi } from "@services/api";
+import useSnackbars from "src/hooks/useSnackbars";
+import HassuGrid from "@components/HassuGrid";
 
 type Props = {};
 
 export default function KuulutusJaJulkaisuPaiva({}: Props) {
+  const {
+    register,
+    formState: { errors },
+    control,
+    setValue,
+  } = useFormContext<KuulutuksenTiedotFormValues>();
+
+  const { showSuccessMessage, showErrorMessage } = useSnackbars();
+
+  const today = dayjs().format();
+
+  const getPaattymispaiva = useCallback(
+    async (value: string) => {
+      try {
+        //const paattymispaiva = await api.laskePaattymisPaiva(value, LaskuriTyyppi.NAHTAVILLAOLOKUULUTUKSEN_PAATTYMISPAIVA);
+        //setValue("nahtavillaolo.siirtyyLainvoimaan", paattymispaiva);
+      } catch (error) {
+        showErrorMessage("Kuulutuksen päättymispäivän laskennassa tapahtui virhe");
+        log.error("Nähtävilläolon päättymispäivän laskennassa virhe", error);
+      }
+      try {
+        //const muistutuspaiva = await api.laskePaattymisPaiva(value, LaskuriTyyppi.MUISTUTUSTEN_PAATTYMISPAIVA);
+        //setValue("nahtavillaolo.siirtyyLainvoimaan", paattymispaiva);
+      } catch (error) {
+        showErrorMessage("Muistutuspäivän laskennassa tapahtui virhe");
+        log.error("Muistutuspäivän laskennassa virhe", error);
+      }
+    },
+    [setValue, showErrorMessage]
+  );
+
   return (
     <Section noDivider>
-      <h4 className="vayla-small-title">Kuulutus ja julkaisupäivä</h4>
-      <p>
-        Anna päivämäärä, jolle kuulutus päivätään ja nähtävilläolevan suunnitelman materiaalit julkaistaan palvelun
-        julkisella puolella.
-      </p>
+      <SectionContent>
+        <h4 className="vayla-small-title">Kuulutus ja julkaisupäivä</h4>
+        <p>
+          Anna päivämäärä, jolle kuulutus päivätään ja nähtävilläolevan suunnitelman materiaalit julkaistaan palvelun
+          julkisella puolella.
+        </p>
+        <HassuGrid cols={{ lg: 3 }}>
+          <DatePicker
+            label="Kuulutuspäivä *"
+            className="md:max-w-min"
+            //{...register("nahtavillaolo.kuulutusPaiva")}
+            min={today}
+            //error={errors.nahtavillaolo?.kuulutusPaiva}
+            onChange={(event) => {
+              getPaattymispaiva(event.target.value);
+            }}
+          />
+          <DatePicker
+            className="md:max-w-min"
+            label="Kuulutusvaihe päättyy"
+            readOnly
+            //{...register("nahtavillaolo.siirtyyLainvoimaan")}
+          />
+        </HassuGrid>
+      </SectionContent>
+      <SectionContent>
+        <h4 className="vayla-small-title">Muistutusten antaminen</h4>
+        <p>
+          Kansalaisten tulee muistuttaa suunnitelmasta järjestelmän kautta viimeistään alla olevana päivämääränä.
+          Muistutusten päivämäärä määräytyy kuulutuksen nähtävilläoloajan mukaan, ja sitä ei voi muokata.
+        </p>
+        <DatePicker
+          label="Muistutusoikeus päättyy"
+          className="md:max-w-min"
+          //{...register("nahtavillaolo.muistutusoikeusPaattyy")}
+          min={today}
+          //error={errors.nahtavillaolo?.muistutusoikeusPaattyy}
+          readOnly
+          onChange={(event) => {
+            getPaattymispaiva(event.target.value);
+          }}
+        />
+      </SectionContent>
     </Section>
   );
 }
