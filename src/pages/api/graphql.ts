0cfbb82917783c0152a9890ec0cd8e114ba8e0db
Tomi Korkalainen <tomi.v.korkalainen@gmail.com>
2022-06-09 16:59:30 +0300

    fix: Ohjaa käyttäjä kirjautumaan jos käyttäjän kirjautumistiedot ovat vanhentuneet

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -32,6 +32,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   });
   const { body: responseBody, statusCode } = await sendSignedRequest(request, "appsync");
 
+  if (statusCode === 302) {
+    res.redirect(302, "/yllapito/kirjaudu");
+    return;
+  }
+
   res.status(statusCode).json(JSON.stringify(responseBody));
 }
 
