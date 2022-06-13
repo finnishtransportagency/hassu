63074e3bdedb061fc5639216f07117fdbd902424
Valhe Kouneli <valhe.kouneli@cgi.com>
2022-06-13 10:35:52 +0300

    Lisää nähtävilläoloilmon vastaanottajat UI

--- src/components/projekti/nahtavillaolo/kuulutuksentiedot/KuulutuksenTiedot.tsx
+++ src/components/projekti/nahtavillaolo/kuulutuksentiedot/KuulutuksenTiedot.tsx
@@ -1,45 +1,105 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { TallennaProjektiInput } from "@services/api";
+import {
+  TallennaProjektiInput,
+  IlmoitettavaViranomainen,
+  KuntaVastaanottajaInput,
+  ViranomaisVastaanottajaInput,
+  Projekti,
+  IlmoituksenVastaanottajatInput,
+  IlmoituksenVastaanottajat,
+} from "@services/api";
 import React, { useEffect } from "react";
 import { UseFormProps, useForm, FormProvider } from "react-hook-form";
 import { useProjektiRoute } from "src/hooks/useProjektiRoute";
 import { nahtavillaoloKuulutusSchema } from "src/schemas/nahtavillaoloKuulutus";
-import { removeTypeName } from "src/util/removeTypeName";
 import NahtavillaoloPainikkeet from "../NahtavillaoloPainikkeet";
 import HankkeenSisallonKuvaus from "./HankkeenSisallonKuvaus";
 import KuulutuksenJaIlmoituksenEsikatselu from "./KuulutuksenJaIlmoituksenEsikatselu";
 import KuulutuksessaEsitettavatYhteystiedot from "./KuulutuksessaEsitettavatYhteystiedot";
 import KuulutusJaJulkaisuPaiva from "./KuulutusJaJulkaisuPaiva";
+import IlmoituksenVastaanottajatKomponentti from "./IlmoituksenVastaanottajat";
+import getIlmoitettavaViranomainen from "src/util/getIlmoitettavaViranomainen";
+import { removeTypeName } from "src/util/removeTypeName";
 
-type Props = {};
+export const defaultVastaanottajat = (
+  projekti: Projekti | null | undefined,
+  ilmoituksenVastaanottajat: IlmoituksenVastaanottajat | null | undefined,
+  kirjaamoOsoitteet: ViranomaisVastaanottajaInput[] | null
+): IlmoituksenVastaanottajatInput => {
+  let kunnat: KuntaVastaanottajaInput[];
+  let viranomaiset: ViranomaisVastaanottajaInput[];
+  if (ilmoituksenVastaanottajat?.kunnat) {
+    kunnat = ilmoituksenVastaanottajat?.kunnat.map((kunta) => {
+      kunta = removeTypeName(kunta);
+      delete kunta.lahetetty;
+      return kunta;
+    });
+  } else {
+    kunnat =
+      projekti?.velho?.kunnat?.map((s) => {
+        return {
+          nimi: s,
+          sahkoposti: "",
+        } as KuntaVastaanottajaInput;
+      }) || [];
+  }
+  if (ilmoituksenVastaanottajat?.viranomaiset) {
+    viranomaiset = ilmoituksenVastaanottajat?.viranomaiset.map((kunta) => {
+      kunta = removeTypeName(kunta);
+      delete kunta.lahetetty;
+      return kunta;
+    });
+  } else {
+    viranomaiset =
+      projekti?.velho?.suunnittelustaVastaavaViranomainen === "VAYLAVIRASTO"
+        ? projekti?.velho?.maakunnat?.map((maakunta) => {
+            const ely: IlmoitettavaViranomainen = getIlmoitettavaViranomainen(maakunta);
+            return (
+              kirjaamoOsoitteet?.find((osoite) => osoite.nimi == ely) ||
+              ({ nimi: maakunta, sahkoposti: "" } as ViranomaisVastaanottajaInput)
+            );
+          }) || []
+        : [
+            kirjaamoOsoitteet?.find((osoite) => osoite.nimi == "VAYLAVIRASTO") ||
+              ({ nimi: "VAYLAVIRASTO" as IlmoitettavaViranomainen, sahkoposti: "" } as ViranomaisVastaanottajaInput),
+          ];
+  }
+  return {
+    kunnat,
+    viranomaiset,
+  };
+};
+
+function defaultValues(projekti: Projekti, kirjaamoOsoitteet: ViranomaisVastaanottajaInput[] | null) {
+  return {
+    oid: projekti.oid,
+    nahtavillaoloVaihe: {
+      kuulutusPaiva: projekti?.nahtavillaoloVaihe?.kuulutusPaiva,
+      kuulutusVaihePaattyyPaiva: projekti?.nahtavillaoloVaihe?.kuulutusVaihePaattyyPaiva,
+      muistutusoikeusPaattyyPaiva: projekti?.nahtavillaoloVaihe?.muistutusoikeusPaattyyPaiva,
+      ilmoituksenVastaanottajat: defaultVastaanottajat(
+        projekti,
+        projekti.nahtavillaoloVaihe?.ilmoituksenVastaanottajat,
+        kirjaamoOsoitteet
+      ),
+    },
+  };
+}
+
+type Props = {
+  kirjaamoOsoitteet: ViranomaisVastaanottajaInput[] | null;
+};
 
 export type KuulutuksenTiedotFormValues = Pick<TallennaProjektiInput, "oid" | "nahtavillaoloVaihe">;
 
-export default function KuulutuksenTiedot({}: Props) {
+export default function KuulutuksenTiedot({ kirjaamoOsoitteet }: Props) {
   const { data: projekti } = useProjektiRoute();
 
   const formOptions: UseFormProps<KuulutuksenTiedotFormValues> = {
     resolver: yupResolver(nahtavillaoloKuulutusSchema, { abortEarly: false, recursive: true }),
     mode: "onChange",
     reValidateMode: "onChange",
-    defaultValues: {
-      oid: projekti?.oid,
-      nahtavillaoloVaihe: {
-        kuulutusPaiva: projekti?.nahtavillaoloVaihe?.kuulutusPaiva,
-        kuulutusVaihePaattyyPaiva: projekti?.nahtavillaoloVaihe?.kuulutusVaihePaattyyPaiva,
-        muistutusoikeusPaattyyPaiva: projekti?.nahtavillaoloVaihe?.muistutusoikeusPaattyyPaiva,
-        hankkeenKuvaus: removeTypeName(projekti?.nahtavillaoloVaihe?.hankkeenKuvaus),
-        kuulutusYhteystiedot: projekti?.nahtavillaoloVaihe?.kuulutusYhteystiedot
-          ? projekti.nahtavillaoloVaihe.kuulutusYhteystiedot.map((yhteystieto) => removeTypeName(yhteystieto))
-          : [],
-        kuulutusYhteysHenkilot:
-          projekti?.kayttoOikeudet
-            ?.filter(({ kayttajatunnus }) =>
-              projekti?.nahtavillaoloVaihe?.kuulutusYhteysHenkilot?.includes(kayttajatunnus)
-            )
-            .map(({ kayttajatunnus }) => kayttajatunnus) || [],
-      },
-    },
+    defaultValues: {},
   };
 
   const useFormReturn = useForm<KuulutuksenTiedotFormValues>(formOptions);
@@ -47,12 +107,10 @@ export default function KuulutuksenTiedot({}: Props) {
 
   useEffect(() => {
     if (projekti?.oid) {
-      const tallentamisTiedot: KuulutuksenTiedotFormValues = {
-        oid: projekti.oid,
-      };
+      const tallentamisTiedot: KuulutuksenTiedotFormValues = defaultValues(projekti, kirjaamoOsoitteet);
       reset(tallentamisTiedot);
     }
-  }, [projekti, reset]);
+  }, [projekti, kirjaamoOsoitteet, reset]);
 
   return (
     <FormProvider {...useFormReturn}>
@@ -60,6 +118,10 @@ export default function KuulutuksenTiedot({}: Props) {
         <KuulutusJaJulkaisuPaiva />
         <HankkeenSisallonKuvaus kielitiedot={projekti?.kielitiedot} />
         <KuulutuksessaEsitettavatYhteystiedot />
+        <IlmoituksenVastaanottajatKomponentti
+          kirjaamoOsoitteet={kirjaamoOsoitteet}
+          nahtavillaoloVaihe={projekti?.nahtavillaoloVaihe}
+        />
         <KuulutuksenJaIlmoituksenEsikatselu />
         <NahtavillaoloPainikkeet />
       </form>
