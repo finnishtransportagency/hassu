84432f10271952e6b07bd2d33eb28f17bee30b6c
kettunenj <juhani.kettunen@cgi.com>
2022-06-16 16:36:56 +0300

    lookahead vaihdettu yksinkertaisempaan tapaan safarin tuen takia

--- common/util/formatProperNoun.ts
+++ common/util/formatProperNoun.ts
@@ -1,11 +1,11 @@
 export function formatProperNoun(text: string) {
-  const firstCharactersInWords = new RegExp(/(?<=^|[^\p{L}])\p{L}/, "gu");
-  const notFirstCharacterInWords = new RegExp(/(?<!^|[^\p{L}])\p{L}/, "gu");
+  // Ongelmana oli, ettei Safari tue lookaheadia ja looknehindia.
+  // Talle ei nakynyt kayttoa muualla, kuin kuntanimien yhteydessa
+  // joten uskon whitespacen ja valiviivan jalkeisella uppercasella
+  // parjaavan (naita kuntiakin on vain yksi Mantta-Vilppula)
+  const startOfWords = new RegExp(/(^|[\s-])\S/g);
 
-  return text
-    .trim()
-    .replace(firstCharactersInWords, (a) => a.toUpperCase())
-    .replace(notFirstCharacterInWords, (a) => a.toLowerCase());
+  return text.trim().replace(startOfWords, (a) => a.toUpperCase());
 }
 
 export default { formatProperNoun };
