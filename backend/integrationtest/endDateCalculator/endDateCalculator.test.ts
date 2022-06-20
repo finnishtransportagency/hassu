aba63dc2fdbe893e49e100d1e8d9c7c2088483ae
Mikko Haapamäki <mikko.haapamaki@cgi.com>
2022-06-20 11:09:23 +0300

    feature: Nähtävilläolon kuulutusaikalaskuri. Liian pitkän laskuriajan korjaus.

--- backend/integrationtest/endDateCalculator/endDateCalculator.test.ts
+++ backend/integrationtest/endDateCalculator/endDateCalculator.test.ts
@@ -6,8 +6,13 @@ import { LaskuriTyyppi } from "../../../common/graphql/apiModel";
 const { expect } = require("chai");
 
 describe("EndDateCalculator", () => {
-  it("should calculate correct end date", async () => {
-    expect(await api.laskePaattymisPaiva("2022-11-02", LaskuriTyyppi.KUULUTUKSEN_PAATTYMISPAIVA)).to.be.equal(
+  it("should calculate correct end date (KUULUTUKSEN_PAATTYMISPAIVA)", async () => {
+    expect(await api.laskePaattymisPaiva("2022-11-03", LaskuriTyyppi.KUULUTUKSEN_PAATTYMISPAIVA)).to.be.equal(
+      "2022-12-05"
+    );
+  });
+  it("should calculate correct end date (NAHTAVILLAOLON_KUULUTUKSEN_PAATTYMISPAIVA)", async () => {
+    expect(await api.laskePaattymisPaiva("2022-11-03", LaskuriTyyppi.NAHTAVILLAOLON_KUULUTUKSEN_PAATTYMISPAIVA)).to.be.equal(
       "2022-12-05"
     );
   });
--- backend/test/endDateCalculator/endDateCalculator.test.ts
+++ backend/test/endDateCalculator/endDateCalculator.test.ts
@@ -33,25 +33,25 @@ describe("Api", () => {
 
   it("should calculate correct end date over holidays", async () => {
     expect(
-      await calculateEndDate({ alkupaiva: "2022-11-23", tyyppi: LaskuriTyyppi.KUULUTUKSEN_PAATTYMISPAIVA })
+      await calculateEndDate({ alkupaiva: "2022-11-24", tyyppi: LaskuriTyyppi.KUULUTUKSEN_PAATTYMISPAIVA })
     ).to.be.equal("2022-12-27");
   });
 
   it("should calculate correct end date over holidays in dev and test environments with time 00:00", async () => {
     expect(
-      await calculateEndDate({ alkupaiva: "2022-11-23T00:00", tyyppi: LaskuriTyyppi.KUULUTUKSEN_PAATTYMISPAIVA })
+      await calculateEndDate({ alkupaiva: "2022-11-24T00:00", tyyppi: LaskuriTyyppi.KUULUTUKSEN_PAATTYMISPAIVA })
     ).to.be.equal("2022-12-27T00:00");
   });
 
   it("should calculate correct end date over holidays in dev and test environments with time 12:34", async () => {
     expect(
-      await calculateEndDate({ alkupaiva: "2022-11-23T12:34", tyyppi: LaskuriTyyppi.KUULUTUKSEN_PAATTYMISPAIVA })
+      await calculateEndDate({ alkupaiva: "2022-11-24T12:34", tyyppi: LaskuriTyyppi.KUULUTUKSEN_PAATTYMISPAIVA })
     ).to.be.equal("2022-12-27T12:34");
   });
 
   it("should calculate correct end date over weekend", async () => {
     expect(
-      await calculateEndDate({ alkupaiva: "2022-11-02", tyyppi: LaskuriTyyppi.KUULUTUKSEN_PAATTYMISPAIVA })
+      await calculateEndDate({ alkupaiva: "2022-11-03", tyyppi: LaskuriTyyppi.KUULUTUKSEN_PAATTYMISPAIVA })
     ).to.be.equal("2022-12-05");
   });
 
