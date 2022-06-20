c288bf1982266e61426cb16b63db3c27dc1647d5
Mikko Haapamäki <haapamakim@users.noreply.github.com>
2022-06-20 11:23:30 +0300

    fix: korjaa kuulutustaulusyötteen käyttäjien tunnistaminen (#279)

--- src/util/parameterStore.ts
+++ src/util/parameterStore.ts
@@ -1,6 +1,6 @@
 import AWS from "aws-sdk";
 
-const ssm = new AWS.SSM();
+const ssm = new AWS.SSM({ region: "eu-west-1" });
 
 async function getParameter(name: string) {
   return (await ssm.getParameter({ Name: name, WithDecryption: true }).promise()).Parameter?.Value;
