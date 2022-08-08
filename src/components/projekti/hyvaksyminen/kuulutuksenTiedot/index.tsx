ec7655f2a7084aeb6617153d447d63d59a3c5535
Valhe Kouneli <valhe.kouneli@cgi.com>
2022-08-08 14:53:22 +0300

    Pohja hyvaksymisvaiheen virkamiesnäkymälle

---
+++ src/components/projekti/hyvaksyminen/kuulutuksenTiedot/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useProjekti } from "src/hooks/useProjekti";
+//import { useFormContext } from "react-hook-form";
+
+export default function KuulutuksenTiedot() {
+  const { data: projekti } = useProjekti();
+
+  //const { handleSubmit } = useFormContext<tyypitys>();
+
+  if (!projekti) {
+    return null;
+  }
+
+  return <div>OSIO</div>;
+}
