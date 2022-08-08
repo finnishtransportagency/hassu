55691f59297104e892f4ffa435b5b7dfb90d9ff1
Valhe Kouneli <valhe.kouneli@cgi.com>
2022-08-08 16:02:02 +0300

    temp

--- src/components/projekti/hyvaksyminen/kuulutuksenTiedot/index.tsx
+++ src/components/projekti/hyvaksyminen/kuulutuksenTiedot/index.tsx
@@ -1,15 +1,112 @@
-import React from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { TallennaProjektiInput, KirjaamoOsoite, Projekti } from "@services/api";
+import Notification, { NotificationType } from "@components/notification/Notification";
+import React, { useEffect, useState } from "react";
+import { UseFormProps, useForm, FormProvider } from "react-hook-form";
 import { useProjekti } from "src/hooks/useProjekti";
-//import { useFormContext } from "react-hook-form";
+import { hyvaksymispaatosKuulutusSchema } from "src/schemas/hyvaksymispaatosKuulutus";
+// import Painikkeet from "./Painikkeet";
+// import HankkeenSisallonKuvaus from "./HankkeenSisallonKuvaus";
+// import KuulutuksenJaIlmoituksenEsikatselu from "./KuulutuksenJaIlmoituksenEsikatselu";
+// import KuulutuksessaEsitettavatYhteystiedot from "./KuulutuksessaEsitettavatYhteystiedot";
+// import KuulutusJaJulkaisuPaiva from "./KuulutusJaJulkaisuPaiva";
+// import IlmoituksenVastaanottajatKomponentti from "./IlmoituksenVastaanottajat";
+// import Lukunakyma from "./Lukunakyma";
+import defaultVastaanottajat from "src/util/defaultVastaanottajat";
+import { removeTypeName } from "src/util/removeTypeName";
+import useKirjaamoOsoitteet from "src/hooks/useKirjaamoOsoitteet";
+import PdfPreviewForm from "@components/projekti/PdfPreviewForm";
 
-export default function KuulutuksenJaIlmoituksenEsikatselu() {
+function defaultValues(
+  projekti: Projekti,
+  kirjaamoOsoitteet: KirjaamoOsoite[] | undefined
+): KuulutuksenTiedotFormValues {
+  return {
+    oid: projekti.oid,
+    hyvaksymisVaihe: {
+      kuulutusPaiva: projekti?.hyvaksymisVaihe?.kuulutusPaiva,
+      kuulutusVaihePaattyyPaiva: projekti?.hyvaksymisVaihe?.kuulutusVaihePaattyyPaiva,
+      hallintoOikeudet: projekti?.hyvaksymisVaihe?.hallintoOikeudet,
+      kuulutusYhteystiedot: projekti?.hyvaksymisVaihe?.kuulutusYhteystiedot
+        ? projekti.hyvaksymisVaihe.kuulutusYhteystiedot.map((yhteystieto) => removeTypeName(yhteystieto))
+        : [],
+      ilmoituksenVastaanottajat: defaultVastaanottajat(
+        projekti,
+        projekti.hyvaksymisVaihe?.ilmoituksenVastaanottajat,
+        kirjaamoOsoitteet
+      ),
+    },
+  };
+}
+
+export type KuulutuksenTiedotFormValues = Pick<TallennaProjektiInput, "oid" | "hyvaksymisVaihe">;
+
+export default function KuulutuksenTiedot() {
   const { data: projekti } = useProjekti();
+  const [formContext, setFormContext] = useState<Projekti | undefined>(undefined);
+  const { data: kirjaamoOsoitteet } = useKirjaamoOsoitteet();
 
-  //const { handleSubmit } = useFormContext<tyypitys>();
+  const pdfFormRef = React.useRef<React.ElementRef<typeof PdfPreviewForm>>(null);
+
+  const formOptions: UseFormProps<KuulutuksenTiedotFormValues> = {
+    resolver: yupResolver(hyvaksymispaatosKuulutusSchema, { abortEarly: false, recursive: true }),
+    mode: "onChange",
+    reValidateMode: "onChange",
+    defaultValues: {},
+    context: formContext,
+  };
+
+  const useFormReturn = useForm<KuulutuksenTiedotFormValues>(formOptions);
+  const { reset } = useFormReturn;
+
+  useEffect(() => {
+    if (projekti?.oid) {
+      const tallentamisTiedot: KuulutuksenTiedotFormValues = defaultValues(projekti, kirjaamoOsoitteet);
+      setFormContext(projekti);
+      reset(tallentamisTiedot);
+    }
+  }, [projekti, kirjaamoOsoitteet, reset]);
+
+  const voiMuokata = !projekti?.hyvaksymisVaiheJulkaisut || projekti.hyvaksymisVaiheJulkaisut.length < 1;
 
   if (!projekti) {
     return null;
   }
 
-  return <div>OSIO</div>;
+  return (
+    <>
+      {projekti.hyvaksymisVaihe?.palautusSyy && (
+        <Notification type={NotificationType.WARN}>
+          {"Hyväksymisvaihejulkaisu on palautettu korjattavaksi. Palautuksen syy: " +
+            projekti.hyvaksymisVaihe.palautusSyy}
+        </Notification>
+      )}
+      {voiMuokata && (
+        <>
+          <FormProvider {...useFormReturn}>
+            <form>
+              {/* <KuulutusJaJulkaisuPaiva />
+              <HankkeenSisallonKuvaus kielitiedot={projekti?.kielitiedot} />
+              <KuulutuksessaEsitettavatYhteystiedot />
+              <IlmoituksenVastaanottajatKomponentti hyvaksymisVaihe={projekti?.hyvaksymisVaihe} />
+              {pdfFormRef.current?.esikatselePdf && (
+                <KuulutuksenJaIlmoituksenEsikatselu esikatselePdf={pdfFormRef.current?.esikatselePdf} />
+              )}
+              <Painikkeet projekti={projekti} /> */}
+            </form>
+          </FormProvider>
+          <PdfPreviewForm ref={pdfFormRef} />
+        </>
+      )}
+      {!voiMuokata && projekti && projekti.hyvaksymisVaiheJulkaisut?.[projekti.hyvaksymisVaiheJulkaisut.length - 1] && (
+        <FormProvider {...useFormReturn}>
+          {/* <Lukunakyma
+            projekti={projekti}
+            hyvaksymisVaiheJulkaisu={projekti.hyvaksymisVaiheJulkaisut[projekti.hyvaksymisVaiheJulkaisut.length - 1]}
+          />
+          <Painikkeet projekti={projekti} /> */}
+        </FormProvider>
+      )}
+    </>
+  );
 }
---
+++ src/schemas/hyvaksymispaatosKuulutus.tsx
@@ -0,0 +1,51 @@
+import * as Yup from "yup";
+import { isDevEnvironment } from "@services/config";
+import { ilmoituksenVastaanottajat } from "./common";
+
+function validateDate(dateString: string) {
+  try {
+    const dateString2 = new Date(dateString!).toISOString().split("T")[0];
+    if (isDevEnvironment) {
+      return dateString!.startsWith(dateString2);
+    }
+    return dateString2 === dateString;
+  } catch {
+    return false;
+  }
+}
+export const hyvaksymispaatosKuulutusSchema = Yup.object().shape({
+  oid: Yup.string().required(),
+  nahtavillaoloVaihe: Yup.object()
+    .required()
+    .shape({
+      hallintoOikeudet: Yup.array().of(Yup.string()),
+      kuulutusYhteysHenkilot: Yup.array().notRequired().of(Yup.string()),
+      kuulutusPaiva: Yup.string()
+        .required("Kuulutuspäivä ei voi olla tyhjä")
+        .nullable()
+        .test("is-valid-date", "Virheellinen päivämäärä", (dateString) => {
+          if (!dateString) {
+            return false;
+          }
+          return validateDate(dateString);
+        })
+        .test("not-in-past", "Kuulutuspäivää ei voida asettaa menneisyyteen", (dateString) => {
+          // Kuulutuspaiva is not required when saved as a draft.
+          // This test doesn't throw errors if date is not set.
+          if (!dateString) {
+            return true;
+          }
+          const todayISODate = new Date().toISOString().split("T")[0];
+          return dateString >= todayISODate;
+        }),
+      kuulutusVaihePaattyyPaiva: Yup.string().test("is-valid-date", "Virheellinen päivämäärä", (dateString) => {
+        // kuulutusVaihePaattyyPaiva is not required when saved as a draft.
+        // This test doesn't throw errors if date is not set.
+        if (!dateString) {
+          return true;
+        }
+        return validateDate(dateString);
+      }),
+      ilmoituksenVastaanottajat: ilmoituksenVastaanottajat(),
+    }),
+});
