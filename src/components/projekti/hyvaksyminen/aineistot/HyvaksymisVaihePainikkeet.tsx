bd2b7b24b09645637c94d569c4627241c0fd8001
Valhe Kouneli <valhe.kouneli@cgi.com>
2022-08-09 11:45:00 +0300

    Hyväsymispäätöksen aineistotabi jaettu komponentteihin

---
+++ src/components/projekti/hyvaksyminen/aineistot/HyvaksymisVaihePainikkeet.tsx
@@ -0,0 +1,63 @@
+import Button from "@components/button/Button";
+import HassuSpinner from "@components/HassuSpinner";
+import Section from "@components/layout/Section";
+import { Stack } from "@mui/material";
+import { api, TallennaProjektiInput } from "@services/api";
+import log from "loglevel";
+import React, { useState } from "react";
+import { useFormContext } from "react-hook-form";
+import { useProjekti } from "src/hooks/useProjekti";
+import useSnackbars from "src/hooks/useSnackbars";
+import deleteFieldArrayIds from "src/util/deleteFieldArrayIds";
+import { HyvaksymisVaiheAineistotFormValues } from "./Muokkausnakyma";
+
+const mapFormValuesToTallennaProjektiInput = ({
+  oid,
+  hyvaksymisPaatos,
+  aineistoNahtavilla,
+}: HyvaksymisVaiheAineistotFormValues): TallennaProjektiInput => {
+  const aineistoNahtavillaFlat = Object.values(aineistoNahtavilla).flat();
+  deleteFieldArrayIds(aineistoNahtavillaFlat);
+  deleteFieldArrayIds(hyvaksymisPaatos);
+
+  return { oid, hyvaksymisVaihe: { aineistoNahtavilla: aineistoNahtavillaFlat, hyvaksymisPaatos } };
+};
+
+export default function NahtavillaoloPainikkeet() {
+  const { mutate: reloadProjekti } = useProjekti();
+  const [isFormSubmitting, setIsFormSubmitting] = useState(false);
+  const { showSuccessMessage, showErrorMessage } = useSnackbars();
+
+  const { handleSubmit } = useFormContext<HyvaksymisVaiheAineistotFormValues>();
+
+  const saveSuunnitteluvaihe = async (formData: TallennaProjektiInput) => {
+    await api.tallennaProjekti(formData);
+    if (reloadProjekti) await reloadProjekti();
+  };
+
+  const saveDraft = async (formData: HyvaksymisVaiheAineistotFormValues) => {
+    setIsFormSubmitting(true);
+    try {
+      await saveSuunnitteluvaihe(mapFormValuesToTallennaProjektiInput(formData));
+      showSuccessMessage("Tallennus onnistui!");
+    } catch (e) {
+      log.error("OnSubmit Error", e);
+      showErrorMessage("Tallennuksessa tapahtui virhe");
+    }
+    setIsFormSubmitting(false);
+  };
+
+  return (
+    <>
+      <Section noDivider>
+        <Stack justifyContent={{ md: "flex-end" }} direction={{ xs: "column", md: "row" }}>
+          <Button onClick={handleSubmit(saveDraft)}>Tallenna Luonnos</Button>
+          <Button primary disabled onClick={undefined}>
+            Lähetä Hyväksyttäväksi
+          </Button>
+        </Stack>
+      </Section>
+      <HassuSpinner open={isFormSubmitting} />
+    </>
+  );
+}
---
+++ src/components/projekti/hyvaksyminen/aineistot/Muokkausnakyma.tsx
@@ -0,0 +1,83 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Aineisto, AineistoInput, TallennaProjektiInput } from "@services/api";
+import React, { useEffect } from "react";
+import { UseFormProps, useForm, FormProvider } from "react-hook-form";
+import { useProjekti } from "src/hooks/useProjekti";
+import { nahtavillaoloAineistotSchema } from "src/schemas/nahtavillaoloAineistot";
+import HyvaksymisVaihePainikkeet from "./HyvaksymisVaihePainikkeet";
+import Hyvaksymispaatos from "./Hyvaksymispaatos";
+import SuunnitelmatJaAineistot from "./SuunnitelmatJaAineistot";
+import { ProjektiLisatiedolla } from "src/hooks/useProjekti";
+import { aineistoKategoriat } from "common/aineistoKategoriat";
+
+interface AineistoNahtavilla {
+  [kategoriaId: string]: AineistoInput[];
+}
+
+type FormData = {
+  aineistoNahtavilla: AineistoNahtavilla;
+  hyvaksymisPaatos: AineistoInput[];
+};
+
+export type HyvaksymisVaiheAineistotFormValues = Pick<TallennaProjektiInput, "oid"> & FormData;
+
+const getDefaultValueForAineistoNahtavilla = (aineistot: Aineisto[] | undefined | null) => {
+  return aineistoKategoriat.listKategoriaIds().reduce<AineistoNahtavilla>((aineistoNahtavilla, currentKategoriaId) => {
+    aineistoNahtavilla[currentKategoriaId] =
+      aineistot
+        ?.filter((aineisto) => aineisto.kategoriaId === currentKategoriaId)
+        .map<AineistoInput>((aineisto) => ({
+          dokumenttiOid: aineisto.dokumenttiOid,
+          nimi: aineisto.nimi,
+          jarjestys: aineisto.jarjestys,
+          kategoriaId: aineisto.kategoriaId,
+        })) || [];
+    return aineistoNahtavilla;
+  }, {});
+};
+
+function defaultValues(projekti: ProjektiLisatiedolla): HyvaksymisVaiheAineistotFormValues {
+  const hyvaksymisPaatos: AineistoInput[] =
+    projekti.hyvaksymisVaihe?.hyvaksymisPaatos?.map(({ dokumenttiOid, nimi, jarjestys }) => ({
+      dokumenttiOid,
+      jarjestys,
+      nimi,
+    })) || [];
+
+  return {
+    oid: projekti.oid,
+    aineistoNahtavilla: getDefaultValueForAineistoNahtavilla(projekti.hyvaksymisVaihe?.aineistoNahtavilla),
+    hyvaksymisPaatos,
+  };
+}
+
+export default function Muokkausnakyma() {
+  const { data: projekti } = useProjekti();
+
+  const formOptions: UseFormProps<HyvaksymisVaiheAineistotFormValues> = {
+    resolver: yupResolver(nahtavillaoloAineistotSchema, { abortEarly: false, recursive: true }),
+    mode: "onChange",
+    reValidateMode: "onChange",
+  };
+
+  const useFormReturn = useForm<HyvaksymisVaiheAineistotFormValues>(formOptions);
+  const { reset } = useFormReturn;
+
+  useEffect(() => {
+    if (projekti?.oid) {
+      const tallentamisTiedot = defaultValues(projekti);
+      reset(tallentamisTiedot);
+    }
+  }, [projekti, reset]);
+
+  return (
+    <FormProvider {...useFormReturn}>
+      Tekstiä tähän
+      <form>
+        <Hyvaksymispaatos />
+        <SuunnitelmatJaAineistot />
+        <HyvaksymisVaihePainikkeet />
+      </form>
+    </FormProvider>
+  );
+}
