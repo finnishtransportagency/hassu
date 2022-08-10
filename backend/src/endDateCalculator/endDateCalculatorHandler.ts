6c363ecc12e38c112545d72d9e434a18a81715f7
Mikko Haapamäki <haapamakim@users.noreply.github.com>
2022-08-10 16:30:00 +0300

    feat: BE: Hyväksymispäätöksen kuulutusaikalaskuri (#313)

--- backend/src/endDateCalculator/endDateCalculatorHandler.ts
+++ backend/src/endDateCalculator/endDateCalculatorHandler.ts
@@ -1,14 +1,23 @@
-import { LaskePaattymisPaivaQueryVariables } from "../../../common/graphql/apiModel";
+import { LaskePaattymisPaivaQueryVariables, LaskuriTyyppi } from "../../../common/graphql/apiModel";
 import { dateTimeToString, dateToString, ISO_DATE_FORMAT, parseDate } from "../util/dateUtil";
 import { Dayjs } from "dayjs";
 import { bankHolidaysClient } from "./bankHolidaysClient";
 import { config } from "../config";
 
 export async function calculateEndDate({
-  alkupaiva,
-  tyyppi: _tyyppi,
-}: LaskePaattymisPaivaQueryVariables): Promise<string> {
-  // Calculator currectly has only two types (KUULUTUKSEN_PAATTYMISPAIVA, NAHTAVILLAOLON_KUULUTUKSEN_PAATTYMISPAIVA) and they are calculated with equal formula
+                                         alkupaiva,
+                                         tyyppi,
+                                       }: LaskePaattymisPaivaQueryVariables): Promise<string> {
+  switch (tyyppi) {
+    case LaskuriTyyppi.KUULUTUKSEN_PAATTYMISPAIVA:
+    case LaskuriTyyppi.NAHTAVILLAOLON_KUULUTUKSEN_PAATTYMISPAIVA:
+      return calculateOnePlusGivenDaysSkipHolidays(alkupaiva, 30);
+    case LaskuriTyyppi.HYVAKSYMISPAATOKSEN_KUULUTUSAIKA:
+      return calculateOnePlusGivenDaysSkipHolidays(alkupaiva, 7 + 30); // julkaisupäivä + 7 päivän tiedoksisaantiaika + 30 päivän muutoksenhakuaika
+  }
+}
+
+async function calculateOnePlusGivenDaysSkipHolidays(alkupaiva: string, days: number) {
   let start: Dayjs;
   // Only accept dates in prod, but allow datetimes in other environments
   const isDateOnly = config.isProd() || alkupaiva.length == ISO_DATE_FORMAT.length;
@@ -23,7 +32,7 @@ export async function calculateEndDate({
       throw new Error("Alkupäivän pitää olla muotoa YYYY-MM-DDTHH:mm");
     }
   }
-  let endDate: Dayjs = start.add(30, "day");
+  let endDate: Dayjs = start.add(days, "day");
   const bankHolidays = await bankHolidaysClient.getBankHolidays();
   while (bankHolidays.isBankHoliday(endDate)) {
     endDate = endDate.add(1, "day");
