67cebdef18669caf7f3cb9b4efc12f09c7d1e7e1
Tomi Korkalainen <77731851+tkork@users.noreply.github.com>
2022-08-19 16:46:24 +0300

    feat: siirrä notifikaatiot sivun yläreunaan ja lisää puuttuvia Notification-elementtejä (#325)

--- src/pages/yllapito/projekti/[oid]/nahtavillaolo.tsx
+++ src/pages/yllapito/projekti/[oid]/nahtavillaolo.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { FC, ReactElement } from "react";
 import ProjektiPageLayout from "@components/projekti/ProjektiPageLayout";
 import { PageProps } from "@pages/_app";
 import useProjektiBreadcrumbs from "src/hooks/useProjektiBreadcrumbs";
@@ -6,48 +6,114 @@ import Notification, { NotificationType } from "@components/notification/Notific
 import Tabs from "@components/layout/tabs/Tabs";
 import KuulutuksenTiedot from "@components/projekti/nahtavillaolo/kuulutuksentiedot/KuulutuksenTiedot";
 import NahtavilleAsetettavatAineistot from "@components/projekti/nahtavillaolo/nahtavilleAsetettavatAineistot/NahtavilleAsetettavatAineistot";
+import { useProjekti } from "src/hooks/useProjekti";
+import { NahtavillaoloVaiheTila } from "@services/api";
+import dayjs from "dayjs";
+import SectionContent from "@components/layout/SectionContent";
+import FormatDate from "@components/FormatDate";
+
+const InfoElement: FC = () => {
+  const { data: projekti } = useProjekti();
+
+  const julkaisut = projekti?.nahtavillaoloVaiheJulkaisut;
+
+  const julkaisu = julkaisut?.[julkaisut.length - 1];
+
+  if (julkaisu?.tila === NahtavillaoloVaiheTila.HYVAKSYTTY) {
+    // Toistaiseksi tarkastellaan julkaisupaivatietoa, koska ei ole olemassa erillista tilaa julkaistulle kuulutukselle
+    const julkaisupvm = dayjs(julkaisu.kuulutusPaiva);
+    if (dayjs().isBefore(julkaisupvm, "day")) {
+      return (
+        <Notification type={NotificationType.WARN}>
+          {`Kuulutusta ei ole vielä julkaistu. Kuulutuspäivä ${julkaisupvm.format("DD.MM.YYYY")}.`}
+        </Notification>
+      );
+    } else {
+      return (
+        <Notification type={NotificationType.INFO_GREEN}>
+          Kuulutus on julkaistu {julkaisupvm.format("DD.MM.YYYY")}. Projekti näytetään kuulutuspäivästä lasketun
+          määräajan jälkeen palvelun julkisella puolella suunnittelussa olevana. Kuulutusvaihe päättyy{" "}
+          <FormatDate date={julkaisu.kuulutusVaihePaattyyPaiva} />.
+        </Notification>
+      );
+    }
+  } else if (julkaisu?.tila === NahtavillaoloVaiheTila.ODOTTAA_HYVAKSYNTAA) {
+    if (projekti?.nykyinenKayttaja.onProjektipaallikko || projekti?.nykyinenKayttaja.onYllapitaja) {
+      return (
+        <Notification type={NotificationType.WARN}>
+          {
+            "Kuulutus odottaa hyväksyntää. Tarkasta kuulutus ja a) hyväksy tai a) palauta kuulutus korjattavaksi, jos havaitset puutteita tai virheen."
+          }
+        </Notification>
+      );
+    } else {
+      return (
+        <Notification type={NotificationType.WARN}>
+          Kuulutus on hyväksyttävänä projektipäälliköllä. Jos kuulutusta tarvitsee muokata, ota yhteys
+          projektipäällikköön.
+        </Notification>
+      );
+    }
+  } else if (julkaisu?.tila === NahtavillaoloVaiheTila.PALAUTETTU) {
+    return (
+      <>
+        {projekti?.nahtavillaoloVaihe?.palautusSyy && (
+          <Notification type={NotificationType.WARN}>
+            {"Aloituskuulutus on palautettu korjattavaksi. Palautuksen syy: " + projekti.nahtavillaoloVaihe.palautusSyy}
+          </Notification>
+        )}
+      </>
+    );
+  } else {
+    return <></>;
+  }
+};
 
 export default function Nahtavillaolo({ setRouteLabels }: PageProps): ReactElement {
   useProjektiBreadcrumbs(setRouteLabels);
 
   return (
     <ProjektiPageLayout title="Nähtävilläolovaihe">
-      <Notification type={NotificationType.INFO} hideIcon>
-        <div>
-          <h3 className="vayla-small-title">Ohjeet</h3>
-          <ul className="list-disc block pl-5">
-            <li>
-              Lisää nähtäville asetettavat aineistot sekä lausuntopyynnön lisäaineistot kuulutuksen ensimmäiseltä
-              välilehdeltä.
-            </li>
-            <li>Siirry Kuulutuksen tiedot-välilehdelle täyttämään kuulutuksen perustiedot.</li>
-            <li>
-              Anna päivämäärä, jolloin suunnittelun nähtäville asettamisesta kuulutetaan. Projekti ja sen nähtävilläolon
-              kuulutus julkaistaan samana päivänä Valtion liikenneväylien suunnittelu -palvelun kansalaispuolella.
-            </li>
-            <li>
-              Muokkaa tai täydennä halutessasi tiivistetty sisällönkuvaus hankkeesta. Jos projektista tulee tehdä
-              kuulutus suomen lisäksi toisella kielellä, eikä tälle ole kenttää, tarkista projektin tiedot -sivulta
-              projektin kieliasetus.
-            </li>
-            <li>Valitse kuulutuksessa esitettävät yhteystiedot.</li>
-            <li>Lähetä nähtäville asettamisen kuulutus projektipäällikölle hyväksyttäväksi.</li>
-            <li>
-              {
-                "Projekti näytetään kuulutuspäivästä lasketun määräajan jälkeen palvelun julkisella puolella 'Hyväksyntämenettelyssä' -olevana."
-              }
-            </li>
-          </ul>
-        </div>
-      </Notification>
-      <Tabs
-        tabStyle="Underlined"
-        defaultValue={0}
-        tabs={[
-          { label: "Nähtäville asetettavat aineistot", content: <NahtavilleAsetettavatAineistot /> },
-          { label: "Kuulutuksen tiedot", content: <KuulutuksenTiedot /> },
-        ]}
-      />
+      <SectionContent largeGaps>
+        <InfoElement />
+        <Notification type={NotificationType.INFO} hideIcon>
+          <div>
+            <h3 className="vayla-small-title">Ohjeet</h3>
+            <ul className="list-disc block pl-5">
+              <li>
+                Lisää nähtäville asetettavat aineistot sekä lausuntopyynnön lisäaineistot kuulutuksen ensimmäiseltä
+                välilehdeltä.
+              </li>
+              <li>Siirry Kuulutuksen tiedot-välilehdelle täyttämään kuulutuksen perustiedot.</li>
+              <li>
+                Anna päivämäärä, jolloin suunnittelun nähtäville asettamisesta kuulutetaan. Projekti ja sen
+                nähtävilläolon kuulutus julkaistaan samana päivänä Valtion liikenneväylien suunnittelu -palvelun
+                kansalaispuolella.
+              </li>
+              <li>
+                Muokkaa tai täydennä halutessasi tiivistetty sisällönkuvaus hankkeesta. Jos projektista tulee tehdä
+                kuulutus suomen lisäksi toisella kielellä, eikä tälle ole kenttää, tarkista projektin tiedot -sivulta
+                projektin kieliasetus.
+              </li>
+              <li>Valitse kuulutuksessa esitettävät yhteystiedot.</li>
+              <li>Lähetä nähtäville asettamisen kuulutus projektipäällikölle hyväksyttäväksi.</li>
+              <li>
+                {
+                  "Projekti näytetään kuulutuspäivästä lasketun määräajan jälkeen palvelun julkisella puolella 'Hyväksyntämenettelyssä' -olevana."
+                }
+              </li>
+            </ul>
+          </div>
+        </Notification>
+        <Tabs
+          tabStyle="Underlined"
+          defaultValue={0}
+          tabs={[
+            { label: "Nähtäville asetettavat aineistot", content: <NahtavilleAsetettavatAineistot /> },
+            { label: "Kuulutuksen tiedot", content: <KuulutuksenTiedot /> },
+          ]}
+        />
+      </SectionContent>
     </ProjektiPageLayout>
   );
 }
