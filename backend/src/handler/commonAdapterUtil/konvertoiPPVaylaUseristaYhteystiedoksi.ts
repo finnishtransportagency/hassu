4e7f37cea16557ca626da66c30406d429b981767
Valhe Kouneli <valhe.kouneli@cgi.com>
2022-08-29 12:25:03 +0300

    Korjaa etunimi ja sukunimi oikein päin

--- backend/src/handler/commonAdapterUtil/konvertoiPPVaylaUseristaYhteystiedoksi.ts
+++ backend/src/handler/commonAdapterUtil/konvertoiPPVaylaUseristaYhteystiedoksi.ts
@@ -2,8 +2,8 @@ import { DBVaylaUser, Yhteystieto } from "../../database/model";
 
 export function konvertoiPPVaylaUseristaYhteystiedoksi(pp: DBVaylaUser): Yhteystieto {
   return {
-    etunimi: pp.nimi.split(",")[0].trim(),
-    sukunimi: pp.nimi.split(",")[1].trim(),
+    etunimi: pp.nimi.split(",")[1].trim(),
+    sukunimi: pp.nimi.split(",")[0].trim(),
     puhelinnumero: pp.puhelinnumero,
     sahkoposti: pp.email,
     organisaatio: pp.organisaatio,
