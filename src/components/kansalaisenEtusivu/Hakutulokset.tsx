15dcd134f68a797c96a9869cb104a9a775b04cf4
Valhe Kouneli <valhe.kouneli@cgi.com>
2022-09-08 09:46:57 +0300

    Desktop-näkymän projektikortin muotoilu.

--- src/components/kansalaisenEtusivu/Hakutulokset.tsx
+++ src/components/kansalaisenEtusivu/Hakutulokset.tsx
@@ -1,42 +1,61 @@
-import { ProjektiHakutulosJulkinen } from "@services/api";
-import { experimental_sx as sx, styled } from "@mui/material";
+import { ProjektiHakutulosJulkinen, Status } from "@services/api";
+import { ProjektinTila, Suunnitelmatyyppi, OtsikkoLinkki, HakutulosListaItem, HakutulosLista, Kuvaus } from "./TyylitellytKomponentit";
+import useTranslation from "next-translate/useTranslation";
+import { formatDate } from "../../util/dateUtils";
 
 type Props = {
   hakutulos: ProjektiHakutulosJulkinen | undefined;
   ladataan: boolean | undefined | null;
 };
+
+
+
+function getSivuTilanPerusteella(tila: Status | null | undefined) {
+  if (!tila) {
+    return "";
+  }
+  switch (tila) {
+    case Status.ALOITUSKUULUTUS:
+      return "aloituskuulutus";
+    case Status.SUUNNITTELU:
+        return "suunnitteluvaihe";
+    case Status.NAHTAVILLAOLO:
+      return "nahtavillaolo";
+    case Status.HYVAKSYMISMENETTELYSSA:
+      return "hyvaksymismenettelyssa";
+    case Status.HYVAKSYTTY:
+      return ""; //todo
+    case Status.LAINVOIMA:
+      return "lainvoima";
+    default:
+      return "";
+  }
+}
+
 export default function Hakutulokset({ hakutulos, ladataan }: Props) {
+
+  const { t } = useTranslation();
+
   if (!hakutulos && ladataan) {
     return <>Ladataan...</>;
   }
 
   return (
     <HakutulosLista className="Hakutulokset">
-      {hakutulos?.tulokset?.map((tulos) => (
-        <HakutulosListaItem key={tulos.oid}>{tulos.oid}</HakutulosListaItem>
-      ))}
+      {hakutulos?.tulokset?.map((tulos) => {
+
+        return (
+          <HakutulosListaItem key={tulos.oid}>
+            <OtsikkoLinkki href={`suunnitelma/${tulos.oid}/${getSivuTilanPerusteella(tulos.vaihe)}`}>{tulos.nimi}</OtsikkoLinkki>
+            <Suunnitelmatyyppi>{t(`projekti:projekti-tyyppi.${tulos.projektiTyyppi}`)}</Suunnitelmatyyppi>
+            <ProjektinTila>{t(`projekti:projekti-status.${tulos.vaihe}`)}</ProjektinTila>
+            <Kuvaus>{tulos.hankkeenKuvaus}</Kuvaus>
+            {t("projekti:ui-otsikot.paivitetty")}{" "}{formatDate(tulos.paivitetty)}
+          </HakutulosListaItem>
+        ); 
+      })}
     </HakutulosLista>
   );
 }
 
-const HakutulosLista = styled("ol")(
-  sx({
-    width: "100%",
-    marginLeft: 0,
-    listStyle: "none",
-    borderRight: "1px solid #F7F7F7",
-  })
-);
 
-const HakutulosListaItem = styled("li")(() =>
-  sx({
-    "&:nth-of-type(odd)": {
-      backgroundColor: "#F7F7F7",
-    },
-    "&:nth-of-type(even)": {
-      backgroundColor: "white",
-    },
-    borderBottom: "solid 2px #49c2f1",
-    padding: 7,
-  })
-);
