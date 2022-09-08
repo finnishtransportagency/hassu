ef10b02f60a014938522c64a162e344f49ba446f
Valhe Kouneli <valhe.kouneli@cgi.com>
2022-09-08 14:05:14 +0300

    Alustava sivutus

--- src/components/kansalaisenEtusivu/TyylitellytKomponentit.tsx
+++ src/components/kansalaisenEtusivu/TyylitellytKomponentit.tsx
@@ -34,14 +34,6 @@ export const ProjektinTilaMobiili = styled("div")(
   })
 );
 
-export const ProjektinTilaMobiili = styled('div')(
-  sx({
-    fontWeight: "bold",
-    marginTop: "1em",
-    paddingBottom: "1em"
-  })
-)
-
 export const VuorovaikutusTilaisuus = styled(Tagi)(
   sx({
     width: "200px",
@@ -70,16 +62,6 @@ export const OtsikkoLinkkiMobiili = styled(Link)(
     display: "block",
   })
 );
-
-export const OtsikkoLinkkiMobiili = styled(Link)(
-  sx({
-    color: "#0063AF",
-    fontWeight: "bold",
-    fontSize: 24,
-    position: "relative",
-    display: "block"
-  })
-)
 
 export const HakutulosLista = styled("ol")(
   sx({
