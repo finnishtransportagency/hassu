552a8bb5c5348dfa52219fffb7410c14f3a29ca9
tomi korkalainen <tomi.korkalainen@cgi.com>
2022-09-15 10:23:33 +0300

    korjaa tyylejä

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,10 +1,6 @@
-// import Image from "next/image";
-import { Container } from "@mui/material";
+import { Container, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
 import useTranslation from "next-translate/useTranslation";
-import { Grid } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import { experimental_sx as sx, styled } from "@mui/material";
 import Link from "next/link";
 import Image from "next/image";
@@ -13,76 +9,67 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const Footer = ({}) => {
   const { t } = useTranslation("projekti");
   const theme = useTheme();
-  const desktop = useMediaQuery(theme.breakpoints.up("md"));
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
 
   return (
-    <div className="bg-gray-lightest w-full self-start mt-auto">
-      <Container sx={{ marginTop: 4, paddingBottom: 4 }} component="footer">
-        <Grid container spacing={2} style={{ paddingBottom: "4em", paddingTop: "4em" }}>
-          <Grid item lg={6} md={12} style={desktop ? { width: "100%" } : { width: "100%", paddingLeft: "2em" }}>
-            <div style={{ maxWidth: "23em" }}>
-              <KuvaContainer>
-                <Image src="/vayla-600px.jpg" alt="Väylä" width="100" height="100" />
-                <Image src="/ely-400px.png" alt="ELY" width="100" height="100" />
-              </KuvaContainer>
-              <p>{t("info.hankesuunnitelmista")}</p>
-              <Linkkilista1>
-                <FooterLinkkiEl href="TODO" teksti={t("ui-linkkitekstit.valtion-vaylien-suunnittelu")} />
-                <br />
-                <FooterLinkkiEl href={"http://väylävirasto.fi"} teksti={"Väylävirasto.fi"} />
-                <FooterLinkkiEl href={"http://ely-keskus.fi"} teksti={"Ely-keskus.fi"} />
-              </Linkkilista1>
-            </div>
-          </Grid>
-          <Grid item lg={6} md={12} style={{ width: "100%", position: "relative", marginTop: "4em" }}>
-            <Linkkilista2 className={desktop ? "desktop" : "mobiili"}>
-              <li>
-                <Link href="#">{t("ui-linkkitekstit.saavutettavuus")}</Link>
-              </li>
-              <li>
-                <Link href="#">{t("ui-linkkitekstit.tietoa_sivustosta")}</Link>
-              </li>
-              <li>
-                <Link href="#">{t("ui-linkkitekstit.tietosuoja")}</Link>
-              </li>
-              <li>
-                <Link href="#">{t("ui-linkkitekstit.palautelomake")}</Link>
-              </li>
-            </Linkkilista2>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
+    <Container
+      component="footer"
+      className="py-16 bg-gray-lightest w-full self-start mt-auto gap-x-6 flex flex-col md:flex-row justify-center md:justify-between md:items-end"
+    >
+      <div>
+        <div style={{ width: isDesktop ? "20em" : undefined }}>
+          <div className="flex">
+            <KuvaContainer className="justify-center">
+              <div className="my-auto text-center">
+                <Image src="/vayla_alla_fi_sv_rgb.png" alt="Väylä" width="140.4" height="117" />
+              </div>
+              <div className="my-auto text-center">
+                <Image src="/ely_alla_fi_sv_rgb.png" alt="Ely" width="170.61" height="91" />
+              </div>
+            </KuvaContainer>
+          </div>
+          <p className="mt-5">{t("info.hankesuunnitelmista")}</p>
+          <ul>
+            <FooterLinkkiEl href="/TODO" teksti={t("ui-linkkitekstit.valtion-vaylien-suunnittelu")} />
+            <FooterLinkkiEl href={"http://väylävirasto.fi"} teksti={"Väylävirasto.fi"} />
+            <FooterLinkkiEl href={"http://ely-keskus.fi"} teksti={"Ely-keskus.fi"} />
+          </ul>
+        </div>
+      </div>
+      <div>
+        <Linkkilista2>
+          <li>
+            <Link href="#">{t("ui-linkkitekstit.saavutettavuus")}</Link>
+          </li>
+          <li>
+            <Link href="#">{t("ui-linkkitekstit.tietoa_sivustosta")}</Link>
+          </li>
+          <li>
+            <Link href="#">{t("ui-linkkitekstit.tietosuoja")}</Link>
+          </li>
+          <li>
+            <Link href="#">{t("ui-linkkitekstit.palautelomake")}</Link>
+          </li>
+        </Linkkilista2>
+      </div>
+    </Container>
   );
 };
 
-const FooterLinkkiEl = ({ href, teksti }: { href: string; teksti: string }): JSX.Element => {
-  return (
-    <li>
-      <FooterLinkki href={href}>
-        {teksti}
-        <div style={{ width: "auto", display: "inline-block" }} />
-        <FontAwesomeIcon className="ml-2 mt-1" icon="chevron-right" />
-      </FooterLinkki>
-    </li>
-  );
-};
+const FooterLinkkiEl = ({ href, teksti }: { href: string; teksti: string }): JSX.Element => (
+  <li className="first:mb-6 first:mt-0 mt-1">
+    <FooterLinkki href={href}>
+      <span style={{ minWidth: "8em" }}>{teksti}</span>
+      <FontAwesomeIcon icon="chevron-right" />
+    </FooterLinkki>
+  </li>
+);
 
 const KuvaContainer = styled("div")(
   sx({
-    width: "100%",
     display: "flex",
-    justifyContent: "space-between",
-    paddingBottom: "1em",
-    " > *": {
-      marginRight: "3em!important",
-    },
-  })
-);
-
-const Linkkilista1 = styled("ul")(
-  sx({
-    width: "fit-content",
+    gap: 2,
+    flexWrap: "wrap",
   })
 );
 
@@ -92,31 +79,26 @@ const FooterLinkki = styled("a")(
     "&:hover": {
       textDecoration: "underline",
     },
-    display: "flex",
-    justifyContent: "space-between",
+    display: "inline-flex",
+    gap: 3,
+    alignItems: "center",
   })
 );
 
 const Linkkilista2 = styled("ul")(
   sx({
-    width: "100%",
-    marginTop: "0.5em",
-    marginBottom: "0.5em",
-    "&.mobiili": {
-      textAlign: "center",
-    },
-    "&.desktop": {
-      position: "absolute",
-      bottom: 0,
-      " li": {
-        display: "inline-block",
-      },
-    },
-    " li": {
-      marginRight: "0.75em",
-    },
-    " a": {
+    alignSelf: "end",
+    display: "flex",
+    justifyContent: "end",
+    rowGap: 2,
+    columnGap: 8,
+    flexWrap: "wrap",
+    marginTop: 15,
+    flexDirection: { xs: "column", md: "row" },
+    textAlign: { xs: "center", md: null },
+    a: {
       color: "#0063AF",
+      display: "inline-block",
       "&:hover": {
         textDecoration: "underline",
       },
