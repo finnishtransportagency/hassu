046fa65586fdb6f3e72e2eb86fc08f0977788f35
Mikko Haapamäki <mikko.haapamaki@cgi.com>
2022-10-07 13:46:57 +0300

    fix: korjaa cypress-testien ajaminen

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -28,5 +28,16 @@ module.exports = (on) => {
     },
   });
 
-  require('cypress-mochawesome-reporter/plugin')(on);
+  on("before:browser:launch", (browser = {}, config) => {
+    if (browser.name === "chrome") {
+      // Päivämäärän valinnasta katoaa CI/CD-ajossa kyky kirjoittaa päivämäärä tekstinä. Tällä asetuksella tekstin kirjoittaminen saadaan takaisin.
+      // Asetukset arvattu näiden perusteella:
+      // https://github.com/mui/mui-x/pull/5684/files#diff-8cbe9d03b9fa5e04ef3fa3f8be9763779e57a2b026d5c70cb2ed7afcef0d6df5R129
+      // https://github.com/grafana/xk6-browser/issues/147
+      config.args.push("--blink-settings=primaryHoverType=2,availableHoverTypes=2,primaryPointerType=4,availablePointerTypes=4");
+    }
+    return config;
+  });
+
+  require("cypress-mochawesome-reporter/plugin")(on);
 };
