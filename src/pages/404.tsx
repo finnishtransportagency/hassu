3332e5156245817793f6d6bf477643db4b0a9f17
Tomi Korkalainen <77731851+tkork@users.noreply.github.com>
2022-11-04 08:16:24 +0200

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 
 import useTranslation from "next-translate/useTranslation";
-import { useRouter } from "next/router";
 
 export default function Custom404() {
-  const router = useRouter();
-  console.log(router);
   const { t } = useTranslation("common");
   return <h1>404 - {t("sivua-ei-loydy")}</h1>;
 }
