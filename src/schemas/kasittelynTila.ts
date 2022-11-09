dc34d62be24c0d634ec1990ee522145954431174
kettunenj <juhani.kettunen@cgi.com>
2022-11-09 14:18:41 +0200

    lisaa vaihtoehtoiset pakollisuudet hyvaksymispaatoksen kentille

--- src/schemas/kasittelynTila.ts
+++ src/schemas/kasittelynTila.ts
@@ -3,7 +3,18 @@ import { paivamaara } from "./paivamaaraSchema";
 
 const hyvaksymispaatosSchema = Yup.object()
   .shape({
-    paatoksenPvm: paivamaara({ preventFuture: true }),
+    paatoksenPvm: paivamaara({ preventFuture: true }).test({
+      message: "Päivämäärä pakollinen",
+      test: (pvm, context) => {
+        if (context.path.startsWith("kasittelynTila.hyvaksymispaatos.paatoksenPvm") && !pvm) {
+          return false;
+        }
+        if (context.parent.asianumero && !pvm) {
+          return false;
+        }
+        return true;
+      },
+    }),
     asianumero: Yup.string()
       .max(30, "Asiatunnus voi olla maksimissaan 30 merkkiä pitkä")
       .notRequired()
