0402a7f3a6eca00dac6d6af56e1a1f6fc54f1b14
tomi korkalainen <tomi.korkalainen@cgi.com>
2022-11-23 09:27:05 +0200

    lisää maksimipituus selosteille

--- src/schemas/uudelleenKuulutus.ts
+++ src/schemas/uudelleenKuulutus.ts
@@ -7,11 +7,21 @@ interface UudelleenKuulutusSchemaProps {
   uudelleenKuulutusKey: string;
 }
 
+export const maxSelosteLength = 2000;
+
 export const uudelleenKuulutus = ({ requiredText, uudelleenKuulutusKey }: UudelleenKuulutusSchemaProps) =>
   Yup.object()
     .shape({
-      selosteLahetekirjeeseen: lokalisoituTeksti({ requiredText }),
-      selosteKuulutukselle: lokalisoituTeksti({ requiredText })
+      selosteLahetekirjeeseen: lokalisoituTeksti({
+        requiredText,
+        additionalStringValidations: (schema) =>
+          schema.max(maxSelosteLength, `Selosteelle voidaan kirjoittaa maksimissaan ${maxSelosteLength} merkkiä`),
+      }),
+      selosteKuulutukselle: lokalisoituTeksti({
+        requiredText,
+        additionalStringValidations: (schema) =>
+          schema.max(maxSelosteLength, `Selosteelle voidaan kirjoittaa maksimissaan ${maxSelosteLength} merkkiä`),
+      })
         .when(uudelleenKuulutusKey, {
           is: (uudelleenKuulutus: UudelleenKuulutus | null | undefined) => {
             return uudelleenKuulutus?.tila === UudelleenkuulutusTila.JULKAISTU_PERUUTETTU;
