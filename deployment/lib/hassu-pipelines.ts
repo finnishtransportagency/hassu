f703947806004e22a50281acc2d5b3c5c221d0bb
Mikko Haapamäki <mikko.haapamaki@cgi.com>
2022-12-20 10:53:05 +0200

    feat: hakuindeksien päivitys asennuksen jälkeen

--- deployment/lib/hassu-pipelines.ts
+++ deployment/lib/hassu-pipelines.ts
@@ -264,7 +264,18 @@ export class HassuPipelineStack extends Stack {
       buildProject.addToRolePolicy(
         new PolicyStatement({
           effect: Effect.ALLOW,
-          actions: ["s3:*", "cloudformation:*", "sts:*", "ecr:*", "ssm:*", "secretsmanager:GetSecretValue", "codebuild:StartBuild"],
+          actions: [
+            "s3:*",
+            "cloudformation:*",
+            "sts:*",
+            "ecr:*",
+            "ssm:*",
+            "secretsmanager:GetSecretValue",
+            "codebuild:StartBuild",
+            "lambda:ListFunctions",
+            "lambda:InvokeFunction",
+            "lambda:GetFunction",
+          ],
           resources: ["*"],
         })
       );
