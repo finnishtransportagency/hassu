f900525967300d80251bfbc2fa68ebdbfd8a1565
tomi korkalainen <tomi.korkalainen@cgi.com>
2022-12-28 15:32:32 +0200

    jonkinlaista kategorisointia

--- common/aineistoKategoriat.ts
+++ common/aineistoKategoriat.ts
@@ -1,49 +1,65 @@
 import { Aineisto, AineistoInput } from "./graphql/apiModel";
 
-export {};
-
-type AineistoKategoriaProps = {
-  id: string;
-  hakulauseet?: string[];
+type PaaAineistoKategoriaProps = AineistoKategoriaProps & {
   alakategoriat?: AineistoKategoriaProps[];
 };
 
-export class AineistoKategoria {
-  private readonly props: AineistoKategoriaProps;
-  private readonly parent?: AineistoKategoria;
-  private readonly childKategoriat?: AineistoKategoria[];
+type HakuLauseet = (string | RegExp)[];
 
-  constructor(props: AineistoKategoriaProps, parent?: AineistoKategoria) {
+type AineistoKategoriaProps = {
+  id: string;
+  hakulauseet?: HakuLauseet;
+};
+
+export abstract class AineistoKategoria {
+  private readonly props: AineistoKategoriaProps;
+  constructor(props: AineistoKategoriaProps) {
     this.props = props;
-    this.parent = parent;
-    this.childKategoriat = props.alakategoriat?.map((alakategoriaProps) => new AineistoKategoria(alakategoriaProps, this), this);
   }
 
   public get id(): string {
     return this.props.id;
   }
 
-  public get parentKategoria(): AineistoKategoria | undefined {
-    return this.parent;
+  public get hakulauseet(): HakuLauseet | undefined {
+    return this.props.hakulauseet;
+  }
+}
+
+export class PaaAineistoKategoria extends AineistoKategoria {
+  private readonly childKategoriat?: AlaAineistoKategoria[];
+
+  constructor(props: PaaAineistoKategoriaProps) {
+    super(props);
+    this.childKategoriat = props.alakategoriat?.map((alakategoriaProps) => new AlaAineistoKategoria(alakategoriaProps, this), this);
   }
 
   public get alaKategoriat(): AineistoKategoria[] | undefined {
     return this.childKategoriat;
   }
+}
 
-  public get hakulauseet(): string[] | undefined {
-    return this.props.hakulauseet;
+export class AlaAineistoKategoria extends AineistoKategoria {
+  private readonly parent?: PaaAineistoKategoria;
+
+  constructor(props: AineistoKategoriaProps, parent?: PaaAineistoKategoria) {
+    super(props);
+    this.parent = parent;
+  }
+
+  public get parentKategoria(): PaaAineistoKategoria | undefined {
+    return this.parent;
   }
 }
 
 export class AineistoKategoriat {
-  private readonly ylaKategoriat: AineistoKategoria[];
+  private readonly ylaKategoriat: PaaAineistoKategoria[];
 
-  constructor(aineistoKategoriat: AineistoKategoriaProps[]) {
-    this.ylaKategoriat = aineistoKategoriat.map((kategoria) => new AineistoKategoria(kategoria));
+  constructor(aineistoKategoriat: PaaAineistoKategoriaProps[]) {
+    this.ylaKategoriat = aineistoKategoriat.map((kategoria) => new PaaAineistoKategoria(kategoria));
   }
 
-  public listKategoriat(): AineistoKategoria[] {
+  public listKategoriat(): PaaAineistoKategoria[] {
     return this.ylaKategoriat;
   }
 
@@ -52,13 +68,9 @@ export class AineistoKategoriat {
   }
 
   public findKategoria(aineistoKuvaus: string | undefined, tiedostoNimi: string): AineistoKategoria | undefined {
-    const ylakategoria = findMatchingCategory(this.ylaKategoriat, aineistoKuvaus);
+    const ylakategoria = findMatchingCategory(this.ylaKategoriat, aineistoKuvaus, undefined);
     if (ylakategoria) {
-      let alakategoria = findMatchingCategory(ylakategoria.alaKategoriat, aineistoKuvaus);
-      if (alakategoria) {
-        return alakategoria;
-      }
-      alakategoria = findMatchingCategory(ylakategoria.alaKategoriat, tiedostoNimi);
+      const alakategoria = findMatchingCategory(ylakategoria.alaKategoriat, aineistoKuvaus, tiedostoNimi);
       if (alakategoria) {
         return alakategoria;
       }
@@ -84,7 +96,7 @@ export const getNestedCategoryIds: (kategoriat: AineistoKategoria[]) => string[]
   const kategoriaIds: string[] = [];
   kategoriat.forEach((kategoria) => {
     kategoriaIds.push(kategoria.id);
-    if (kategoria.alaKategoriat) {
+    if (kategoria instanceof PaaAineistoKategoria && kategoria.alaKategoriat) {
       kategoriaIds.push(...getNestedCategoryIds(kategoria.alaKategoriat));
     }
   });
@@ -93,7 +105,7 @@ export const getNestedCategoryIds: (kategoriat: AineistoKategoria[]) => string[]
 
 export function kategorianAllaOlevienAineistojenMaara(aineistoNahtavilla: Aineisto[], kategoria: AineistoKategoria): number {
   const kategorianAineistojenMaara = aineistoNahtavilla.filter((aineisto) => aineisto.kategoriaId === kategoria.id).length;
-  if (!kategoria.alaKategoriat || kategoria.alaKategoriat.length === 0) {
+  if (!(kategoria instanceof PaaAineistoKategoria) || !kategoria.alaKategoriat || kategoria.alaKategoriat.length === 0) {
     return kategorianAineistojenMaara;
   } else {
     return kategoria.alaKategoriat.reduce((acc, kategoria) => {
@@ -109,59 +121,94 @@ export function getNestedAineistoMaaraForCategory(aineistot: (Aineisto | Aineist
   return nestedAineistoMaaraSum;
 }
 
-function isMatchingToHakulauseet(hakulauseet: string[] | undefined, keyword: string): boolean {
-  const matchingHakulauseet = hakulauseet?.filter((hakulause) => {
-    return !!keyword.match(hakulause);
-  });
-  return !!matchingHakulauseet && matchingHakulauseet.length > 0;
+function isMatchingToHakulauseet(hakulauseet: HakuLauseet | undefined, keyword: string | undefined): boolean {
+  return !!hakulauseet?.find((hakulause) => !!keyword?.match(new RegExp(hakulause, "i")));
 }
 
-function findMatchingCategory(kategoriat: AineistoKategoria[] | undefined, keyword?: string): AineistoKategoria | undefined {
-  if (!keyword) {
+function findMatchingCategory<T extends AineistoKategoria>(
+  kategoriat: T[] | undefined,
+  aineistoKuvaus: string | undefined,
+  tiedostoNimi: string | undefined
+): T | undefined {
+  if (!aineistoKuvaus && !tiedostoNimi) {
     return undefined;
   }
   if (kategoriat) {
-    return kategoriat.filter((kategoria) => isMatchingToHakulauseet(kategoria.hakulauseet, keyword)).pop();
+    return kategoriat
+      .filter(
+        (kategoria) =>
+          isMatchingToHakulauseet(kategoria.hakulauseet, aineistoKuvaus) || isMatchingToHakulauseet(kategoria.hakulauseet, tiedostoNimi)
+      )
+      .pop();
   }
 }
 
 export const aineistoKategoriat = new AineistoKategoriat([
   {
-    id: "T1xx",
-    hakulauseet: ["T1[0-9]{2}"],
-    alakategoriat: [{ id: "TBD101", hakulauseet: ["T119"] }, { id: "TBD102" }, { id: "TBD103" }],
-  },
-  {
-    id: "T2xx",
-    hakulauseet: ["T2[0-9]{2}"],
+    id: "osa_a",
+    hakulauseet: paaKategoriaKuvausRegexp(["T100", "Osa A"]),
     alakategoriat: [
-      { id: "TBD201" },
-      { id: "TBD202" },
-      { id: "TBD203" },
-      { id: "TBD204", hakulauseet: ["T216"] },
-      { id: "TBD205" },
-      { id: "TBD206" },
-      { id: "TBD207" },
-      { id: "TBD208" },
-      { id: "TBD209" },
-      { id: "TBD210" },
-      { id: "TBD211" },
-      { id: "TBD212" },
+      { id: "kaavakartat", hakulauseet: ["Kaavakartat", "Kaava", "1.7T", "T119"] },
+      {
+        id: "suunnitteluprosessiin_liittyva_aineisto",
+        hakulauseet: ["Suunnitteluprosessiin_liittyvä_aineisto", "1.6T", "Kuulutus", "Kutsu", "Esittelymateriaali"],
+      },
+      { id: "yva", hakulauseet: ["Ympäristövaikutusten_arviointi_(YVA)", "T120", "Yhteysviranomaisen perusteltu päätelmä", "YVA"] },
     ],
   },
   {
-    id: "T3xx",
-    hakulauseet: ["T3[0-9]{2}"],
+    id: "osa_b",
+    hakulauseet: paaKategoriaKuvausRegexp(["T200", "Osa B"]),
     alakategoriat: [
-      { id: "TBD301" },
-      { id: "TBD302" },
-      { id: "TBD303" },
-      { id: "TBD304" },
-      { id: "TBD305" },
-      { id: "TBD306", hakulauseet: ["T320"] },
-      { id: "TBD307" },
-      { id: "TBD308" },
-      { id: "TBD309" },
+      { id: "yleiskartat", hakulauseet: ["Yleiskartat", "T212", "Yleiskartta", "2.1T"] },
+      {
+        id: "hallinnollisten_jarjestelyiden_kartat",
+        hakulauseet: ["Hallinnollisten_järjestelyiden_kartat", "Hallinnollisten järjestelyiden kartat", "T213", "2.2T"],
+      },
+      { id: "suunnitelmakartat", hakulauseet: ["Suunnitelmakartat", "Suunnitelmakartta", "T214", "3T"] },
+      { id: "pituusleikkaukset", hakulauseet: ["Pituusleikkaukset", "Pituusleikkaus", "T216", "5T"] },
+      { id: "poikkileikkaukset", hakulauseet: ["Poikkileikkaukset", "Poikkileikkaus", "T215", "4T"] },
+      {
+        id: "siltasuunnitelmat_ja_muut_taitorakenteet",
+        hakulauseet: [
+          "Siltasuunnitelmat_ja_muut_taitorakenteet",
+          "Siltasuunnitelmat ja muut taitorakenteet",
+          "15T",
+          "Sillan yleispiirustus",
+        ],
+      },
+      { id: "ymparistorakenteet_esim_meluesteet", hakulauseet: ["Ympäristörakenteet_esim_meluesteet"] },
+      { id: "lunastuskartat", hakulauseet: ["Lunastuskartat"] },
+      { id: "liikennepaikkojen_suunnitelmat", hakulauseet: ["Liikennepaikkojen_suunnitelmat"] },
+      { id: "maanteiden_liittyvat_toimenpiteet_ratasuunnitelmassa", hakulauseet: ["Maanteiden_liittyvät_toimenpiteet_ratasuunnitelmassa"] },
+      { id: "rataan_liittyvat_toimenpiteet_tiesuunnitelmassa", hakulauseet: ["Rataan_liittyvät_toimenpiteet_tiesuunnitelmassa"] },
+      { id: "muut_piirustukset_ja_kuvat", hakulauseet: ["Muut_piirustukset_ja_kuvat"] },
+    ],
+  },
+  {
+    id: "osa_c",
+    hakulauseet: paaKategoriaKuvausRegexp(["T300", "Osa C"]),
+    alakategoriat: [
+      { id: "vaikutuksia_kuvaavat_selvitykset", hakulauseet: ["Vaikutuksia_kuvaavat_selvitykset"] },
+      { id: "ulkopuoliset_rakenteet", hakulauseet: ["Ulkopuoliset_rakenteet"] },
+      { id: "tutkitut_vaihtoehdot", hakulauseet: ["Tutkitut_vaihtoehdot"] },
+      { id: "katusuunnitelmat", hakulauseet: ["Katusuunnitelmat"] },
+      { id: "visualisointikuvat", hakulauseet: ["Visualisointikuvat"] },
+      { id: "ymparistosuunnitelmat", hakulauseet: ["Ympäristösuunnitelmat"] },
+      { id: "muut_selvitykset", hakulauseet: ["Muut_selvitykset"] },
+      { id: "valaistuksen_ja_liikenteenohjauksen_yleiskartat", hakulauseet: ["Valaistuksen_ja_liikenteenohjauksen_yleiskartat"] },
+      { id: "johtosiirrot_ja_kunnallistekniset_suunnitelmat", hakulauseet: ["Johtosiirrot_ja_kunnallistekniset_suunnitelmat"] },
     ],
   },
 ]);
+
+/**
+ *
+ * @param keywords hakusanat, joita haetaan projektivelhon kuvaus kentästä
+ * @returns RegExpit, joilla voidaan matchata joko keywordeilla alkava ja päättyvä merkkijono tai merkkijono, jossa keyword löytyy ja sen ympärillä on /-merkki
+ */
+function paaKategoriaKuvausRegexp(keywords: string[]): RegExp[] {
+  const slashPrefixRegexp = "^(.*/)?";
+  const slashSuffixRegexp = "(/.*)?$";
+  return keywords.map((keyword) => new RegExp(slashPrefixRegexp + keyword + slashSuffixRegexp, "i"));
+}
