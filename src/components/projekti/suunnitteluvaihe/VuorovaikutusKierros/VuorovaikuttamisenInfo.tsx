f8c529ab46fcb87cba7c201b1997d73336d29be8
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-01-17 11:54:40 +0200

    poistettu page layouttiin siirtyneet infot muualta

--- src/components/projekti/suunnitteluvaihe/VuorovaikutusKierros/VuorovaikuttamisenInfo.tsx
+++ src/components/projekti/suunnitteluvaihe/VuorovaikutusKierros/VuorovaikuttamisenInfo.tsx
@@ -1,18 +1,12 @@
 import React, { ReactElement } from "react";
-import Notification, { NotificationType } from "@components/notification/Notification";
-import { examineJulkaisuPaiva } from "src/util/dateUtils";
-import { VuorovaikutusKierros, VuorovaikutusKierrosTila } from "@services/api";
+import { VuorovaikutusKierros } from "@services/api";
 
 interface Props {
   vuorovaikutus: VuorovaikutusKierros | undefined;
   eiOleJulkaistu: boolean;
 }
 
-export default function VuorovaikuttamisenInfo({ vuorovaikutus, eiOleJulkaistu }: Props): ReactElement {
-  const julkinen = vuorovaikutus?.tila === VuorovaikutusKierrosTila.JULKINEN;
-  console.log("julkinen" + julkinen);
-  console.log("vuorovaikutus?.vuorovaikutusJulkaisuPaiva" + vuorovaikutus?.vuorovaikutusJulkaisuPaiva);
-  console.log("eiOleJulkaistu" + eiOleJulkaistu);
+export default function VuorovaikuttamisenInfo({ eiOleJulkaistu }: Props): ReactElement {
   if (eiOleJulkaistu) {
     return (
       <p className="mb-8">
@@ -22,23 +16,5 @@ export default function VuorovaikuttamisenInfo({ vuorovaikutus, eiOleJulkaistu }
     );
   }
 
-  let { julkaisuPaiva, published } = examineJulkaisuPaiva(julkinen, vuorovaikutus?.vuorovaikutusJulkaisuPaiva);
-  console.log("julkaisuPaiva" + julkaisuPaiva);
-  console.log("published" + published);
-
-
-  if (published) {
-    return <Notification type={NotificationType.INFO_GREEN}>Vuorovaikutus on julkaistu {julkaisuPaiva}.</Notification>;
-  }
-
-
-
-  return (
-    <Notification type={NotificationType.WARN}>
-      Vuorovaikutusta ei ole vielä julkaistu palvelun julkisella puolella. Julkaisu {julkaisuPaiva}.
-      {!vuorovaikutus?.esittelyaineistot?.length && !vuorovaikutus?.suunnitelmaluonnokset?.length
-        ? " Huomaathan, että suunnitelma-aineistot tulee vielä lisätä."
-        : ""}
-    </Notification>
-  );
+  return <></>;
 }
