0a5f0338aaa1294feb99b85c1e9c7cf087d312fc
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-01-20 15:31:17 +0200

    Tyypitä videot ja suunnittelumateriaali lokalisoiduiksi

--- src/schemas/suunnittelunPerustiedot.ts
+++ src/schemas/suunnittelunPerustiedot.ts
@@ -1,9 +1,6 @@
 import { Kieli, Kielitiedot } from "@services/api";
 import * as Yup from "yup";
-<<<<<<< HEAD
 import { ObjectShape } from "yup/lib/object";
-=======
->>>>>>> c3d0248f (virkamiespuolen FE-muutokset)
 import { lokalisoituTekstiEiPakollinen } from "./lokalisoituTeksti";
 import { paivamaara } from "./paivamaaraSchema";
 
