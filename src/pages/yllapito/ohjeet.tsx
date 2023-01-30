af78a4b2927a96f37519d5cb28b5f96e83c935e7
tomi korkalainen <tomi.korkalainen@cgi.com>
2023-01-30 14:24:50 +0200

    feat: Poista lorem ipsum Ohjeet-sivulta ja aseta linkki koulutusohjeisiin

--- src/pages/yllapito/ohjeet.tsx
+++ src/pages/yllapito/ohjeet.tsx
@@ -1,13 +1,26 @@
+import StyledLink from "@components/StyledLink";
+import { styled } from "@mui/material";
 import React, { ReactElement } from "react";
 
 export default function Ohjeet(): ReactElement {
   return (
     <section>
       <h1>Ohjeet</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto adipisci sequi quas! Cum, quidem, hic impedit
-        odio soluta fugit aliquid repellendus dicta quasi excepturi molestiae voluptatem est dolor, animi ipsum!
-      </p>
+      <LinkkiLista>
+        <li>
+          <StyledLink
+            download
+            href="https://extranet.vayla.fi/share/proxy/alfresco/slingshot/node/content/workspace/SpacesStore/67100eff-61f7-41bf-88e9-1e1d02a4b0b7/Valtion%20liikennev%c3%a4ylien%20suunnittelu%20K%c3%84YTT%c3%96OHJE.pptx?a=true"
+          >
+            Valtion liikenneväylien suunnittelu KÄYTTÖOHJE.pptx
+          </StyledLink>
+        </li>
+      </LinkkiLista>
     </section>
   );
 }
+
+const LinkkiLista = styled("ol")({
+  listStyleType: "disc",
+  listStylePosition: "inside",
+});
