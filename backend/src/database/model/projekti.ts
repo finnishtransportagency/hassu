84f650ceddfeab00547d178b6d8216e02d64da6c
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-02-03 13:16:01 +0200

    eurahoituslogot db + graphql

--- backend/src/database/model/projekti.ts
+++ backend/src/database/model/projekti.ts
@@ -66,6 +66,11 @@ export type SuunnitteluSopimus = {
   yhteysHenkilo: string;
 };
 
+export type EuRahoitusLogot = {
+  logoFI?: string;
+  logoSV?: string;
+};
+
 export type SuunnitteluSopimusJulkaisu = {
   kunta: number;
   logo?: string | null;
@@ -115,6 +120,7 @@ export type DBProjekti = {
   suunnittelustaVastaavaViranomainen?: Viranomainen | null;
   kielitiedot?: Kielitiedot | null;
   euRahoitus?: boolean | null;
+  euRahoitusLogot?: EuRahoitusLogot | null;
   vahainenMenettely?: boolean | null;
   aloitusKuulutus?: AloitusKuulutus | null;
   aloitusKuulutusJulkaisut?: AloitusKuulutusJulkaisu[] | null;
