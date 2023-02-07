cffca5a217c53bf13ee03b63e2f771db81521194
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-02-07 12:46:51 +0200

    Ajansiirtotoimintoihin aineistojen synkronointi mukaan

--- src/pages/api/test/[oid].dev.ts
+++ src/pages/api/test/[oid].dev.ts
@@ -7,6 +7,7 @@ import { DBProjekti } from "../../../../backend/src/database/model";
 import { testProjektiDatabase } from "../../../../backend/src/database/testProjektiDatabase";
 import { importProjekti, TargetStatuses } from "../../../../backend/src/migraatio/migration";
 import { NykyinenKayttaja } from "../../../../common/graphql/apiModel";
+import { aineistoSynchronizerService } from "backend/src/aineisto/aineistoSynchronizerService";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const dbProjekti = await authenticateAndLoadProjekti(req, res);
@@ -26,6 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         julkaisu.kuulutusVaihePaattyyPaiva = "2022-01-01";
         await projektiDatabase.nahtavillaoloVaiheJulkaisut.update(dbProjekti, julkaisu);
       }
+      await aineistoSynchronizerService.synchronizeProjektiFiles(dbProjekti.oid);
     }
   });
 
@@ -38,6 +40,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         julkaisu.kuulutusVaihePaattyyPaiva = yesterday;
         await projektiDatabase.hyvaksymisPaatosVaiheJulkaisut.update(dbProjekti, julkaisu);
       }
+      await aineistoSynchronizerService.synchronizeProjektiFiles(dbProjekti.oid);
     }
   });
 
@@ -50,6 +53,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         julkaisu.kuulutusVaihePaattyyPaiva = yearAgo;
         await projektiDatabase.hyvaksymisPaatosVaiheJulkaisut.update(dbProjekti, julkaisu);
       }
+      await aineistoSynchronizerService.synchronizeProjektiFiles(dbProjekti.oid);
     }
   });
 
