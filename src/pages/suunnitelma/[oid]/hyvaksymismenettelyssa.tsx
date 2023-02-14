7c538528dfaab4b6e9265900ebd1810a4c9dd46d
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-02-14 15:25:06 +0200

    näytä logo kansalaiselle

--- src/pages/suunnitelma/[oid]/hyvaksymismenettelyssa.tsx
+++ src/pages/suunnitelma/[oid]/hyvaksymismenettelyssa.tsx
@@ -1,12 +1,16 @@
 import React, { ReactElement } from "react";
 import ProjektiJulkinenPageLayout from "@components/projekti/kansalaisnakyma/ProjektiJulkinenPageLayout";
 import useTranslation from "next-translate/useTranslation";
+import EuLogo from "@components/projekti/common/EuLogo";
+import { useProjektiJulkinen } from "../../../hooks/useProjektiJulkinen";
 
 export default function Hyvaksymismenettelyssa(): ReactElement {
   const { t } = useTranslation("hyvaksymismenettelyssa");
+  const { data: projekti } = useProjektiJulkinen();
   return (
     <ProjektiJulkinenPageLayout selectedStep={3} title={t("suunnitelma_on_siirtynyt")}>
       <p>{t("nahtavilla_olon_jalkeen")}</p>
+      <EuLogo projekti={projekti} />
     </ProjektiJulkinenPageLayout>
   );
 }
