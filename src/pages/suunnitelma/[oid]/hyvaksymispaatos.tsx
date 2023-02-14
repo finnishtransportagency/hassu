5c910fed96cbf570e59978d24379b48eebf993d2
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-02-14 15:25:06 +0200

    näytä logo kansalaiselle

--- src/pages/suunnitelma/[oid]/hyvaksymispaatos.tsx
+++ src/pages/suunnitelma/[oid]/hyvaksymispaatos.tsx
@@ -3,6 +3,7 @@ import { useProjektiJulkinen } from "src/hooks/useProjektiJulkinen";
 import HyvaksymispaatosTiedot from "@components/projekti/kansalaisnakyma/HyvaksymispaatosTiedot";
 import PaatosPageLayout from "@components/projekti/kansalaisnakyma/PaatosPageLayout";
 import useTranslation from "next-translate/useTranslation";
+import EuLogo from "@components/projekti/common/EuLogo";
 
 export default function Hyvaksymispaatos(): ReactElement {
   const { t } = useTranslation("projekti");
@@ -16,6 +17,7 @@ export default function Hyvaksymispaatos(): ReactElement {
   return (
     <PaatosPageLayout pageTitle={t("ui-otsikot.kuulutus_suunnitelman_hyvaksymisesta")}>
       <HyvaksymispaatosTiedot kuulutus={projekti.hyvaksymisPaatosVaihe} />
+      <EuLogo projekti={projekti} />
     </PaatosPageLayout>
   );
 }
