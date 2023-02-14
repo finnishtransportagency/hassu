5b55f0b87a7109e390fbe65479d4b4262fdc34d0
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-02-14 14:07:12 +0200

    rebase merge

--- src/pages/suunnitelma/[oid]/nahtavillaolo.tsx
+++ src/pages/suunnitelma/[oid]/nahtavillaolo.tsx
@@ -14,6 +14,7 @@ import KansalaisenAineistoNakyma from "@components/projekti/common/KansalaisenAi
 import useKansalaiskieli from "src/hooks/useKansalaiskieli";
 import { kuntametadata } from "../../../../common/kuntametadata";
 import { yhteystietoKansalaiselleTekstiksi } from "src/util/kayttajaTransformationUtil";
+import EuLogo from "@components/projekti/common/EuLogo";
 import { renderTextAsHTML } from "../../../util/renderTextAsHTML";
 import { splitFilePath } from "src/util/fileUtil";
 import ExtLink from "@components/ExtLink";
@@ -138,6 +139,7 @@ export default function Nahtavillaolo(): ReactElement {
           </SectionContent>
         </Section>
       </Section>
+      <EuLogo projekti={projekti} />
     </ProjektiJulkinenPageLayout>
   );
 }
