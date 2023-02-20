f182d1897dca0e2cbaf9c43f7880619279cf39b7
tomi korkalainen <tomi.korkalainen@cgi.com>
2023-02-20 13:44:46 +0200

    paranna kuulutuksessa olevien linkkien tyylejä

--- src/util/renderTextAsHTML.tsx
+++ src/util/renderTextAsHTML.tsx
@@ -1,15 +1,22 @@
 import htmlReactParser, { DOMNode, domToReact, Element, HTMLReactParserOptions } from "html-react-parser";
-import ExtLink from "@components/ExtLink";
-import { Link } from "@mui/material";
 import React from "react";
+import StyledLink, { ExternalStyledLink } from "@components/StyledLink";
 
 let options: HTMLReactParserOptions = {
   replace: (domNode: DOMNode) => {
     if (domNode instanceof Element && domNode.attribs && domNode.name == "a") {
       if (domNode.attribs.external) {
-        return <ExtLink href={domNode.attribs.href || ""}>{domToReact(domNode.children, options)}</ExtLink>;
+        return (
+          <ExternalStyledLink sx={{ fontWeight: 400 }} href={domNode.attribs.href || ""}>
+            {domToReact(domNode.children, options)}
+          </ExternalStyledLink>
+        );
       } else {
-        return <Link href={domNode.attribs.href || ""}>{domToReact(domNode.children, options)}</Link>;
+        return (
+          <StyledLink sx={{ fontWeight: 400 }} href={domNode.attribs.href || ""}>
+            {domToReact(domNode.children, options)}
+          </StyledLink>
+        );
       }
     }
   },
