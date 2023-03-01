3eeb8e0156f8dc1b8c1027ef8f3e8687ce3a98a9
mustonenk <kalle.mustonen@cgi.com>
2023-03-01 15:44:29 +0200

    fix: aloituskuulutusilmoitus(#655)
    * rebase merge
    
    * aloituskuulutusilmoitus meiliin liitteeksi myös ei-suomi pdf jos on
    
    * update intgration snaps
    
    * tekstit kielifileihin
    
    * kaksi kieltä meilissä
    
    * kaksi kieltä meilissä
    
    * kaksi kieltä meilissä - ruotsi
    
    * testi fix; ala vastaa oikea asianhallinta
    
    * update snap
    
    * update intgration snaps
    
    * snap up
    
    * Päivitä snapshotit
    
    * Päivitä snapshotit
    
    ---------
    
    Co-authored-by: Valhe Kouneli <valhe.kouneli@cgi.com>

--- backend/src/email/lahetekirje/lahetekirjeEmailTemplate.ts
+++ backend/src/email/lahetekirje/lahetekirjeEmailTemplate.ts
@@ -1,15 +1,16 @@
 import { EmailOptions } from "../email";
 import { AloituskuulutusKutsuAdapter } from "../../asiakirja/adapter/aloituskuulutusKutsuAdapter";
+import { projektiPaallikkoJaVarahenkilotEmails } from "../emailTemplates";
+import { Kieli } from "../../../../common/graphql/apiModel";
 
 const lahetekirje11 = (adapter: AloituskuulutusKutsuAdapter) => {
   const paragraphs = [
+    adapter.text("asiakirja.ala_vastaa"),
     adapter.nimi,
     adapter.uudelleenKuulutusSeloste,
-    "{{kuuluttaja_pitka}} ilmoittaa, että se julkaisee kuulutuksen, joka koskee otsikossa mainitun {{suunnitelman}} " +
-      "ja maastotöiden aloittamista. {{kuuluttaja}} saattaa asian tiedoksi julkisesti kuuluttamalla vähintään 30 päivän ajan siten, kuin julkisesta kuulutuksesta säädetään hallintolaissa, sekä julkaisemalla kuulutuksen vähintään " +
-      "yhdessä alueella yleisesti ilmestyvässä sanomalehdessä. ({{lakiviite_kunnan_ilmoitus}})",
-    "Kuulutus julkaistaan {{kuulutusPaiva}} osoitteessa {{aloituskuulutusUrl}}.",
-    "Suunnittelualueen kuntia pyydetään julkaisemaan liitteenä oleva ilmoitus kuulutuksesta verkkosivuillaan. Ilmoitus tulee julkaista mahdollisuuksien mukaan edellä mainittuna kuulutuksen julkaisupäivänä. Ilmoituksen nähtävillä oloa ei tarvitse todentaa ilmoituksen lähettäjälle. Ilmoitus menee ELY-keskuksen ja Väyläviraston kuulutussivulle automaattisesti Valtion liikenneväylien suunnittelu palvelun kautta.",
+    adapter.text("asiakirja.aloituskuulutus_lahete_email.kappale1"),
+    adapter.text("asiakirja.aloituskuulutus_lahete_email.kappale2"),
+    adapter.text("asiakirja.aloituskuulutus_lahete_email.kappale3"),
     adapter.hankkeenKuvaus(),
     adapter.text("asiakirja.tietosuoja"),
     adapter.text("asiakirja.lisatietoja_antavat"),
@@ -21,11 +22,25 @@ const lahetekirje11 = (adapter: AloituskuulutusKutsuAdapter) => {
 };
 
 export function createLahetekirjeEmail(adapter: AloituskuulutusKutsuAdapter): EmailOptions {
-  const subject = adapter.substituteText("{{kuuluttaja_pitka}} kuulutuksesta ilmoittaminen");
-  const text = lahetekirje11(adapter);
+  const ruotsiProps = adapter.props;
+  ruotsiProps.kieli = Kieli.RUOTSI;
+  const ruotsiAdapter = new AloituskuulutusKutsuAdapter(ruotsiProps);
+
+  let text2 = "";
+  let text = lahetekirje11(adapter);
+  if (adapter.kielitiedot.ensisijainenKieli === Kieli.RUOTSI) {
+    text2 = text;
+    text = lahetekirje11(ruotsiAdapter);
+  } else if (adapter.kielitiedot.toissijainenKieli === Kieli.RUOTSI) {
+    text2 = lahetekirje11(ruotsiAdapter);
+  }
+  text += "\n\n" + text2;
+
+  const subject = adapter.text("asiakirja.aloituskuulutus_lahete_email.otsikko");
   return {
     subject,
     text,
     to: adapter.laheteKirjeVastaanottajat,
+    cc: adapter.kayttoOikeudet && projektiPaallikkoJaVarahenkilotEmails(adapter.kayttoOikeudet),
   };
 }
