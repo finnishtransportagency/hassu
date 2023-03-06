55f27eb3cc9e75a236653a7cf192708d3a88115d
kettuncgi <48211670+kettunju@users.noreply.github.com>
2023-03-06 10:14:18 +0200

    feat: HASSU-1523 kansalaisen nahtavilläolo saavutettavuus ja design (#646)
    * otsikot, valit, infolaatikot ok, kansalaisen aineistonakyman saavutettavuus puuttuu
    
    * ruotsinkieliset muistutustekstit

--- src/pages/suunnitelma/[oid]/nahtavillaolo.tsx
+++ src/pages/suunnitelma/[oid]/nahtavillaolo.tsx
@@ -13,12 +13,14 @@ import MuistutusLomakeDialogi from "@components/projekti/kansalaisnakyma/Muistut
 import KansalaisenAineistoNakyma from "@components/projekti/common/KansalaisenAineistoNakyma";
 import useKansalaiskieli from "src/hooks/useKansalaiskieli";
 import { kuntametadata } from "../../../../common/kuntametadata";
-import { yhteystietoKansalaiselleTekstiksi } from "src/util/kayttajaTransformationUtil";
 import EuLogo from "@components/projekti/common/EuLogo";
 import { renderTextAsHTML } from "../../../util/renderTextAsHTML";
 import { splitFilePath } from "src/util/fileUtil";
 import ExtLink from "@components/ExtLink";
 import FormatDate from "@components/FormatDate";
+import { Yhteystietokortti } from "./suunnittelu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import HassuStack from "@components/layout/HassuStack";
 
 export default function Nahtavillaolo(): ReactElement {
   const { t, lang } = useTranslation("projekti");
@@ -61,7 +63,7 @@ export default function Nahtavillaolo(): ReactElement {
   const nahtavillaoloKuulutusPDFPath = splitFilePath(kuulutus.kuulutusPDF?.[kieli] || undefined);
 
   return migroitu ? (
-    <ProjektiJulkinenPageLayout selectedStep={2} title="Kuulutus suunnitelman nähtäville asettamisesta">
+    <ProjektiJulkinenPageLayout selectedStep={2} title={t("asiakirja.kuulutus_nahtavillaolosta.otsikko")}>
       <>
         <Section noDivider>
           <p>{t("projekti:suunnitelma_on_tuotu_toisesta_jarjestelmasta")}</p>
@@ -69,24 +71,28 @@ export default function Nahtavillaolo(): ReactElement {
       </>
     </ProjektiJulkinenPageLayout>
   ) : (
-    <ProjektiJulkinenPageLayout selectedStep={2} title="Kuulutus suunnitelman nähtäville asettamisesta">
-      <Section noDivider>
-        <KeyValueTable rows={keyValueData}></KeyValueTable>
+    <ProjektiJulkinenPageLayout selectedStep={2} title={t("asiakirja.kuulutus_nahtavillaolosta.otsikko")}>
+      <Section noDivider className="mt-8">
+        <KeyValueTable rows={keyValueData} kansalaisnakyma={true}></KeyValueTable>
         {kuulutus.uudelleenKuulutus?.selosteKuulutukselle?.[kieli] && <p>{kuulutus.uudelleenKuulutus.selosteKuulutukselle[kieli]}</p>}
         <SectionContent>
           {kuulutusTekstit?.leipaTekstit?.map((teksti) => (
             <p key={pKey++}>{renderTextAsHTML(teksti)}</p>
           ))}
+          {kuulutusTekstit?.kuvausTekstit?.map((teksti) => (
+            <p key={pKey++}>{renderTextAsHTML(teksti)}</p>
+          ))}
         </SectionContent>
-        <h4 className="vayla-small-title">{t(`ui-otsikot.nahtavillaolo.suunnitteluhankkeen_kuvaus`)}</h4>
-        <SectionContent>{kuulutus.hankkeenKuvaus?.[kieli]}</SectionContent>
-        {kuulutusTekstit?.kuvausTekstit?.map((teksti) => (
-          <p key={pKey++}>{renderTextAsHTML(teksti)}</p>
-        ))}
-        <h4 className="vayla-small-title">{t(`ui-otsikot.nahtavillaolo.asianosaisen_oikeudet`)}</h4>
+
         <SectionContent>
-          <Notification type={NotificationType.INFO} hideIcon>
-            <SectionContent sx={{ padding: "1rem 1rem" }}>
+          <h3 className="vayla-subtitle">{t(`ui-otsikot.nahtavillaolo.suunnitteluhankkeen_kuvaus`)}</h3>
+          <p>{kuulutus.hankkeenKuvaus?.[kieli]}</p>
+        </SectionContent>
+
+        <SectionContent className="mt-8">
+          <h3 className="vayla-subtitle">{t(`ui-otsikot.nahtavillaolo.asianosaisen_oikeudet`)}</h3>
+          <Notification type={NotificationType.INFO} hideIcon className="mt-6">
+            <SectionContent sx={{ padding: "1rem 1rem", fontSize: "1rem" }}>
               <ul>
                 {kuulutusTekstit?.infoTekstit?.map((teksti) => (
                   <li key={pKey++}>{renderTextAsHTML(teksti)}</li>
@@ -94,6 +100,9 @@ export default function Nahtavillaolo(): ReactElement {
               </ul>
             </SectionContent>
           </Notification>
+          <SectionContent className="mt-8">
+            <p>{renderTextAsHTML(kuulutusTekstit?.tietosuoja)}</p>
+          </SectionContent>
         </SectionContent>
         <KansalaisenAineistoNakyma
           projekti={projekti}
@@ -101,9 +110,23 @@ export default function Nahtavillaolo(): ReactElement {
           uudelleenKuulutus={projekti.nahtavillaoloVaihe?.uudelleenKuulutus}
         />
         {isProjektiInNahtavillaoloVaihe && (
-          <>
-            <h4 className="vayla-small-title">{t(`ui-otsikot.nahtavillaolo.muistutuksen_jattaminen`)}</h4>
+          <Section noDivider className="mt-10 mb-10">
             <SectionContent>
+              <HassuStack direction={"row"} alignItems="baseline" columnGap="1rem">
+                <h2 className="vayla-title">{t(`ui-otsikot.nahtavillaolo.muistutuksen_jattaminen`)}</h2>
+                <FontAwesomeIcon color="rgb(0, 100, 175)" size="lg" icon="info-circle" type={NotificationType.INFO_GRAY} />
+              </HassuStack>
+              <p className="mt-0">
+                <strong>{t("muistutuslomake.jata_muistutus_mennessa", { pvm: formatDate(kuulutus.muistutusoikeusPaattyyPaiva) })}</strong>
+              </p>
+              <Notification type={NotificationType.INFO_GRAY} className="mt-4" closable={true}>
+                <SectionContent sx={{ fontSize: "1rem" }}>
+                  <p>{t("muistutuslomake.muistutus_info_1")}</p>
+                  <p>{t("muistutuslomake.muistutus_info_2")}</p>
+                </SectionContent>
+              </Notification>
+            </SectionContent>
+            <SectionContent className="mt-10 mb-10">
               <JataPalautettaNappi teksti={t("muistutuslomake.jata_muistutus")} onClick={() => setMuistutusLomakeOpen(true)} />
               <MuistutusLomakeDialogi
                 nahtavillaolo={kuulutus}
@@ -112,32 +135,32 @@ export default function Nahtavillaolo(): ReactElement {
                 projekti={projekti}
               />
             </SectionContent>
-          </>
+          </Section>
         )}
+
         <SectionContent>
-          <p>{renderTextAsHTML(kuulutusTekstit?.tietosuoja)}</p>
+          <h2 className="vayla-title">{t("projekti:ui-otsikot.ladattava_kuulutus")}</h2>
+          <p>
+            <ExtLink className="file_download" href={nahtavillaoloKuulutusPDFPath.path} style={{ marginRight: "0.5rem" }}>
+              {nahtavillaoloKuulutusPDFPath.fileName}
+            </ExtLink>{" "}
+            ({nahtavillaoloKuulutusPDFPath.fileExt}) (
+            <FormatDate date={kuulutus.kuulutusPaiva} />)
+          </p>
         </SectionContent>
-        <h4 className="vayla-small-title">{t(`ui-otsikot.nahtavillaolo.yhteystiedot`)}</h4>
-        <SectionContent>
+
+        <SectionContent className="mt-8">
+          <h2 className="vayla-title">{t(`ui-otsikot.nahtavillaolo.yhteystiedot`)}</h2>
           <p>
             {t("common:lisatietoja_antavat", {
               count: kuulutus.yhteystiedot.length,
             })}
           </p>
           {kuulutus.yhteystiedot.map((yhteystieto, index) => (
-            <p key={index}>{yhteystietoKansalaiselleTekstiksi(lang, yhteystieto, t)}</p>
+            <Yhteystietokortti key={index} yhteystieto={yhteystieto} />
           ))}
         </SectionContent>
-        <Section noDivider>
-          <h5 className="vayla-smallest-title">{t("projekti:ui-otsikot.ladattava_kuulutus")}</h5>
-          <SectionContent className="flex gap-4">
-            <ExtLink className="file_download" href={nahtavillaoloKuulutusPDFPath.path}>
-              {nahtavillaoloKuulutusPDFPath.fileName}
-            </ExtLink>{" "}
-            ({nahtavillaoloKuulutusPDFPath.fileExt}) (
-            <FormatDate date={kuulutus.kuulutusPaiva} />)
-          </SectionContent>
-        </Section>
+        <Section noDivider></Section>
       </Section>
       <EuLogo projekti={projekti} />
     </ProjektiJulkinenPageLayout>
