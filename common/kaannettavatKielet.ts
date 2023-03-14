105b04450794abae9fa89e6f5dc7918ffb8956e6
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-03-14 15:07:12 +0200

    Lisää funktio kieliIsTranslatable

--- common/kaannettavatKielet.ts
+++ common/kaannettavatKielet.ts
@@ -1,3 +1,7 @@
 import { Kieli } from "./graphql/apiModel";
 
 export type KaannettavaKieli = Kieli.SUOMI | Kieli.RUOTSI;
+
+export function kieliIsTranslatable(kieli: Kieli): boolean {
+  return [Kieli.SUOMI, Kieli.RUOTSI].includes(kieli);
+}
