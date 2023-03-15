34b336e5b75fed0b55dfca8d8dc039bb9cbe08b8
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-03-15 12:08:49 +0200

    Nimeä apufunkkari paremmin ja laita se ottaa sisään myös null ja undefined

--- common/kaannettavatKielet.ts
+++ common/kaannettavatKielet.ts
@@ -2,6 +2,7 @@ import { Kieli } from "./graphql/apiModel";
 
 export type KaannettavaKieli = Kieli.SUOMI | Kieli.RUOTSI;
 
-export function kieliIsTranslatable(kieli: Kieli): boolean {
+export function isKieliTranslatable(kieli: Kieli | undefined | null): boolean {
+  if (!kieli) return false;
   return [Kieli.SUOMI, Kieli.RUOTSI].includes(kieli);
 }
