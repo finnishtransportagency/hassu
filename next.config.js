7fd2316230273469c39ceeb0c3c4372bb5f4bf82
Mikko Haapamäki <haapamakim@users.noreply.github.com>
2023-03-15 16:36:10 +0200

    fix: korjaa lyhytosoitteet (#696)

--- next.config.js
+++ next.config.js
@@ -16,6 +16,13 @@ const lyhytOsoiteRedirects = [
     locale: false,
   },
   {
+    source: "/fi/s/:path*",
+    destination: "/api/s/:path*",
+    permanent: true,
+    basePath: false,
+    locale: false,
+  },
+  {
     source: "/sv/s/:path*",
     destination: "/api/sv/s/:path*",
     permanent: true,
@@ -110,6 +117,7 @@ module.exports = (phase) => {
     INFRA_ENVIRONMENT: BaseConfig.infraEnvironment,
     ENVIRONMENT: BaseConfig.env,
     TABLE_PROJEKTI: BaseConfig.projektiTableName,
+    TABLE_LYHYTOSOITE: BaseConfig.lyhytOsoiteTableName,
     INTERNAL_BUCKET_NAME: BaseConfig.internalBucketName,
     AINEISTO_IMPORT_SQS_URL: process.env.AINEISTO_IMPORT_SQS_URL,
   };
