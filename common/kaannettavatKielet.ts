35375b9f5d33c7afbb444aabc884256029e133e6
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-03-15 13:00:14 +0200

    luo getKaannettavatKielet funktio

--- common/kaannettavatKielet.ts
+++ common/kaannettavatKielet.ts
@@ -1,8 +1,22 @@
-import { Kieli } from "./graphql/apiModel";
+import * as API from "./graphql/apiModel";
+import { Kielitiedot } from "./graphql/apiModel";
 
-export type KaannettavaKieli = Kieli.SUOMI | Kieli.RUOTSI;
+export type KaannettavaKieli = API.Kieli.SUOMI | API.Kieli.RUOTSI;
 
-export function isKieliTranslatable(kieli: Kieli | undefined | null): boolean {
+export function isKieliTranslatable(kieli: API.Kieli | undefined | null): boolean {
   if (!kieli) return false;
-  return [Kieli.SUOMI, Kieli.RUOTSI].includes(kieli);
+  return [API.Kieli.SUOMI, API.Kieli.RUOTSI].includes(kieli);
+}
+
+export function getKaannettavatKielet(kielitiedot: API.Kielitiedot | Kielitiedot | undefined | null): {
+  ensisijainenKaannettavaKieli: KaannettavaKieli | null;
+  toissijainenKaannettavaKieli: KaannettavaKieli | null;
+} {
+  if (!kielitiedot) {
+    return { ensisijainenKaannettavaKieli: null, toissijainenKaannettavaKieli: null };
+  }
+  const { ensisijainenKieli, toissijainenKieli } = kielitiedot;
+  const ensisijainenKaannettavaKieli = isKieliTranslatable(ensisijainenKieli) ? (ensisijainenKieli as KaannettavaKieli) : null;
+  const toissijainenKaannettavaKieli = isKieliTranslatable(toissijainenKieli) ? (toissijainenKieli as KaannettavaKieli) : null;
+  return { ensisijainenKaannettavaKieli, toissijainenKaannettavaKieli };
 }
