6166edcbaea989fa14b554febe66ac0eed8f2c2a
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-03-27 14:33:41 +0300

    Älä näytä uudelleenkuulutuksen selitteissä saamelomakkeita

--- src/components/projekti/SelitteetUudelleenkuulutukselle.tsx
+++ src/components/projekti/SelitteetUudelleenkuulutukselle.tsx
@@ -2,6 +2,7 @@ import Textarea from "@components/form/Textarea";
 import Section from "@components/layout/Section";
 import SectionContent from "@components/layout/SectionContent";
 import { Kielitiedot, TallennaProjektiInput, UudelleenKuulutus, UudelleenkuulutusTila } from "@services/api";
+import { getKaannettavatKielet } from "common/kaannettavatKielet";
 import lowerCase from "lodash/lowerCase";
 import React from "react";
 import { useFormContext } from "react-hook-form";
@@ -20,7 +21,7 @@ export default function SelitteetUudelleenkuulutukselle({ uudelleenKuulutus, kie
     register,
     formState: { errors },
   } = useFormContext();
-  const { ensisijainenKieli, toissijainenKieli } = kielitiedot || {};
+  const { ensisijainenKaannettavaKieli, toissijainenKaannettavaKieli } = getKaannettavatKielet(kielitiedot) || {};
   return (
     <>
       {uudelleenKuulutus && (
@@ -33,19 +34,19 @@ export default function SelitteetUudelleenkuulutukselle({ uudelleenKuulutus, kie
                 Kirjoita kuulutusta varten seloste uudelleenkuuluttamisen syistä. Seloste tulee nähtäville palvelun julkiselle puolelle sekä
                 kuulutuksen pdf-tiedostoon. Älä lisää tekstiin linkkejä.
               </p>
-              {ensisijainenKieli && (
+              {ensisijainenKaannettavaKieli && (
                 <Textarea
-                  label={`Suunnitelman uudelleenkuuluttamisen syy ensisijaisella kielellä (${lowerCase(ensisijainenKieli)}) *`}
-                  {...register(`${vaiheenAvain}.uudelleenKuulutus.selosteKuulutukselle.${ensisijainenKieli}`)}
-                  error={(errors[vaiheenAvain]?.uudelleenKuulutus as any)?.selosteKuulutukselle?.[ensisijainenKieli]}
+                  label={`Suunnitelman uudelleenkuuluttamisen syy ensisijaisella kielellä (${lowerCase(ensisijainenKaannettavaKieli)}) *`}
+                  {...register(`${vaiheenAvain}.uudelleenKuulutus.selosteKuulutukselle.${ensisijainenKaannettavaKieli}`)}
+                  error={(errors[vaiheenAvain]?.uudelleenKuulutus as any)?.selosteKuulutukselle?.[ensisijainenKaannettavaKieli]}
                   disabled={disabled}
                 />
               )}
-              {toissijainenKieli && (
+              {toissijainenKaannettavaKieli && (
                 <Textarea
-                  label={`Suunnitelman uudelleenkuuluttamisen syy toissijaisella kielellä (${lowerCase(toissijainenKieli)}) *`}
-                  {...register(`${vaiheenAvain}.uudelleenKuulutus.selosteKuulutukselle.${toissijainenKieli}`)}
-                  error={(errors[vaiheenAvain]?.uudelleenKuulutus as any)?.selosteKuulutukselle?.[toissijainenKieli]}
+                  label={`Suunnitelman uudelleenkuuluttamisen syy toissijaisella kielellä (${lowerCase(toissijainenKaannettavaKieli)}) *`}
+                  {...register(`${vaiheenAvain}.uudelleenKuulutus.selosteKuulutukselle.${toissijainenKaannettavaKieli}`)}
+                  error={(errors[vaiheenAvain]?.uudelleenKuulutus as any)?.selosteKuulutukselle?.[toissijainenKaannettavaKieli]}
                   disabled={disabled}
                 />
               )}
@@ -57,19 +58,19 @@ export default function SelitteetUudelleenkuulutukselle({ uudelleenKuulutus, kie
               Kirjoita lähetekirjettä varten seloste uudelleenkuuluttamisen syistä. Seloste tulee nähtäville viranomaiselle ja kunnille
               lähetettävän lähetekirjeen alkuun. Älä lisää tekstiin linkkejä.
             </p>
-            {ensisijainenKieli && (
+            {ensisijainenKaannettavaKieli && (
               <Textarea
-                label={`Suunnitelman uudelleenkuuluttamisen syy ensisijaisella kielellä (${lowerCase(ensisijainenKieli)}) *`}
-                {...register(`${vaiheenAvain}.uudelleenKuulutus.selosteLahetekirjeeseen.${ensisijainenKieli}`)}
-                error={(errors[vaiheenAvain]?.uudelleenKuulutus as any)?.selosteLahetekirjeeseen?.[ensisijainenKieli]}
+                label={`Suunnitelman uudelleenkuuluttamisen syy ensisijaisella kielellä (${lowerCase(ensisijainenKaannettavaKieli)}) *`}
+                {...register(`${vaiheenAvain}.uudelleenKuulutus.selosteLahetekirjeeseen.${ensisijainenKaannettavaKieli}`)}
+                error={(errors[vaiheenAvain]?.uudelleenKuulutus as any)?.selosteLahetekirjeeseen?.[ensisijainenKaannettavaKieli]}
                 disabled={disabled}
               />
             )}
-            {toissijainenKieli && (
+            {toissijainenKaannettavaKieli && (
               <Textarea
-                label={`Suunnitelman uudelleenkuuluttamisen syy toissijaisella kielellä (${lowerCase(toissijainenKieli)}) *`}
-                {...register(`${vaiheenAvain}.uudelleenKuulutus.selosteLahetekirjeeseen.${toissijainenKieli}`)}
-                error={(errors[vaiheenAvain]?.uudelleenKuulutus as any)?.selosteLahetekirjeeseen?.[toissijainenKieli]}
+                label={`Suunnitelman uudelleenkuuluttamisen syy toissijaisella kielellä (${lowerCase(toissijainenKaannettavaKieli)}) *`}
+                {...register(`${vaiheenAvain}.uudelleenKuulutus.selosteLahetekirjeeseen.${toissijainenKaannettavaKieli}`)}
+                error={(errors[vaiheenAvain]?.uudelleenKuulutus as any)?.selosteLahetekirjeeseen?.[toissijainenKaannettavaKieli]}
                 disabled={disabled}
               />
             )}
