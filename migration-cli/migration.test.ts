3751d4e58843b41995904381de2ba55955435593
kettunenj <juhani.kettunen@cgi.com>
2023-04-05 15:39:23 +0300

    korjaa migraatiotesti ja lisaa testeja

--- migration-cli/migration.test.ts
+++ migration-cli/migration.test.ts
@@ -7,13 +7,34 @@ dotenv.config({ path: ".env.local" });
 
 import { recordProjektiTestFixture } from "../backend/integrationtest/api/testFixtureRecorder";
 import { importProjektis } from "./migration-cli";
+import * as sinon from "sinon";
 
 describe("Migraatio", () => {
-  it("ajetaan migraatio testiprojekteille", async () => {
-    const importedProjektis = await importProjektis(__dirname + "/migraatiotesti.xlsx", argv.sheetNum, argv.overwrite);
+  beforeEach(async () => sinon.restore());
+
+  it("ajetaan migraatio ensimmaisen valilehden testiprojekteille", async () => {
+    const importedProjektis = await importProjektis(__dirname + "/migraatiotesti.xlsx", 1, true);
     for (const oid in importedProjektis) {
       const targetStatus = importedProjektis[oid];
       await recordProjektiTestFixture("migraatio_" + targetStatus, oid);
     }
+  });
+
+  it("ajetaan migraatio toisen valilehden testiprojekteille", async () => {
+    const importedProjektis = await importProjektis(__dirname + "/migraatiotesti.xlsx", 2, true);
+    for (const oid in importedProjektis) {
+      const targetStatus = importedProjektis[oid];
+      await recordProjektiTestFixture("migraatio_" + targetStatus, oid);
+    }
+  });
+
+  // it("ajetaan migraatio valilehdelle, jota ei ole olemassa", async () => {
+  //   const processStub = sinon.stub(process, "exit");
+  //   await importProjektis(__dirname + "/migraatiotesti.xlsx", 10, true);
+  //   assert(processStub.calledWith(1));
+  // });
+
+  it("ajetaan migraatio olemassa oleville projekteille paallekirjoituksen ollessa estetty", async () => {
+    await importProjektis(__dirname + "/migraatiotesti.xlsx", 1, false);
   });
 });
