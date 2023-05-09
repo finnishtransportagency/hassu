a4947bae0a93a014af574eb2a2145443df53c8bb
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-05-09 13:39:28 +0300

    Ota pdf-generoinnissa huomioon, että nyt muissakin tilaisuuksissa voi olla lisätieto.

--- backend/src/asiakirja/suunnittelunAloitus/Kutsu20.ts
+++ backend/src/asiakirja/suunnittelunAloitus/Kutsu20.ts
@@ -161,6 +161,18 @@ export class Kutsu20 extends CommonPdf<SuunnitteluVaiheKutsuAdapter> {
                   underline: true,
                 });
                 this.doc.fillColor("black").text("", { baseline, link: undefined, underline: false, continued: false }).moveDown();
+
+                if (tilaisuus.lisatiedot && tilaisuus.lisatiedot[this.kieli] !== undefined) {
+                  this.doc.fillColor("black").font("ArialMTBold");
+                  this.doc.text(this.kutsuAdapter.text(ASIAKIRJA_KUTSU_PREFIX + "lisatiedot") + ": ", {
+                    baseline,
+                    continued: true,
+                    underline: false,
+                  });
+                  this.doc.font("ArialMT");
+                  this.doc.text((tilaisuus.lisatiedot[this.kieli] as string) + "\n\n");
+                  this.doc.fillColor("black").text("", { baseline, link: undefined, underline: false, continued: false }).moveDown();
+                }
               }),
             ]);
           } else if (tilaisuus.tyyppi == VuorovaikutusTilaisuusTyyppi.PAIKALLA) {
@@ -267,6 +279,15 @@ export class Kutsu20 extends CommonPdf<SuunnitteluVaiheKutsuAdapter> {
                 }
                 this.doc.text(`${etunimi} ${sukunimi}${organisaatio},\n${this.kutsuAdapter.localizedPuh} ${puhelinnumero}`, { baseline });
               });
+            }
+            if (tilaisuus.lisatiedot && tilaisuus.lisatiedot[this.kieli] !== undefined) {
+              this.doc.fillColor("black").font("ArialMTBold");
+              this.doc.text(this.kutsuAdapter.text(ASIAKIRJA_KUTSU_PREFIX + "lisatiedot") + ": ", {
+                baseline,
+                continued: true,
+              });
+              this.doc.font("ArialMT");
+              this.doc.text((tilaisuus.lisatiedot[this.kieli] as string) + "\n\n");
             }
             this.doc.moveDown();
           });
