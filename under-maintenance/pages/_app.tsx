4bb8886b513285d7ff7eaa5d67e5ca7bebc80eff
Tomi Korkalainen <77731851+tkork@users.noreply.github.com>
2023-05-15 10:31:15 +0300

    fix: HASSU-1738 Muokkaa huoltokatkosivun Väylä.fi ulkoseksi linkiksi (lisää ikoni ja avaa uuteen välilehteen) (#811)

--- under-maintenance/pages/_app.tsx
+++ under-maintenance/pages/_app.tsx
@@ -3,15 +3,16 @@ import Head from "next/head";
 
 import log from "loglevel";
 import { AppProps } from "next/app";
-import { useRouter } from "next/dist/client/router";
 import React from "react";
 import HassuMuiThemeProvider from "../../src/components/layout/HassuMuiThemeProvider";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+library.add(faExternalLinkAlt);
 
 log.setDefaultLevel("DEBUG");
 
 function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
   return (
     <>
       <Head>
