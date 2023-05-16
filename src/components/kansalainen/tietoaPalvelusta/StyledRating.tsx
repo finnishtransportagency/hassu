7d62d3831980dd7690125f70666a1129f7e73bdd
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-05-16 13:58:39 +0300

    Mobiilissa slideri tekee hienon animaation kun lomake avataan, jos slideri on nollassa.

--- src/components/kansalainen/tietoaPalvelusta/StyledRating.tsx
+++ src/components/kansalainen/tietoaPalvelusta/StyledRating.tsx
@@ -3,7 +3,8 @@ import { styled } from "@mui/material/styles";
 import Rating, { IconContainerProps, RatingProps } from "@mui/material/Rating";
 import { Box, Slider, SliderProps, useMediaQuery, useTheme } from "@mui/material";
 import useTranslation from "next-translate/useTranslation";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { keyframes } from "@mui/system";
 
 const MAX_RATING = 10;
 const ratingElementHeight = "57px";
@@ -80,27 +81,66 @@ const DesktopRating = styled(({ onChange: onChangeProp, ...props }: DesktopRatin
   },
 }));
 
-const MobileRating = styled(({ onChange: onChangeProp, ...props }: MobileRatingProps) => {
-  const onChange = useCallback(
-    (_event: Event, value: number | number[]) => {
-      if (typeof value === "number") {
-        onChangeProp(value);
-      }
-    },
-    [onChangeProp]
-  );
-  return <Slider step={1} max={MAX_RATING} onChange={onChange} {...props} />;
-})(({ theme }) => ({
+const slide = keyframes`
+  0% {
+    transform: translate(-50%, -50%);
+  }
+  50% {
+    transform: translate(700%, -50%);
+  }
+  100% {
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const expand = keyframes`
+  0% {
+    left: 0;
+    width: 0;
+  }
+  50% {
+    left: 0;
+    width: 12em
+  }
+  100% {
+    left: 0;
+    width: 0;
+  }
+`;
+
+const MobileRating = styled(
+  ({
+    onChange: onChangeProp,
+    animationValue,
+    hasAnimated,
+    ...props
+  }: MobileRatingProps & { animationValue: number; hasAnimated: boolean }) => {
+    const onChange = useCallback(
+      (_event: Event, value: number | number[]) => {
+        if (typeof value === "number") {
+          onChangeProp(value);
+        }
+      },
+      [onChangeProp]
+    );
+    return <Slider step={1} max={MAX_RATING} onChange={onChange} {...props} />;
+  }
+)(({ theme, value, animationValue, hasAnimated }) => ({
   color: theme.palette.primary.dark,
   height: 3,
   padding: "13px 0",
   "& .MuiSlider-thumb": {
     height: 27,
     width: 27,
+    animation: !hasAnimated && !value && `${slide} 1500ms ease-in-out`, // Pelkkä "!hasAnimated" riittäisi ehdoksi, mutta tuplavarmistu parempi tulevaisuuden kannalta
     color: "current-color",
+    boxShadow: !value && value !== animationValue && "0 0 0 14px rgba(0, 153, 255, 0.08)", // Näytetään "halo" sliderille animaation ajan
     "&:hover": {
       boxShadow: "0 0 0 8px rgba(0, 153, 255, 0.08)",
     },
+  },
+  "& .MuiSlider-track": {
+    animation: !hasAnimated && !value && `${expand} 1500ms ease-in-out`, // Pelkkä "!hasAnimated" riittäisi ehdoksi, mutta tuplavarmistu parempi tulevaisuuden kannalta
   },
   "& .MuiSlider-thumb.Mui-active": {
     boxShadow: "0 0 0 14px rgba(0, 153, 255, 0.08)",
@@ -110,9 +150,48 @@ const MobileRating = styled(({ onChange: onChangeProp, ...props }: MobileRatingP
   },
 }));
 
+function recursiveTimeout(setter: (value: number) => void, value: number, direction: number, endSetter: () => void) {
+  let nextDirection = direction;
+  if ((direction === 1 && value < 8) || (direction === -1 && value > 0)) {
+    setter(value + direction);
+  } else if (value === 8) {
+    nextDirection = -1;
+  } else if (value === 0) {
+    return endSetter();
+  }
+  setTimeout(() => {
+    recursiveTimeout(setter, value + direction, nextDirection, endSetter);
+  }, 75);
+}
+
 export const ResponsiveRating = styled((props: ResponsiveRatingProps) => {
   const theme = useTheme();
   const desktop = useMediaQuery(theme.breakpoints.up("lg"));
+  const [animationValue, setAnimationValue] = useState(0); //Arvo, joka näytetään Sliderille animaation ajan.
+  const [hasAnimated, setHasAnimated] = useState(false);
+
+  useEffect(() => {
+    if (!hasAnimated && !props.value) {
+      recursiveTimeout(setAnimationValue, 0, 1, () => setHasAnimated(true));
+      /**
+       * Tässä useEffectissä kutsutaan funktiota, joka asettaa animaation aikana näytettävän
+       * arvon 0, 1, ...8, 7, ...0 tasaisin välein.
+       * Toivotaan sormet ristissä, että käyttäjä tämä komponentti ei häviä DOMista
+       * ennen kuin setteri on vikan kerran ajettu.
+       */
+    }
+  }, [hasAnimated, setAnimationValue, setHasAnimated, props.value]);
+
+  useEffect(() => {
+    if (props.value) {
+      setHasAnimated(true);
+      /**
+       * HasAnimatedResetoituu jos palautelomakkeen sulkee, mutta props.value ei reseoitu.
+       * Silloin ei kuitenkaan haluta animoida enää, jos käyttäjä selvästi on antanut jo jonkin arvosanan.
+       * Tällä estetään olennaisesti ottaen se, että jos käyttäjä itse slaidaa arvosanan nollaksi, niin animaatio ei toistu.
+       */
+    }
+  }, [props.value, setHasAnimated]);
 
   if (desktop) {
     return <DesktopRating {...props} />;
@@ -127,8 +206,8 @@ export const ResponsiveRating = styled((props: ResponsiveRatingProps) => {
           },
         }}
       >
-        <MobileRating {...props} />
-        <p>{props.value}</p>
+        <MobileRating animationValue={animationValue} hasAnimated={hasAnimated} {...props} />
+        <p>{animationValue || props.value}</p>
       </Box>
     );
   }
