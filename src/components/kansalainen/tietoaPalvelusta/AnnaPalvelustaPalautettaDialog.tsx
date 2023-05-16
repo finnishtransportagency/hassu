158a3f7b146c3b2d2cc8071fb6318686876d84f3
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-05-16 15:38:29 +0300

    Tee mobiiliversioon palautelomakkeesta kaksi sivua

--- src/components/kansalainen/tietoaPalvelusta/AnnaPalvelustaPalautettaDialog.tsx
+++ src/components/kansalainen/tietoaPalvelusta/AnnaPalvelustaPalautettaDialog.tsx
@@ -4,7 +4,7 @@ import HassuDialog, { HassuDialogProps } from "@components/HassuDialog";
 import ContentSpacer from "@components/layout/ContentSpacer";
 import { DialogActions, DialogContent, useMediaQuery, useTheme } from "@mui/material";
 import useTranslation from "next-translate/useTranslation";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { ResponsiveRating } from "./StyledRating";
 import * as Yup from "yup";
@@ -35,6 +35,14 @@ export default function AnnaPalvelustaPalautettaDialog(props: Omit<HassuDialogPr
     reValidateMode: "onChange",
   });
   const { t } = useTranslation("tietoa-palvelusta/palautetta-palvelusta-dialog");
+  const [mobilePage, setMobilePage] = useState(1);
+
+  useEffect(() => {
+    if (props.open) {
+      setMobilePage(1);
+      //Jos käyttäjä sulkee dialogin, seuraavan kerran kun hän avaa sen, sivu on taas 1
+    }
+  }, [props.open, setMobilePage]);
 
   const { showInfoMessage } = useSnackbars();
 
@@ -67,35 +75,61 @@ export default function AnnaPalvelustaPalautettaDialog(props: Omit<HassuDialogPr
   return (
     <HassuDialog scroll="paper" fullScreen={isMobile} title={t("otsikko")} {...props}>
       <form style={{ display: "contents" }}>
-        <DialogContent>
-          <ContentSpacer gap={8}>
-            <p>{t("kappale1")}</p>
-            <h5 className="vayla-small-title">{t("minka-arvosanan-antaisit-palvelulle.otsikko")}</h5>
-            <p className="vayla-label">{t("minka-arvosanan-antaisit-palvelulle.kappale1")}</p>
-            <p>{t("minka-arvosanan-antaisit-palvelulle.kappale2")}</p>
-            <Controller
-              control={control}
-              name="arvosana"
-              render={({ field: { value, onChange }, fieldState: { error } }) => (
-                <div>
-                  <ResponsiveRating value={value} onChange={onChange} />
-                  {error && <p className="text-red">{t("anna-arvosana")}</p>}
-                </div>
-              )}
-            />
-            <h5 className="vayla-small-title">{t("miten-kehittaisit-palvelua-paremmaksi.otsikko")}</h5>
-            <Textarea minRows={3} {...register("kehitysehdotus")} label={t("miten-kehittaisit-palvelua-paremmaksi.tekstikentan-nimike")} />
-            <p>{t("miten-kehittaisit-palvelua-paremmaksi.kappale1")}</p>
+        <DialogContent style={isMobile ? { fontSize: "small" } : {}}>
+          <ContentSpacer gap={isMobile ? 4 : 8}>
+            {(!isMobile || (isMobile && mobilePage === 1)) && (
+              <React.Fragment>
+                <p>{t("kappale1")}</p>
+                <h5 className="vayla-small-title">{t("minka-arvosanan-antaisit-palvelulle.otsikko")}</h5>
+                <p className="vayla-label">{t("minka-arvosanan-antaisit-palvelulle.kappale1")}</p>
+                <p>{t("minka-arvosanan-antaisit-palvelulle.kappale2")}</p>
+                <Controller
+                  control={control}
+                  name="arvosana"
+                  render={({ field: { value, onChange }, fieldState: { error } }) => (
+                    <div>
+                      <ResponsiveRating value={value} onChange={onChange} />
+                      {error && <p className="text-red">{t("anna-arvosana")}</p>}
+                    </div>
+                  )}
+                />
+              </React.Fragment>
+            )}
+            {(!isMobile || (isMobile && mobilePage === 2)) && (
+              <React.Fragment>
+                <h5 className="vayla-small-title">{t("miten-kehittaisit-palvelua-paremmaksi.otsikko")}</h5>
+                <Textarea
+                  minRows={3}
+                  {...register("kehitysehdotus")}
+                  label={t("miten-kehittaisit-palvelua-paremmaksi.tekstikentan-nimike")}
+                />
+                <p>{t("miten-kehittaisit-palvelua-paremmaksi.kappale1")}</p>
+              </React.Fragment>
+            )}
           </ContentSpacer>
         </DialogContent>
-        <DialogActions>
-          <Button type="button" primary onClick={handleSubmit(laheta)}>
-            {t("laheta")}
-          </Button>
-          <Button type="button" onClick={peruuta}>
-            {t("peruuta")}
-          </Button>
-        </DialogActions>
+        {isMobile ? (
+          <DialogActions>
+            {mobilePage === 1 ? (
+              <Button type="button" primary onClick={() => setMobilePage(2)}>
+                {t("jatka")}
+              </Button>
+            ) : (
+              <Button type="button" primary onClick={handleSubmit(laheta)}>
+                {t("laheta")}
+              </Button>
+            )}
+          </DialogActions>
+        ) : (
+          <DialogActions>
+            <Button type="button" primary onClick={handleSubmit(laheta)}>
+              {t("laheta")}
+            </Button>
+            <Button type="button" onClick={peruuta}>
+              {t("peruuta")}
+            </Button>
+          </DialogActions>
+        )}
       </form>
     </HassuDialog>
   );
