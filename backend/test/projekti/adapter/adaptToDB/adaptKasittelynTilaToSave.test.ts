3a752bafbd64c8c920def82148ae56baafce6ad9
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-06-05 13:44:48 +0300

    lisää testi, joka olisi saanut kiinni sen, että adaptKasittelynTilaToSave ei tallentanut paatos-kenttää

--- backend/test/projekti/adapter/adaptToDB/adaptKasittelynTilaToSave.test.ts
+++ backend/test/projekti/adapter/adaptToDB/adaptKasittelynTilaToSave.test.ts
@@ -9,6 +9,76 @@ import { KasittelynTila } from "../../../../src/database/model";
 const { expect } = require("chai");
 
 describe("adaptKasittelynTilaToSave", () => {
+  it("should save paatos when valipaatos is already saved", async () => {
+    const dbProjekti = new ProjektiFixture().dbProjekti1();
+    dbProjekti.kasittelynTila = {
+      ennakkoneuvotteluPaiva: "2023-04-20",
+      ensimmainenJatkopaatos: {
+        paatoksenPvm: "2023-04-21",
+      },
+      hyvaksymisesitysTraficomiinPaiva: "2023-04-21",
+
+      hyvaksymispaatos: {
+        aktiivinen: true,
+        asianumero: "VÄYLÄ/1234/01.02.03/2022",
+        paatoksenPvm: "2023-04-21",
+      },
+      lainvoimaAlkaen: "2023-04-22",
+      lainvoimaPaattyen: "2023-04-23",
+      liikenteeseenluovutusKokonaan: "2022-11-25",
+      liikenteeseenluovutusOsittain: "2022-11-25",
+      suunnitelmanTila: "suunnitelman-tila/sutil09",
+      toinenJatkopaatos: {
+        paatoksenPvm: "2020-01-01",
+      },
+      valitustenMaara: 3,
+      hallintoOikeus: {
+        valipaatos: {
+          paiva: "2023-01-01",
+          sisalto: "Blah blah",
+        },
+        hyvaksymisPaatosKumottu: false,
+      },
+      korkeinHallintoOikeus: undefined,
+    };
+    const kasittelynTilaInput: KasittelyntilaInput = {
+      hallintoOikeus: {
+        valipaatos: {
+          paiva: "2023-01-01",
+          sisalto: "Blah blah",
+        },
+        paatos: {
+          paiva: "2023-01-03",
+          sisalto: "Blah blah2",
+        },
+        hyvaksymisPaatosKumottu: true,
+      },
+    };
+
+    const expectedResult: KasittelynTila = cloneDeep({
+      ...dbProjekti.kasittelynTila,
+      hallintoOikeus: {
+        valipaatos: {
+          paiva: "2023-01-01",
+          sisalto: "Blah blah",
+        },
+        paatos: {
+          paiva: "2023-01-03",
+          sisalto: "Blah blah2",
+        },
+        hyvaksymisPaatosKumottu: true,
+      },
+    });
+
+    const result = adaptKasittelynTilaToSave(
+      pickBy(dbProjekti.kasittelynTila, identity),
+      pickBy(kasittelynTilaInput, identity),
+      new ProjektiAdaptationResult(dbProjekti)
+    );
+
+    expect(result).to.eql(expectedResult);
+  });
+
   it("should adapt kasittelynTila to save", async () => {
     const dbProjekti = new ProjektiFixture().dbProjekti1();
     delete dbProjekti.kielitiedot?.toissijainenKieli;
