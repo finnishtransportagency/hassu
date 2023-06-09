1d5c89b61371a145c9b8fa32b350b61841ae858a
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-06-09 13:44:22 +0300

    Monikielistä tutustu vaylavirastoon jne. widget

--- src/components/kansalaisenEtusivu/TutustuVaylavirastoonJaElyynWidget.tsx
+++ src/components/kansalaisenEtusivu/TutustuVaylavirastoonJaElyynWidget.tsx
@@ -1,16 +1,17 @@
-import React, { ComponentProps } from "react";
+import React from "react";
 import styles from "@styles/kansalaisenEtusivu/EtusivuJulkinenSideNavigation.module.css";
 import Section from "@components/layout/Section2";
 import SectionContent from "@components/layout/SectionContent";
-import { styled } from "@mui/material";
 import ButtonLink from "@components/button/ButtonLink";
+import useTranslation from "next-translate/useTranslation";
 
-const TutustuVaylavirastoonJaElyynWidget = styled((props) => {
+function TutustuVaylavirastoonJaElyynWidget() {
+  const { t, lang } = useTranslation("main-page-sidebar");
+
   return (
     <Section
       sx={{ boxShadow: 1 }}
       noDivider
-      {...props}
       style={{
         backgroundImage: "url(rata_ja_tie_background.jpeg)",
         paddingLeft: "1em",
@@ -22,24 +23,31 @@ const TutustuVaylavirastoonJaElyynWidget = styled((props) => {
       <div role="navigation" className={styles["side-nav"]}>
         <SectionContent className={styles["side-nav-content"]} style={{ textAlign: "center", backgroundColor: "white" }}>
           <h3 className="vayla-subtitle" style={{ color: "rgb(0, 100, 175)" }}>
-            Tutustu Väylävirastoon ja ELY-keskukseen
+            {t("widgetit.tutustu-vaylaan-elyyn")}
           </h3>
-          <p className="mt-4 mb-8">
-            Väylävirasto vastaa valtion tieverkon, rautateiden ja vesiväylien kehittämisestä sekä kunnossapidosta. Se huolehtii myös
-            liikenteen palvelutasosta sekä osallistuu liikenteen ja maankäytön yhteensovittamiseen. Elinkeino-, liikenne- ja
-            ympäristökeskukset (ELY-keskus) huolehtivat alueensa tienpidon tehtävistä Väyläviraston ohjauksen mukaisesti. Sekä Väylävirasto
-            että ELY-keskus tekevät väylähankkeiden suunnittelua.
-          </p>
-          <ButtonLink external primary href="http://vayla.fi" style={{ marginLeft: "auto", marginRight: "auto" }} target="_blank">
-            Väylävirasto
+          <p className="mt-4 mb-8">{t("widgetit.tietoa-vaylasta-elysta")}</p>
+          <ButtonLink
+            external
+            primary
+            href={lang == "fi" ? "http://vayla.fi" : "https://vayla.fi/sv/framsida"}
+            style={{ marginLeft: "auto", marginRight: "auto" }}
+            target="_blank"
+          >
+            {t("widgetit.vaylavirasto")}
           </ButtonLink>
-          <ButtonLink external primary href="http://ely-keskus.fi" style={{ marginLeft: "auto", marginRight: "auto" }} target="_blank">
-            ELY-keskus
+          <ButtonLink
+            external
+            primary
+            href={lang == "fi" ? "http://ely-keskus.fi" : "https://www.ely-keskus.fi/sv/etusivu"}
+            style={{ marginLeft: "auto", marginRight: "auto" }}
+            target="_blank"
+          >
+            {t("widgetit.ely-keskus")}
           </ButtonLink>
         </SectionContent>
       </div>
     </Section>
   );
-})<ComponentProps<typeof Section>>({});
+}
 
 export default TutustuVaylavirastoonJaElyynWidget;
