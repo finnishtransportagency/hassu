61c149f27a93082947e5361929bd9b64f149076e
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-06-09 09:24:25 +0300

    Käytä TietoaSuunnittelustaWidgetissä ButtonLink:iä

--- src/components/kansalaisenEtusivu/TietoaSuunnittelustaWidget.tsx
+++ src/components/kansalaisenEtusivu/TietoaSuunnittelustaWidget.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Button from "@components/button/Button";
-import HassuLink from "@components/HassuLink";
 import Widget from "./Widget";
 import useTranslation from "next-translate/useTranslation";
+import ButtonLink from "@components/button/ButtonLink";
 
 function TietoaSuunnittelustaWidget() {
   const { t, lang } = useTranslation("main-page-sidebar");
@@ -11,11 +10,13 @@ function TietoaSuunnittelustaWidget() {
       <img src="/rata_ja_tie_background.jpeg" alt="Kuva rautatiestä" />
       <Widget title={t("widgetit.tietoa-suunnittelusta")}>
         {t("widgetit.tutustu-hankesuunnittelun")}
-        <HassuLink href="/tietoa-palvelusta/tietoa-suunnittelusta">
-          <Button className="mt-8 mb-4" style={lang == "fi" ? { whiteSpace: "nowrap" } : { whiteSpace: "nowrap", fontSize: "medium" }}>
-            {t("widgetit.tutustu-suunnitteluun")}
-          </Button>
-        </HassuLink>
+        <ButtonLink
+          href="/tietoa-palvelusta/tietoa-suunnittelusta"
+          className="mt-8 mb-4"
+          style={lang == "fi" ? { whiteSpace: "nowrap" } : { whiteSpace: "nowrap", fontSize: "medium" }}
+        >
+          {t("widgetit.tutustu-suunnitteluun")}
+        </ButtonLink>
       </Widget>
     </React.Fragment>
   );
