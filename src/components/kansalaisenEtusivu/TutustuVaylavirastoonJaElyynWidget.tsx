942ca02afb1f584cdb21e99bfbeb9f6c8ec8276f
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-06-09 13:52:38 +0300

    refaktoroi widgettiä

--- src/components/kansalaisenEtusivu/TutustuVaylavirastoonJaElyynWidget.tsx
+++ src/components/kansalaisenEtusivu/TutustuVaylavirastoonJaElyynWidget.tsx
@@ -4,22 +4,24 @@ import Section from "@components/layout/Section2";
 import SectionContent from "@components/layout/SectionContent";
 import ButtonLink from "@components/button/ButtonLink";
 import useTranslation from "next-translate/useTranslation";
+import { styled, experimental_sx as sx } from "@mui/material";
+
+const TutustuSection = styled(Section)(() => {
+  return sx({
+    backgroundImage: "url(rata_ja_tie_background.jpeg)",
+    paddingLeft: "1em",
+    paddingRight: "1em",
+    paddingTop: "1.5em",
+    paddingBottom: "1.5em",
+    boxShadow: 1,
+  });
+});
 
 function TutustuVaylavirastoonJaElyynWidget() {
   const { t, lang } = useTranslation("main-page-sidebar");
 
   return (
-    <Section
-      sx={{ boxShadow: 1 }}
-      noDivider
-      style={{
-        backgroundImage: "url(rata_ja_tie_background.jpeg)",
-        paddingLeft: "1em",
-        paddingRight: "1em",
-        paddingTop: "1.5em",
-        paddingBottom: "1.5em",
-      }}
-    >
+    <TutustuSection noDivider>
       <div role="navigation" className={styles["side-nav"]}>
         <SectionContent className={styles["side-nav-content"]} style={{ textAlign: "center", backgroundColor: "white" }}>
           <h3 className="vayla-subtitle" style={{ color: "rgb(0, 100, 175)" }}>
@@ -46,7 +48,7 @@ function TutustuVaylavirastoonJaElyynWidget() {
           </ButtonLink>
         </SectionContent>
       </div>
-    </Section>
+    </TutustuSection>
   );
 }
 
