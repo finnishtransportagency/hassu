9000680d767624855baf6d272060c584c7c650de
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-06-09 10:57:51 +0300

    Mobiililla ruotsiteksti on vieläkin pienempi

--- src/components/kansalaisenEtusivu/TietoaSuunnittelustaWidget.tsx
+++ src/components/kansalaisenEtusivu/TietoaSuunnittelustaWidget.tsx
@@ -7,7 +7,10 @@ import { styled, experimental_sx as sx } from "@mui/material";
 const ResponsiveButtonLink = styled(ButtonLink)((props: ButtonLinkProps & { lang?: string }) => {
   return sx({
     whiteSpace: "nowrap",
-    fontSize: props.lang == "fi" ? { lg: "medium!important", xl: "normal!important" } : { lg: "small!important", xl: "medium!important" },
+    fontSize:
+      props.lang == "fi"
+        ? { lg: "medium!important", xl: "normal!important" }
+        : { xs: "medium!important", sm: "small!important", md: "small!important", lg: "small!important", xl: "medium!important" },
   });
 });
 
