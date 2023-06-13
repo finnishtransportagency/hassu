60e0b4386ad69c9b96d69c081e282609a7edc0d3
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-06-13 14:12:48 +0300

    Refaktoroi ProjektiSideNavigation-komponenttia. Eriytä siitä TopLevelRoute-komponentti.

--- src/components/projekti/ProjektiSideNavigation.tsx
+++ src/components/projekti/ProjektiSideNavigation.tsx
@@ -1,9 +1,9 @@
 import React, { FunctionComponent, ReactElement, useEffect } from "react";
 import HassuLink from "../HassuLink";
 import classNames from "classnames";
-import { useRouter } from "next/router";
+import { NextRouter, useRouter } from "next/router";
 import { ProjektiLisatiedolla, useProjekti } from "src/hooks/useProjekti";
-import routes, { isVisible, projektiMeetsMinimumStatus } from "src/util/routes";
+import routes, { isVisible, projektiMeetsMinimumStatus, Route } from "src/util/routes";
 import ProjektiKortti from "./ProjektiKortti";
 import useSnackbars from "src/hooks/useSnackbars";
 import useIsAllowedOnCurrentProjektiRoute from "src/hooks/useIsOnAllowedProjektiRoute";
@@ -39,27 +39,41 @@ const ProjektiSideNavigation: FunctionComponent<{ projekti: ProjektiLisatiedolla
         <ul>
           {routes
             .filter((route) => isVisible(projekti, route))
-            .map((route, index) => {
-              const statusDisabled = !projektiMeetsMinimumStatus(projekti, route.requiredStatus);
-              const isSelected = route.requireExactMatch ? route.pathname === router.pathname : router.pathname.startsWith(route.pathname!);
-              return (
-                <li key={index}>
-                  <HassuLink
-                    id={"sidenavi_" + route.id}
-                    href={!statusDisabled ? { pathname: route.pathname, query: { oid: projekti.oid } } : undefined}
-                    className={classNames(
-                      "block pr-12 p-4 border-l-4",
-                      isSelected ? "border-primary bg-gray-light" : "border-transparent",
-                      statusDisabled ? "text-gray" : "hover:bg-gray-light"
-                    )}
-                  >
-                    {route.title}
-                  </HassuLink>
-                </li>
-              );
-            })}
+            .map((route, index) => (
+              <TopLevelRoute route={route} key={index} projekti={projekti} router={router} />
+            ))}
         </ul>
       </div>
     </>
   );
 };
+
+function TopLevelRoute({
+  route,
+  key,
+  projekti,
+  router,
+}: {
+  route: Route;
+  key: number;
+  projekti: ProjektiLisatiedolla;
+  router: NextRouter;
+}) {
+  const statusDisabled = !projektiMeetsMinimumStatus(projekti, route.requiredStatus);
+  const isSelected = route.requireExactMatch ? route.pathname === router.pathname : router.pathname.startsWith(route.pathname!);
+  return (
+    <li key={key}>
+      <HassuLink
+        id={"sidenavi_" + route.id}
+        href={!statusDisabled ? { pathname: route.pathname, query: { oid: projekti.oid } } : undefined}
+        className={classNames(
+          "block pr-12 p-4 border-l-4",
+          isSelected ? "border-primary bg-gray-light" : "border-transparent",
+          statusDisabled ? "text-gray" : "hover:bg-gray-light"
+        )}
+      >
+        {route.title}
+      </HassuLink>
+    </li>
+  );
+}
