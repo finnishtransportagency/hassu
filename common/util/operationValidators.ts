13741fd9954946181fc99a316dbc4435ccb7260c
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-06-20 11:10:02 +0300

    Luo commoniin funkkar, joka kertoo, onko sallittua muuttaa vähäisen menettelyn boolean-arvoa

--- common/util/operationValidators.ts
+++ common/util/operationValidators.ts
@@ -1,5 +1,5 @@
 import { DBProjekti } from "../../backend/src/database/model";
-import { KuulutusJulkaisuTila, TilasiirtymaTyyppi, Projekti } from "../graphql/apiModel";
+import { KuulutusJulkaisuTila, TilasiirtymaTyyppi, Projekti, AloitusKuulutus, MuokkausTila } from "../graphql/apiModel";
 
 export function isAllowedToMoveBack(tilasiirtymatyyppi: TilasiirtymaTyyppi, projekti: DBProjekti | Projekti): boolean {
   if (tilasiirtymatyyppi === TilasiirtymaTyyppi.NAHTAVILLAOLO) {
@@ -37,4 +37,66 @@ export function isAllowedToMoveBackToSuunnitteluvaihe(projekti: DBProjekti | Pro
   // eikä uudelleenkuulutusta ole avattu, mutta jätetty julkaisematta.
   // On mahdollista siirtyä nähtävilläolovaiheesta suunnitteluun.
   return true;
+}
+
+export function isAllowedToChangeVahainenMenettely(projekti: Projekti): boolean {
+  const aloitusKuulutus: AloitusKuulutus | null | undefined = projekti.aloitusKuulutus;
+  if (!aloitusKuulutus) {
+    return true;
+  }
+  if (aloitusKuulutus.muokkausTila === MuokkausTila.LUKU) {
+    return false;
+  }
+  if (aloitusKuulutus.muokkausTila === MuokkausTila.MUOKKAUS) {
+    return true;
+  }
+  // aloituskuulutus.muokkausTila === MuokkausTila.MIGROITU
+  const suunnittelu = projekti.vuorovaikutusKierros;
+  if (suunnittelu) {
+    return false;
+  }
+  // suunnittelu == null || suunnittelu == undefined
+  const nahtavillaolo = projekti.nahtavillaoloVaihe;
+  if (!nahtavillaolo) {
+    return true;
+  }
+  if (nahtavillaolo.muokkausTila === MuokkausTila.MUOKKAUS) {
+    return true;
+  }
+  if (nahtavillaolo.muokkausTila === MuokkausTila.LUKU) {
+    return false;
+  }
+  const hyvaksymisVaihe = projekti.hyvaksymisPaatosVaihe;
+  if (!hyvaksymisVaihe) {
+    return true;
+  }
+  if (hyvaksymisVaihe.muokkausTila === MuokkausTila.MUOKKAUS) {
+    return true;
+  }
+  if (hyvaksymisVaihe.muokkausTila === MuokkausTila.LUKU) {
+    return false;
+  }
+  // hyvaksymisVaihe.muokkausTila === MuokkausTila.MIGROITU
+  const jatkopaatos = projekti.jatkoPaatos1Vaihe;
+  if (!jatkopaatos) {
+    return true;
+  }
+  if (jatkopaatos.muokkausTila === MuokkausTila.MUOKKAUS) {
+    return true;
+  }
+  if (jatkopaatos.muokkausTila === MuokkausTila.LUKU) {
+    return false;
+  }
+  // jatkopaatos.muokkausTila === MuokkausTila.MIGROITU
+  const jatkopaatos2 = projekti.jatkoPaatos2Vaihe;
+  if (!jatkopaatos2) {
+    return true;
+  }
+  if (jatkopaatos2.muokkausTila === MuokkausTila.MUOKKAUS) {
+    return true;
+  }
+  if (jatkopaatos2.muokkausTila === MuokkausTila.LUKU) {
+    return false;
+  }
+  return false;
 }
