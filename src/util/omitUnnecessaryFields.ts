cc3281fcbf13c5f6ddecac85d0fac3c4234e39c7
tomi korkalainen <tomi.korkalainen@cgi.com>
2023-07-11 10:39:25 +0300

    muokkaa tyypitystä omitUnnecessaryFieldsissä

--- src/util/omitUnnecessaryFields.ts
+++ src/util/omitUnnecessaryFields.ts
@@ -4,6 +4,6 @@ const omitUnnecessaryFields = <T extends Record<string, unknown>>(data: T) =>
   omitBy(
     data,
     (value) => !value || (Array.isArray(value) && (value.length === 0 || (value.length === 1 && (value as string[]).includes(""))))
-  );
+  ) as Partial<T>;
 
 export default omitUnnecessaryFields;
