616d76b2c95a07d1a3f3b529aef880bed04e2c64
tomi korkalainen <tomi.korkalainen@cgi.com>
2023-07-11 13:09:29 +0300

    fix: Lisää sama saavutettavuus ohjelaatikko suunnitteluvaiheeseen mikä on muissa vaiheissa

--- src/components/projekti/suunnitteluvaihe/LuonnoksetJaAineistot/EiJulkinen.tsx
+++ src/components/projekti/suunnitteluvaihe/LuonnoksetJaAineistot/EiJulkinen.tsx
@@ -1,8 +1,7 @@
-import SectionContent from "@components/layout/SectionContent";
-import Section from "@components/layout/Section";
-import Notification, { NotificationType } from "@components/notification/Notification";
+import Section from "@components/layout/Section2";
 import { VuorovaikutusKierros } from "@services/api";
 import MuokkaustilainenLomake from "./MuokkaustilainenLomake";
+import { AineistotSaavutettavuusOhje } from "@components/projekti/common/AineistotSaavutettavuusOhje";
 
 interface Props {
   vuorovaikutus: VuorovaikutusKierros | null | undefined;
@@ -11,17 +10,13 @@ interface Props {
 export default function EiJulkinenLuonnoksetJaAineistotLomake({ vuorovaikutus }: Props) {
   return (
     <Section>
-      <SectionContent className="mt-8 pb-8">
-        <h4 className="vayla-small-title">Suunnitelmaluonnokset ja esittelyaineistot</h4>
-        <p>
-          Kansalaiselle järjestelmän julkisella puolella esiteltävät suunnitelmaluonnokset ja esittelylyaineistot tuodaan Projektivelhosta.
-          Suunnitelmaluonnokset ja aineistot julkaistaan palvelun julkisella puolella kutsun julkaisupäivänä. Suunnitelmaluonnoksia ja
-          esittelyaineistoja on mahdollista päivittää myös kutsun julkaisun jälkeen.
-        </p>
-        <Notification type={NotificationType.INFO_GRAY}>
-          Huomioithan, että suunnitelmaluonnoksien ja esittelyaineistojen tulee täyttää saavutettavuusvaatimukset.
-        </Notification>
-      </SectionContent>
+      <h4 className="vayla-small-title">Suunnitelmaluonnokset ja esittelyaineistot</h4>
+      <p>
+        Kansalaiselle järjestelmän julkisella puolella esiteltävät suunnitelmaluonnokset ja esittelylyaineistot tuodaan Projektivelhosta.
+        Suunnitelmaluonnokset ja aineistot julkaistaan palvelun julkisella puolella kutsun julkaisupäivänä. Suunnitelmaluonnoksia ja
+        esittelyaineistoja on mahdollista päivittää myös kutsun julkaisun jälkeen.
+      </p>
+      <AineistotSaavutettavuusOhje />
       <MuokkaustilainenLomake vuorovaikutus={vuorovaikutus} hidden={false} />
     </Section>
   );
