a0f9f3c60f62fb3a49faa2de8eaa6731a64897ee
Mikko Haapamäki <mikko.haapamaki@cgi.com>
2023-08-29 14:31:15 +0300

    korjaa suomifi-muutoksessa hajonnut pdf-esikatselu

--- src/services/api/commonApi.ts
+++ src/services/api/commonApi.ts
@@ -35,12 +35,15 @@ export class API extends AbstractApi {
     super();
 
     const headerForwarderLink = setContext((_operation: GraphQLRequest, prevContext: Record<string, any>) => {
-      const cookies = Object.fromEntries(document.cookie.split("; ").map((v) => v.split(/=(.*)/s).map(decodeURIComponent)));
-      if (cookies["x-vls-access-token"]) {
-        if (!prevContext.headers) {
-          prevContext.headers = {};
+      // Jos APIa kutsutaan nextjs-api-endpointista, document ei ole saatavilla, eikä relevantti
+      if (typeof document !== "undefined") {
+        const cookies = Object.fromEntries(document.cookie.split("; ").map((v) => v.split(/=(.*)/s).map(decodeURIComponent)));
+        if (cookies["x-vls-access-token"]) {
+          if (!prevContext.headers) {
+            prevContext.headers = {};
+          }
+          prevContext.headers["x-vls-access-token"] = cookies["x-vls-access-token"];
         }
-        prevContext.headers["x-vls-access-token"] = cookies["x-vls-access-token"];
       }
       if (this.oneTimeHeaders && enableOneTimeHeaders) {
         // This code is required to pass both signed cookie and basic authentication from incoming request to API in src/pages/api/projekti/[oid]/aloituskuulutus/pdf.ts
