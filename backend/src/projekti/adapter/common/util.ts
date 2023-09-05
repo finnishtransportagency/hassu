0e6a290ba7e6f6df431e513564f1e3ba5cbeb160
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-09-05 16:33:02 +0300

    Korjaa funktion findPublishedKuulutusJulkaisu tyypitystä

--- backend/src/projekti/adapter/common/util.ts
+++ backend/src/projekti/adapter/common/util.ts
@@ -1,6 +1,6 @@
 import * as API from "../../../../../common/graphql/apiModel";
 import { isDateTimeInThePast } from "../../../util/dateUtil";
-import { findJulkaisuWithTila, GenericKuulutus } from "../../projektiUtil";
+import { findJulkaisuWithTila, GenericDbKuulutusJulkaisu } from "../../projektiUtil";
 import { SaameKieli, SuomiRuotsiKieli } from "../../../database/model";
 
 /**
@@ -8,7 +8,9 @@ import { SaameKieli, SuomiRuotsiKieli } from "../../../database/model";
  * @param kuulutusJulkaisut
  * @returns HYVAKSYTTY kuulutus whose kuulutusPaiva is in the past or a MIGRATOITU kuulutus
  */
-export function findPublishedKuulutusJulkaisu<J extends GenericKuulutus>(kuulutusJulkaisut: J[] | undefined | null): J | undefined {
+export function findPublishedKuulutusJulkaisu<J extends GenericDbKuulutusJulkaisu>(
+  kuulutusJulkaisut: J[] | undefined | null
+): J | undefined {
   const hyvaksyttyKuulutus = findJulkaisuWithTila(kuulutusJulkaisut, API.KuulutusJulkaisuTila.HYVAKSYTTY);
   if (hyvaksyttyKuulutus?.kuulutusPaiva && isDateTimeInThePast(hyvaksyttyKuulutus.kuulutusPaiva, "start-of-day")) {
     return hyvaksyttyKuulutus;
