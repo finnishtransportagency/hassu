40b0327c6cba5df26b997401d76e2d916987e400
Petri Hakala <petri.hakala@cgi.com>
2023-09-12 14:54:11 +0300

    Kuva yläreunaan

--- src/components/projekti/ProjektiKortti.tsx
+++ src/components/projekti/ProjektiKortti.tsx
@@ -12,10 +12,11 @@ export default function ProjektiKortti(props: Props): ReactElement {
   const julkinenURL = window.location.protocol + "//" + window.location.host + "/suunnitelma/" + props.projekti.oid;
   const asiatunnus = getAsiatunnus(props.projekti) || "-/0000/00.00.00/0000";
   const nimi = props.projekti.velho.nimi;
+
   return (
     <div role="navigation" className="bg-gray-lightest" style={{ marginBottom: "1em", paddingTop: "1.2em", paddingBottom: "1.2em" }}>
       <div className="flex">
-        <img style={{ display: "inline", marginLeft: "1rem", marginRight: "1rem" }} src="/projektikortin-ikoni.svg" alt="maapallo" />
+        <img style={{ display: "inline", alignSelf: "start", marginLeft: "1rem", marginRight: "1rem" }} src="/projektikortin-ikoni.svg" alt="maapallo" />
         {nimi}
       </div>
       <div style={{ marginLeft: "3.5rem", marginRight: "1rem" }}>
