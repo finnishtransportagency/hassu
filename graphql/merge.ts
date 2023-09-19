dd128b246a60740157018e652b069e624774415c
Petri Hakala <petri.hakala@cgi.com>
2023-09-19 16:15:26 +0300

    fix(deps): Scheman mergaustyökalu vaihdettu kun vanha deprikoitu

---
+++ graphql/merge.ts
@@ -0,0 +1,9 @@
+
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeTypeDefs } from "@graphql-tools/merge";
+import { print } from "graphql";
+import fs from "fs"; 
+const loadedFiles = loadFilesSync(`${__dirname}/*.graphql`);
+const typeDefs = mergeTypeDefs(loadedFiles);
+const printedTypeDefs = print(typeDefs);
+fs.writeFileSync(`${__dirname}/../schema.graphql`, printedTypeDefs);
