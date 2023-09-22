1a968ae598e1e207fdb3b41e326b935f9c27ffb7
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-09-22 15:17:53 +0300

    Tiedostoa ei löytynyt tekstit

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,5 +4,10 @@ import useTranslation from "next-translate/useTranslation";
 
 export default function Custom404() {
   const { t } = useTranslation("common");
-  return <h1>404 - {t("sivua-ei-loydy")}</h1>;
+  return (
+    <>
+      <h1>{t("tiedostoa-ei-loydy")}</h1>
+      <p>{t("tiedostoa-ei-loytynyt")}</p>
+    </>
+  );
 }
