b1b5261ba1cc5768500229657703322f6a75896b
Tomi Korkalainen <77731851+tkork@users.noreply.github.com>
2023-10-05 10:53:22 +0300

    fix: HASSUYP-105 Korjaa selostelähetekirjeeseen osuuden tekstiä ja tyyliä (#960)

--- src/components/projekti/suunnitteluvaihe/VuorovaikutusKierros/SelosteVuorovaikutuskierrokselle.tsx
+++ src/components/projekti/suunnitteluvaihe/VuorovaikutusKierros/SelosteVuorovaikutuskierrokselle.tsx
@@ -1,38 +1,30 @@
 import Textarea from "@components/form/Textarea";
-import Section from "@components/layout/Section";
-import SectionContent from "@components/layout/SectionContent";
-import { VuorovaikutusKierros } from "@services/api";
+import { H4 } from "@components/Headings";
+import ContentSpacer from "@components/layout/ContentSpacer";
+import Section from "@components/layout/Section2";
 import React from "react";
 import { useFormContext } from "react-hook-form";
 
-type Props = {
-  vuorovaikutuskierros: VuorovaikutusKierros | null | undefined;
-};
-
-export default function SelosteVuorovaikutuskierrokselle({}: Props) {
+export default function SelosteVuorovaikutuskierrokselle() {
   const {
     register,
     formState: { errors },
   } = useFormContext();
 
   return (
-    <>
-      <Section>
-        <h5 className="vayla-small-title">Uudelleenkuuluttamisen seloste</h5>
-
-        <SectionContent>
-          <h6 className="vayla-smallest-title">Seloste lähetekirjeeseen</h6>
-          <p>
-            Kirjoita lähetekirjettä varten seloste kutsun uudelleenjulkaisun syistä. Seloste tulee nähtäville viranomaiselle ja kunnille
-            lähetettävän lähetekirjeen alkuun. Älä lisää tekstiin linkkejä.
-          </p>
-          <Textarea
-            label={`Kutsun uudelleenlähettämisen syy *`}
-            {...register(`vuorovaikutusKierros.selosteVuorovaikutuskierrokselle`)}
-            error={(errors as any)?.vuorovaikutusKierros?.selosteVuorovaikutuskierrokselle}
-          />
-        </SectionContent>
-      </Section>
-    </>
+    <Section>
+      <H4>Seloste lähetekirjeeseen</H4>
+      <ContentSpacer>
+        <p>
+          Kirjoita lähetekirjettä varten seloste kutsun uudelleenjulkaisun syistä. Seloste tulee nähtäville viranomaiselle ja kunnille
+          lähetettävän lähetekirjeen alkuun. Älä lisää tekstiin linkkejä.
+        </p>
+        <Textarea
+          label={`Kutsun uudelleenlähettämisen syy *`}
+          {...register(`vuorovaikutusKierros.selosteVuorovaikutuskierrokselle`)}
+          error={(errors as any)?.vuorovaikutusKierros?.selosteVuorovaikutuskierrokselle}
+        />
+      </ContentSpacer>
+    </Section>
   );
 }
