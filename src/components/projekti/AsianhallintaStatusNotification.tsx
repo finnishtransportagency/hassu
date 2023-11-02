c2297ec2107f1e4b955bf1e26ec7da46a4c46ae3
Tomi Korkalainen <77731851+tkork@users.noreply.github.com>
2023-11-02 10:38:26 +0200

    feat: HASSU-1864 Näytä varoitukset väärästä menettelytavasta (#988)
    * feat: HASSU-1864 Näytä varoitukset väärästä menettelytavasta
    
    * piilota asianhallintavaroitukset jos integraatio ei ole aktivoitavissa projektilla (USPA)

--- src/components/projekti/AsianhallintaStatusNotification.tsx
+++ src/components/projekti/AsianhallintaStatusNotification.tsx
@@ -1,4 +1,5 @@
 import Notification, { NotificationType } from "@components/notification/Notification";
+import { StyledLink } from "@components/StyledLink";
 import { AsianTila, Vaihe } from "@services/api";
 import { ProjektiLisatiedolla } from "common/ProjektiValidationContext";
 import { vaiheOnMuokkausTilassa } from "common/util/haeVaiheidentiedot";
@@ -8,13 +9,19 @@ type Props = { projekti: ProjektiLisatiedolla; vaihe: Vaihe };
 
 const ODOTTAMATON_VIRHE_VAROITUS = "Odottamaton virhe asian tilan tarkastamisessa asianhallinnasta.";
 
-const tilakohtainenVaroitus = (asianTila: AsianTila, isAdmin: boolean, lomakeMuokattavissa: boolean): string | undefined => {
+const KIRJAAMO_OSOITE_VAYLA = "kirjaamo@vayla.fi";
+
+const tilakohtainenVaroitus = (
+  asianTila: AsianTila | null | undefined,
+  isAdmin: boolean,
+  lomakeMuokattavissa: boolean
+): JSX.Element | string | undefined => {
   const additionalParagrph = lomakeMuokattavissa ? " Lue ohjeista lisää." : "";
   const vaarassaTilassaVaroitus = `Tarkasta asian tila asianhallintajärjestelmästä. Asialla on auki väärä toimenpide.${
     isAdmin ? " Asialla tulee olla oikea toimenpide auki ennen kuin kuulutuksen pystyy hyväksymään." : additionalParagrph
   }`;
 
-  const tilojenVaroitukset: Record<AsianTila, string | undefined> = {
+  const tilojenVaroitukset: Record<AsianTila, JSX.Element | string | undefined> = {
     ASIAA_EI_LOYDY: "Suunnitelmaa ei löydy asianhallintajärjestelmästä. Varmista, että Projektivelhoon on asetettu oikea asiatunnus.",
     ASIANHALLINTA_VAARASSA_TILASSA: vaarassaTilassaVaroitus,
     SYNKRONOITU: vaarassaTilassaVaroitus,
@@ -22,25 +29,30 @@ const tilakohtainenVaroitus = (asianTila: AsianTila, isAdmin: boolean, lomakeMuo
     VIRHE: ODOTTAMATON_VIRHE_VAROITUS,
     // VALMIS VIENTIIN = EI VAROITUSTA
     VALMIS_VIENTIIN: undefined,
+    VAARA_MENETTELYTAPA: (
+      <>
+        Asianhallintajärjestelmässä asia on avattu väärällä menettelyllä. Ota yhteyttä kirjaamoon sähköpostilla{" "}
+        <StyledLink href={`mailto:kirjaamo@vayla.fi`}>{KIRJAAMO_OSOITE_VAYLA}</StyledLink>
+      </>
+    ),
   };
-  return tilojenVaroitukset[asianTila];
+  return asianTila ? tilojenVaroitukset[asianTila] : ODOTTAMATON_VIRHE_VAROITUS;
 };
 
 export default function AsianhallintaStatusNotification({ projekti, vaihe }: Props) {
   // Näytetään varoitukset vain aktiivisen vaiheen sivuilla
   const aktiivisenVaiheenSivu = projekti.asianhallinta?.aktiivinenTila?.vaihe === vaihe;
-
-  if (aktiivisenVaiheenSivu && projekti.asianhallinta?.inaktiivinen && projekti.asianhallinta.aktivoitavissa) {
+  if (aktiivisenVaiheenSivu && projekti.asianhallinta?.inaktiivinen) {
     return (
       <Notification type={NotificationType.WARN}>
         Integraatioyhteys on pois päältä. Käyttäjän tulee itse viedä tiedostot asianhallintaan.
       </Notification>
     );
   }
+
   const varoitus =
     aktiivisenVaiheenSivu &&
     !projekti.asianhallinta?.inaktiivinen &&
-    projekti.asianhallinta?.aktiivinenTila?.tila &&
     tilakohtainenVaroitus(
       projekti.asianhallinta.aktiivinenTila?.tila,
       projekti.nykyinenKayttaja.onProjektipaallikkoTaiVarahenkilo,
