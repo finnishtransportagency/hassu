3abd4b78bfe0c3cf99f85fe335ff7dd4f73c7d6c
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-11-02 21:00:26 +0200

    Lisää testi sille, että adaptLausuntoPyynnonTaydennykset käsittelee aineitot ja ladatut tiedostot oikein

--- backend/test/projekti/adapter/adaptToAPI/adaptLausuntoPyynnot.test.ts
+++ backend/test/projekti/adapter/adaptToAPI/adaptLausuntoPyynnot.test.ts
@@ -1,4 +1,4 @@
-import { Aineisto, DBProjekti, LausuntoPyynnonTaydennys, LausuntoPyynto } from "../../../../src/database/model";
+import { Aineisto, DBProjekti, LadattuTiedosto, LausuntoPyynnonTaydennys, LausuntoPyynto } from "../../../../src/database/model";
 import { adaptLausuntoPyynnonTaydennykset, adaptLausuntoPyynnot } from "../../../../src/projekti/adapter/adaptToAPI";
 import * as API from "hassu-common/graphql/apiModel";
 import { expect } from "chai";
@@ -106,5 +106,96 @@ describe("adaptLausuntoPyynnot:", () => {
     )?.pop() as API.LausuntoPyynnonTaydennys;
     const secondHash = adaptedLausuntoPyynnonTaydennysAfterPoistumisPaivaUpdate.hash;
     expect(firstHash).to.eql(secondHash);
+  });
+
+  it("adaptLausuntoPyynnonTaydennykset adapts muutAineistot and muistutukset by converting the relative tiedosto path in db to the long one including Projekti oid, and ordering Aineisto array and LadattuTiedosto array by 'jarjetys' field", () => {
+    const dbProjekti: DBProjekti = {
+      oid: "123",
+      salt: "salt",
+    } as any as DBProjekti; // adaptLausuntoPyynnot does not require anything else from dbProjekti
+    const muuAineisto: Aineisto[] = [
+      {
+        dokumenttiOid: "foo",
+        tiedosto: "/lausuntopyynnon_taydennys/joku-uuid/Aineisto%201.txt",
+        nimi: "Aineisto 1",
+        tila: API.AineistoTila.VALMIS,
+        tuotu: "2021-12-01T01:01",
+        jarjestys: 2,
+      },
+      {
+        dokumenttiOid: "bar",
+        tiedosto: "/lausuntopyynnon_taydennys/joku-uuid/Aineisto%202.txt",
+        nimi: "Aineisto 2",
+        tila: API.AineistoTila.VALMIS,
+        tuotu: "2021-12-01T01:02",
+        jarjestys: 1,
+      },
+    ];
+    const muistutukset: LadattuTiedosto[] = [
+      {
+        tiedosto: "/lausuntopyynnon_taydennys/joku-uuid/Tiedosto%201.txt",
+        nimi: "Tiedosto 1",
+        tila: API.LadattuTiedostoTila.VALMIS,
+        tuotu: "2021-12-01T01:03",
+        jarjestys: 2,
+      },
+      {
+        tiedosto: "/lausuntopyynnon_taydennys/joku-uuid/Tiedosto%202.txt",
+        nimi: "Tiedosto 2",
+        tila: API.LadattuTiedostoTila.VALMIS,
+        tuotu: "2021-12-01T01:04",
+        jarjestys: 1,
+      },
+    ];
+    const lausuntoPyynnonTaydennys: LausuntoPyynnonTaydennys = {
+      uuid: "joku-uuid",
+      poistumisPaiva: "2022-01-01",
+      muuAineisto,
+      muistutukset,
+      aineistopaketti: "osoite/aineistopakettiin",
+      poistetaan: false,
+      kunta: 1,
+    };
+    const adaptedLausuntoPyynnonTaydennys: API.LausuntoPyynnonTaydennys = adaptLausuntoPyynnonTaydennykset(dbProjekti, [
+      lausuntoPyynnonTaydennys,
+    ])?.pop() as API.LausuntoPyynnonTaydennys;
+    expect(adaptedLausuntoPyynnonTaydennys.muuAineisto).to.eql([
+      {
+        __typename: "Aineisto",
+        dokumenttiOid: "bar",
+        tiedosto: "/yllapito/tiedostot/projekti/123/lausuntopyynnon_taydennys/joku-uuid/Aineisto%202.txt",
+        nimi: "Aineisto 2",
+        tila: "VALMIS",
+        tuotu: "2021-12-01T01:02",
+        jarjestys: 1,
+      },
+      {
+        __typename: "Aineisto",
+        dokumenttiOid: "foo",
+        tiedosto: "/yllapito/tiedostot/projekti/123/lausuntopyynnon_taydennys/joku-uuid/Aineisto%201.txt",
+        nimi: "Aineisto 1",
+        tila: "VALMIS",
+        tuotu: "2021-12-01T01:01",
+        jarjestys: 2,
+      },
+    ]);
+    expect(adaptedLausuntoPyynnonTaydennys.muistutukset).to.eql([
+      {
+        __typename: "LadattuTiedosto",
+        tiedosto: "/yllapito/tiedostot/projekti/123/lausuntopyynnon_taydennys/joku-uuid/Tiedosto%202.txt",
+        nimi: "Tiedosto 2",
+        tila: "VALMIS",
+        tuotu: "2021-12-01T01:04",
+        jarjestys: 1,
+      },
+      {
+        __typename: "LadattuTiedosto",
+        tiedosto: "/yllapito/tiedostot/projekti/123/lausuntopyynnon_taydennys/joku-uuid/Tiedosto%201.txt",
+        nimi: "Tiedosto 1",
+        tila: "VALMIS",
+        tuotu: "2021-12-01T01:03",
+        jarjestys: 2,
+      },
+    ]);
   });
 });
