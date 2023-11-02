34235f9dcf6b0e42a9848c86a1fbb2d3e9ca77bc
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-11-02 19:44:46 +0200

    Tee adaptLausuntoPyynnot ja adaptLausuntoPyynnonTaydennykset toteutukset

--- backend/src/projekti/adapter/adaptToAPI/adaptLausuntoPyynnot.ts
+++ backend/src/projekti/adapter/adaptToAPI/adaptLausuntoPyynnot.ts
@@ -1,16 +1,56 @@
 import * as API from "hassu-common/graphql/apiModel";
-import { DBProjekti, LausuntoPyynnonTaydennys, LausuntoPyynto } from "../../../database/model";
+import { DBProjekti, LadattuTiedosto, LausuntoPyynnonTaydennys, LausuntoPyynto } from "../../../database/model";
+import { assertIsDefined } from "../../../util/assertions";
+import { PathTuple, ProjektiPaths } from "../../../files/ProjektiPath";
+import { adaptAineistot } from "../common";
+import { tiedostoDownloadLinkService } from "../../../tiedostot/tiedostoDownloadLinkService";
+import { adaptLadattuTiedostoToAPI } from ".";
+import { jarjestaTiedostot } from "hassu-common/util/jarjestaTiedostot";
 
 export function adaptLausuntoPyynnot(
-  _dbProjekti: DBProjekti,
-  _lausuntoPyynnot: LausuntoPyynto[] | undefined | null
-): API.LausuntoPyynto[] | undefined {
-  return undefined;
+  dbProjekti: DBProjekti,
+  lausuntoPyynnot?: Array<LausuntoPyynto> | null
+): Array<API.LausuntoPyynto> | undefined {
+  const oid = dbProjekti.oid;
+  return lausuntoPyynnot?.map((lausuntoPyynto: LausuntoPyynto) => {
+    const { lisaAineistot, legacy: _legacy, ...rest } = lausuntoPyynto;
+    assertIsDefined(dbProjekti.salt);
+    const paths = new ProjektiPaths(oid).lausuntoPyynto(lausuntoPyynto);
+    const apiLausuntoPyynto: API.LausuntoPyynto = {
+      __typename: "LausuntoPyynto",
+      ...rest,
+      lisaAineistot: adaptAineistot(lisaAineistot, paths),
+      hash: tiedostoDownloadLinkService.generateHashForLausuntoPyynto(oid, lausuntoPyynto.uuid, dbProjekti.salt),
+    };
+    return apiLausuntoPyynto;
+  });
 }
 
 export function adaptLausuntoPyynnonTaydennykset(
-  _dbProjekti: DBProjekti,
-  _lausuntoPyynnonTaydennykset: LausuntoPyynnonTaydennys[] | undefined | null
-): API.LausuntoPyynnonTaydennys[] | undefined {
-  return undefined;
+  dbProjekti: DBProjekti,
+  lausuntoPyynnonTaydennykset?: Array<LausuntoPyynnonTaydennys> | null
+): Array<API.LausuntoPyynnonTaydennys> | undefined {
+  const oid = dbProjekti.oid;
+  return lausuntoPyynnonTaydennykset?.map((lausuntoPyynnonTaydennys: LausuntoPyynnonTaydennys) => {
+    const { muuAineisto, muistutukset, ...rest } = lausuntoPyynnonTaydennys;
+    assertIsDefined(dbProjekti.salt);
+    const paths = new ProjektiPaths(oid).lausuntoPyynnonTaydennys(lausuntoPyynnonTaydennys);
+    const taydennys: API.LausuntoPyynnonTaydennys = {
+      __typename: "LausuntoPyynnonTaydennys",
+      ...rest,
+      muuAineisto: adaptAineistot(muuAineisto, paths),
+      muistutukset: adaptLadatutTiedostotToApi(muistutukset, paths),
+      hash: tiedostoDownloadLinkService.generateHashForLausuntoPyynnonTaydennys(oid, lausuntoPyynnonTaydennys.uuid, dbProjekti.salt),
+    };
+    return taydennys;
+  });
+}
+
+function adaptLadatutTiedostotToApi(
+  tiedostot: LadattuTiedosto[] | undefined,
+  projektiPath: PathTuple
+): Array<API.LadattuTiedosto> | undefined {
+  return tiedostot
+    ?.map((tiedosto) => adaptLadattuTiedostoToAPI(projektiPath, tiedosto, false) as API.LadattuTiedosto)
+    .sort(jarjestaTiedostot);
 }
