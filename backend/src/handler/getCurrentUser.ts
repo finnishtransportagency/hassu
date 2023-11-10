618cde3c6ef0f1e941c607b5eb5e14e048ab8041
Petri Hakala <petri.hakala@cgi.com>
2023-11-10 10:32:07 +0200

    test fix

--- backend/src/handler/getCurrentUser.ts
+++ backend/src/handler/getCurrentUser.ts
@@ -6,7 +6,8 @@ export async function getCurrentUser(): Promise<API.NykyinenKayttaja> {
   const kayttaja = userService.requireVaylaUser();
   const ashaEnabled = await parameters.isAsianhallintaIntegrationEnabled()
   const uspaEnabled = await parameters.isUspaIntegrationEnabled()
-  kayttaja.features = { __typename: "Features", asianhallintaIntegraatio: ashaEnabled, uspaIntegraatio: uspaEnabled };
-  
+  if (ashaEnabled || uspaEnabled) {
+    kayttaja.features = { __typename: "Features", asianhallintaIntegraatio: ashaEnabled, uspaIntegraatio: uspaEnabled };
+  }
   return kayttaja;
 }
