808a538256da8f06fbabc97e9d0645089e28b66b
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-12-07 15:19:55 +0200

    Älä näytä aineistonkatseluhommille ylä- ja alapalkkeja

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,12 +5,27 @@ import { Footer } from "./footer";
 import { Container } from "@mui/material";
 import NotificationBar from "@components/notification/NotificationBar";
 import ScrollToTopButton from "./ScrollToTopButton";
+import { useRouter } from "next/router";
 
 interface Props {
   children?: ReactNode;
 }
 
 export default function Layout({ children }: Props): ReactElement {
+  const { route } = useRouter();
+  if (route.includes("lausuntopyyntoaineistot") || route.includes("lausuntopyynnon-taydennysaineistot")) {
+    return (
+      <div className="min-h-screen relative flex flex-col">
+        {process.env.ENVIRONMENT !== "prod" && <NotificationBar />}
+
+        <Container sx={{ marginBottom: "110px", marginTop: "50px" }}>
+          <main>{children}</main>
+        </Container>
+        <ScrollToTopButton />
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen relative flex flex-col">
       {process.env.ENVIRONMENT !== "prod" && <NotificationBar />}
