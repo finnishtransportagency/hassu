6115bf73d81a13fc8b40a5961740a31621b335d2
Valhe Kouneli <valhe.kouneli@cgi.com>
2023-12-08 13:15:14 +0200

    Näytä tuotu-aikaleimat lpt ja lp:ien katselussa ja esikatselussa

--- src/components/projekti/lausuntopyynnot/renderLadattavaTiedosto.tsx
+++ src/components/projekti/lausuntopyynnot/renderLadattavaTiedosto.tsx
@@ -1,19 +1,24 @@
 import ExtLink from "@components/ExtLink";
 import { LadattavaTiedosto } from "@services/api";
+import { formatDate } from "common/util/dateUtils";
 
 export function renderLadattavaTiedosto(tiedosto: LadattavaTiedosto, props?: { esikatselu: boolean }) {
   const fileNameSplit = tiedosto.nimi.split(".");
   const fileType: string = fileNameSplit[fileNameSplit?.length - 1];
+  const tuotu = tiedosto.tuotu ? formatDate(tiedosto.tuotu) : "";
   if (!tiedosto.linkki || props?.esikatselu) {
     return (
       <>
-        {tiedosto.nimi} ({fileType}) {!tiedosto.linkki && "(ODOTTAA TUONTIA)"}
+        {tiedosto.nimi} ({fileType} {tuotu}) {!tiedosto.linkki && "(ODOTTAA TUONTIA)"}
       </>
     );
   } else {
     return (
       <ExtLink className="file_download" href={tiedosto.linkki} sx={{ mr: 3 }}>
-        {tiedosto.nimi} <span className="text-black ml-2">({fileType})</span>
+        {tiedosto.nimi}{" "}
+        <span className="text-black ml-2">
+          ({fileType} {tuotu})
+        </span>
       </ExtLink>
     );
   }
