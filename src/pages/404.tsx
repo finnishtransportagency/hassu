7fb5750bdc03463789792af4d7f16c38ff961da6
tomi korkalainen <tomi.korkalainen@cgi.com>
2023-12-08 13:23:13 +0200

    lisää tekstit 'ei-tiedosto-sivuille'

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 import useTranslation from "next-translate/useTranslation";
+import { useRouter } from "next/router";
+import { H1 } from "@components/Headings";
 
 export default function Custom404() {
   const { t } = useTranslation("common");
+  const router = useRouter();
+
+  const isTiedostoSivu = /^(\/yllapito)?\/tiedostot/.test(router.asPath);
+
   return (
     <>
-      <h1>{t("tiedostoa-ei-loydy")}</h1>
-      <p>{t("tiedostoa-ei-loytynyt")}</p>
+      <H1>{t(isTiedostoSivu ? "tiedostoa-ei-loydy" : "sivua-ei-loydy")}</H1>
+      <p>{t(isTiedostoSivu ? "tiedostoa-ei-loytynyt" : "sivua-ei-loytynyt")}</p>
     </>
   );
 }
