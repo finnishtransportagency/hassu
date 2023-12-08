b475caa88ab12aefbbf5fafb72d2f086ecb031fd
mustonenk <kalle.mustonen@cgi.com>
2023-12-08 15:26:05 +0200

    feat: HASSUYP-116 Tiedostoa ei löytynyt tekstit (#953)
    * Tiedostoa ei löytynyt tekstit
    
    * lisää tekstit 'ei-tiedosto-sivuille'
    
    ---------
    
    Co-authored-by: tomi korkalainen <tomi.korkalainen@cgi.com>

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 
 import useTranslation from "next-translate/useTranslation";
+import { useRouter } from "next/router";
+import { H1 } from "@components/Headings";
 
 export default function Custom404() {
   const { t } = useTranslation("common");
-  return <h1>404 - {t("sivua-ei-loydy")}</h1>;
+  const router = useRouter();
+
+  const isTiedostoSivu = /^(\/yllapito)?\/tiedostot/.test(router.asPath);
+
+  return (
+    <>
+      <H1>{t(isTiedostoSivu ? "tiedostoa-ei-loydy" : "sivua-ei-loydy")}</H1>
+      <p>{t(isTiedostoSivu ? "tiedostoa-ei-loytynyt" : "sivua-ei-loytynyt")}</p>
+    </>
+  );
 }
