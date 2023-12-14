0739812cb6416e664743f78defc2f71c7dfa38bb
Petri Hakala <petri.hakala@cgi.com>
2023-12-14 15:39:49 +0200

    Update for certificate script

--- deployment/bin/updateCertificate.sh
+++ deployment/bin/updateCertificate.sh
@@ -9,10 +9,26 @@ echo "Processing certificate $INPUT_FILE into $2 using AWS_PROFILE=$AWS_PROFILE"
 WORKDIR="ssl_$ENV"
 mkdir -p "$WORKDIR"
 
-openssl pkcs12 -in "$INPUT_FILE" -out "$WORKDIR/privatekey.pem" -nocerts -nodes -legacy
-openssl pkcs12 -in "$INPUT_FILE" -out "$WORKDIR/cert.pem" -nokeys -legacy
+if [ ! -f "$WORKDIR/privatekey.pem" ]; then
+  openssl pkcs12 -in "$INPUT_FILE" -out "$WORKDIR/privatekey.pem" -nocerts -nodes -legacy
+  openssl pkcs12 -in "$INPUT_FILE" -out "$WORKDIR/cert.pem" -nokeys -legacy
+fi
+# pfx filussa voi olla myös certicate chain
+NUMBER_OF_CERTS=$(grep -F "BEGIN CERTIFICATE" -c $WORKDIR/cert.pem)
+if [ $NUMBER_OF_CERTS -gt 1 ]; then
+  echo "Edit $WORKDIR/cert.pem to contain only server certificate. You can put rest to Intermediate.crt. Then run script again."
+  exit 1;
+fi
 aws acm import-certificate --certificate "fileb://$WORKDIR/cert.pem" --private-key "fileb://$WORKDIR/privatekey.pem" --certificate-chain "fileb://$INTERMEDIATE" --region us-east-1 >"$WORKDIR/output.json"
 CERTIFICATE_ARN=$(grep CertificateArn $WORKDIR/output.json | cut -d '"' -f 4)
 aws ssm put-parameter --overwrite --region us-east-1 --name "/$ENV/CloudfrontCertificateArn" --value "$CERTIFICATE_ARN" --type String
 aws ssm put-parameter --overwrite --region eu-west-1 --name "/$ENV/CloudfrontCertificateArn" --value "$CERTIFICATE_ARN" --type String
+CERTIFICATE=$(cat $WORKDIR/cert.pem)
+PRIVATEKEY=$(cat $WORKDIR/privatekey.pem)
+if [ "$ENV" == "dev" ] || [ "$ENV" == "prod" ]; then
+  aws ssm put-parameter --overwrite --region eu-west-1 --name "/$ENV/Certificate" --value "$CERTIFICATE" --type SecureString
+  aws ssm put-parameter --overwrite --region eu-west-1 --name "/$ENV/PrivateKey" --value "$PRIVATEKEY" --type SecureString
+fi
+CN=$(openssl x509 -noout -subject -in $WORKDIR/cert.pem | awk -F= '{gsub(/^[ \t]+/,"",$6); print $6}')
+echo "Certificate $CN updated"
 rm -rf "$WORKDIR"
