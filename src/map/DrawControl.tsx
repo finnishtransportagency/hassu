b1e30e58c461442f9884090f1c14a7fa8cb2adea
tomi korkalainen <tomi.korkalainen@cgi.com>
2024-01-12 12:23:50 +0200

    hallinnoi painikkeiden tiloja DrawControllissa

--- src/map/DrawControl.tsx
+++ src/map/DrawControl.tsx
@@ -30,17 +30,28 @@ class DrawControl extends Control {
   private drawTools: Record<DrawType, Draw>;
   private source: VectorSource<Geometry>;
 
+  private drawToolButtons: Readonly<Record<DrawType, HTMLButtonElement>>;
+
   constructor(options: DrawLineControlProps) {
-    const deleteButton = document.createElement("button");
-    deleteButton.className = "ol-draw-delete";
-    deleteButton.innerHTML = "D";
+    const element = document.createElement("div");
+    element.className = "ol-draw ol-unselectable ol-control";
+
+    super({
+      element: element,
+      target: options.target,
+    });
+
+    const clearButton = document.createElement("button");
+    clearButton.className = "ol-draw-clear";
+    clearButton.innerHTML = "C";
 
     const removeFeatureButton = document.createElement("button");
     removeFeatureButton.className = "ol-draw-remove-feature";
     removeFeatureButton.innerHTML = "R";
+    removeFeatureButton.disabled = true;
 
     const lineButton = document.createElement("button");
-    lineButton.className = "ol-draw-string-line";
+    lineButton.className = "ol-draw-line-string";
     lineButton.innerHTML = "L";
 
     const polygonButton = document.createElement("button");
@@ -54,20 +65,14 @@ class DrawControl extends Control {
     const undoButton = document.createElement("button");
     undoButton.className = "ol-draw-undo";
     undoButton.innerHTML = "U";
+    undoButton.disabled = true;
 
-    const element = document.createElement("div");
-    element.className = "ol-draw ol-unselectable ol-control";
     element.appendChild(lineButton);
     element.appendChild(polygonButton);
     element.appendChild(boxButton);
     element.appendChild(undoButton);
     element.appendChild(removeFeatureButton);
-    element.appendChild(deleteButton);
-
-    super({
-      element: element,
-      target: options.target,
-    });
+    element.appendChild(clearButton);
 
     this.drawType = null;
 
@@ -75,29 +80,57 @@ class DrawControl extends Control {
     this.modify = options.interactions.MODIFY;
     this.drawTools = options.interactions.DRAW;
     this.select = options.interactions.SELECT;
+    this.drawToolButtons = { Box: boxButton, LineString: lineButton, Polygon: polygonButton };
+
+    const selectedFeatures = this.select.getFeatures();
+    this.select.on("change:active", function () {
+      selectedFeatures.forEach(function (feature) {
+        selectedFeatures.remove(feature);
+      });
+    });
+
+    Object.values(this.drawTools).forEach((draw) => {
+      draw.on("drawstart", () => {
+        undoButton.disabled = false;
+      });
+      draw.on("drawend", () => {
+        undoButton.disabled = true;
+      });
+      draw.on("drawabort", () => {
+        undoButton.disabled = true;
+      });
+    });
+
+    this.select.on("select", (e) => {
+      removeFeatureButton.disabled = !e.selected.length;
+    });
+
+    this.source.on("addfeature", () => {
+      clearButton.disabled = !this.source.getFeatures().length;
+    });
+
+    this.source.on("removefeature", () => {
+      clearButton.disabled = !this.source.getFeatures().length;
+    });
 
     lineButton.addEventListener("click", this.toggleDrawOfType.bind(this, DrawType.LINE_STRING), false);
     boxButton.addEventListener("click", this.toggleDrawOfType.bind(this, DrawType.BOX), false);
     polygonButton.addEventListener("click", this.toggleDrawOfType.bind(this, DrawType.POLYGON), false);
     undoButton.addEventListener("click", this.undo.bind(this), false);
-    removeFeatureButton.addEventListener("click", this.removeFeature.bind(this), false);
-    deleteButton.addEventListener("click", this.removeFeatures.bind(this), false);
+    removeFeatureButton.addEventListener("click", this.removeSelectedFeature.bind(this), false);
+    clearButton.addEventListener("click", this.removeAllFeatures.bind(this), false);
   }
 
-  private removeFeatures() {
+  private removeAllFeatures() {
     this.source.getFeatures().forEach((feature) => {
       this.source.removeFeature(feature);
     });
-    this.select.setActive(false);
-    this.modify.setActive(false);
   }
 
-  private removeFeature() {
+  private removeSelectedFeature() {
     this.select.getFeatures().forEach((feature) => {
       this.source.removeFeature(feature);
     });
-    this.select.setActive(false);
-    this.modify.setActive(false);
   }
 
   private undo() {
@@ -105,7 +138,7 @@ class DrawControl extends Control {
   }
 
   private toggleDrawOfType(drawType: DrawType) {
-    this.currentDrawTool()?.setActive(false);
+    this.setDrawToolActive(false);
     if (this.drawType === drawType) {
       this.drawType = null;
       this.modify.setActive(true);
@@ -114,14 +147,31 @@ class DrawControl extends Control {
     }
 
     this.drawType = drawType;
-    this.currentDrawTool()?.setActive(true);
+    this.setDrawToolActive(true);
     this.modify.setActive(false);
     this.select.setActive(false);
   }
 
+  private setDrawToolActive(active: boolean) {
+    const previousTool = this.currentDrawTool();
+    if (previousTool) {
+      previousTool.setActive(active);
+    }
+    this.drawType && this.setButtonClassnames(this.drawType, active);
+  }
+
+  private setButtonClassnames(draw: DrawType, active: boolean) {
+    const element = this.drawToolButtons[draw];
+    const activeClassName = "draw-active";
+    if (active) {
+      element.classList.add(activeClassName);
+    } else {
+      element.classList.remove(activeClassName);
+    }
+  }
+
   private currentDrawTool(): Draw | undefined {
-    const drawTool = this.drawType ? this.drawTools[this.drawType] : undefined;
-    return drawTool;
+    return this.drawType ? this.drawTools[this.drawType] : undefined;
   }
 }
 
@@ -141,19 +191,6 @@ function initInteraction<T extends Interaction>(interaction: T, active: boolean)
 
 export const createDrawToolInteractions: CreateDrawToolInteractions = (source) => {
   const select = initInteraction(new Select(), true);
-  const modify = initInteraction(
-    new Modify({
-      features: select.getFeatures(),
-    }),
-    true
-  );
-
-  const selectedFeatures = select.getFeatures();
-  select.on("change:active", function () {
-    selectedFeatures.forEach(function (feature) {
-      selectedFeatures.remove(feature);
-    });
-  });
 
   return {
     DRAW: {
@@ -175,7 +212,12 @@ export const createDrawToolInteractions: CreateDrawToolInteractions = (source) =
       ),
     },
     SNAP: initInteraction(new Snap({ source }), true),
-    MODIFY: modify,
+    MODIFY: initInteraction(
+      new Modify({
+        features: select.getFeatures(),
+      }),
+      true
+    ),
     SELECT: select,
   };
 };
