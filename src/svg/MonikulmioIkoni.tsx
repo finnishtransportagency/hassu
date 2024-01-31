20d41763cf388ec37d380adaaf84c36737462678
Tomi Korkalainen <77731851+tkork@users.noreply.github.com>
2024-01-31 14:27:38 +0200

    fix: Hae ikonit kartalle simppelimmin ja korjaa kartta 'kiinteissä' ympäristöissä (#1079)
    * fix: Hae ikonit kartalle simppelimmin ja korjaa kartta 'kiinteissä' ympäristöissä
    
    * korjaa svg elementtien attribuuttinimet

---
+++ src/svg/MonikulmioIkoni.tsx
@@ -0,0 +1,18 @@
+import { styled, SvgIcon, SvgIconProps } from "@mui/material";
+import React from "react";
+
+export const MonikulmioIkoni = styled(({ children, ...props }: SvgIconProps) => (
+  <SvgIcon {...props}>
+    <g id="Symbols" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <g id="Artboard" transform="translate(-11.000000, -71.000000)">
+        <g id="icon/kartta3" transform="translate(13.000000, 73.000000)">
+          <g id="Group-3" transform="translate(1.904762, 0.000000)" stroke="currentColor" strokeLinejoin="round" strokeWidth="2">
+            <polygon id="Path-2" points="0 17.8947368 15.2380952 17.8947368 7.61904762 4.47602134e-16 0 6.23933526"></polygon>
+          </g>
+          <circle id="Oval" fill="currentColor" cx="2" cy="6" r="2"></circle>
+          <circle id="Oval" fill="currentColor" cx="17" cy="18" r="2"></circle>
+        </g>
+      </g>
+    </g>
+  </SvgIcon>
+))({});
