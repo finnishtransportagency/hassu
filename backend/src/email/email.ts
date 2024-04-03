b28263073f83c49baaec02d8d0926b11adee200d
Petri Hakala <petri.hakala@cgi.com>
2024-04-03 13:30:43 +0300

    fix: HASSUYP-369: Muistutuksia jäänyt lähettämättä, turvapostissa yhteysvirhe

--- backend/src/email/email.ts
+++ backend/src/email/email.ts
@@ -18,6 +18,7 @@ type TurvapostiConfig = {
   LOGIN: string;
   PASSWORD: string;
   SERVER: string;
+  PORT: number;
 };
 
 async function getSMTPConfig() {
@@ -45,11 +46,11 @@ function getTransport(smtpConfig: SMTPConfig) {
   });
 }
 
-function getTurvapostiTransport(turvapostiConfig: TurvapostiConfig) {
+export function getTurvapostiTransport(turvapostiConfig: TurvapostiConfig) {
   return nodemailer.createTransport({
-    port: 465,
+    port: turvapostiConfig.PORT,
     host: turvapostiConfig.SERVER,
-    secure: true,
+    secure: false,
     auth: {
       user: turvapostiConfig.LOGIN,
       pass: turvapostiConfig.PASSWORD,
