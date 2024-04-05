837a4e0764d7fc54447ba23a753e37594168043b
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-04-05 13:50:00 +0300

    Luo hyväksymisesityksen dybamodb-kutsut

---
+++ backend/src/HyvaksymisEsitys/dynamoDBCalls/index.ts
@@ -0,0 +1,11 @@
+import tallennaMuokattavaHyvaksymisEsitys from "./tallennaMuokattavaHyvaksymisEsitys";
+import muutaMuokattavanHyvaksymisEsityksenTilaa from "./muutaTilaa";
+import palautaHyvaksymisEsitysJaAsetaSyy from "./palautaJaAsetaSyy";
+import hyvaksyHyvaksymisEsitys from "./hyvaksy";
+
+module.exports = {
+  tallennaMuokattavaHyvaksymisEsitys,
+  muutaMuokattavanHyvaksymisEsityksenTilaa,
+  palautaHyvaksymisEsitysJaAsetaSyy,
+  hyvaksyHyvaksymisEsitys,
+};
---
+++ backend/src/HyvaksymisEsitys/dynamoDBCalls/muutaTilaa.ts
@@ -0,0 +1,42 @@
+import { UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { config } from "../../config";
+import * as API from "hassu-common/graphql/apiModel";
+import { FULL_DATE_TIME_FORMAT_WITH_TZ, nyt } from "../../util/dateUtil";
+import { sendParamsToDynamoDB } from "./util";
+
+export default async function muutaMuokattavanHyvaksymisEsityksenTilaa(input: {
+  oid: string;
+  versio: number;
+  uusiTila: API.HyvaksymisTila;
+  vanhaTila: API.HyvaksymisTila;
+}): Promise<number> {
+  const { oid, versio, uusiTila, vanhaTila } = input;
+  const nextVersion = versio + 1;
+  const params = new UpdateCommand({
+    TableName: config.projektiTableName,
+    Key: {
+      oid,
+    },
+    UpdateExpression: "SET " + "#versio = :versio, " + "#muokattavaHyvaksymisEsitys.#tila = :uusiTila, " + "#paivitetty = :paivitetty",
+    ExpressionAttributeNames: {
+      "#versio": "versio",
+      "#muokattavaHyvaksymisEsitys": "muokattavaHyvaksymisEsitys",
+      "#tila": "tila",
+      "#paivitetty": "paivitetty",
+      "#hyvaksymisPaatosVaihe": "hyvaksymisPaatosVaihe",
+    },
+    ExpressionAttributeValues: {
+      ":versio": nextVersion,
+      ":uusiTila": uusiTila,
+      ":paivitetty": nyt().format(FULL_DATE_TIME_FORMAT_WITH_TZ),
+      ":vanhaTila": vanhaTila,
+    },
+    ConditionExpression:
+      "(attribute_not_exists(#versio) OR #versio = :versioFromInput) AND " +
+      "attribute_not_exists(#hyvaksymisPaatosVaihe) AND " +
+      "#muokattavaHyvaksymisEsitys.#tila = :vanhaTila",
+  });
+
+  await sendParamsToDynamoDB(params);
+  return nextVersion;
+}
