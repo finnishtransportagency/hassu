2776f0c839bb3ee01894a5f44157f8f808ad2362
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-04-11 12:50:27 +0300

    Korjaa sekoilua

--- backend/src/projekti/adapter/adaptToAPI/common/adaptLadattuTiedostoToAPI.ts
+++ backend/src/projekti/adapter/adaptToAPI/common/adaptLadattuTiedostoToAPI.ts
@@ -1,5 +1,5 @@
 import { PathTuple } from "../../../../files/ProjektiPath";
-import { KunnallinenLadattuTiedosto, LadattuTiedosto } from "../../../../database/model";
+import { LadattuTiedosto } from "../../../../database/model";
 import * as API from "hassu-common/graphql/apiModel";
 import { fileService } from "../../../../files/fileService";
 import { jarjestaTiedostot } from "hassu-common/util/jarjestaTiedostot";
@@ -30,34 +30,5 @@ export function adaptLadatutTiedostotToApi(
 ): Array<API.LadattuTiedosto> | undefined | null {
   return tiedostot
     ?.map((tiedosto) => adaptLadattuTiedostoToAPI(projektiPath, tiedosto, false) as API.LadattuTiedosto)
-    .sort(jarjestaTiedostot);
-}
-
-export function adaptKunnallinenLadattuTiedostoToAPI(
-  projektiPath: PathTuple,
-  ladattuTiedosto: KunnallinenLadattuTiedosto,
-  julkinen: boolean
-): API.KunnallinenLadattuTiedosto | undefined {
-  if (ladattuTiedosto && ladattuTiedosto.nimi) {
-    const { tiedosto, nimi, tuotu, tila, jarjestys, uuid, kunta } = ladattuTiedosto;
-    let fullPath: string = tiedosto;
-    if (julkinen) {
-      fullPath = fileService.getPublicPathForProjektiFile(projektiPath, tiedosto);
-    } else if (ladattuTiedosto.tila === API.LadattuTiedostoTila.VALMIS) {
-      fullPath = fileService.getYllapitoPathForProjektiFile(projektiPath, tiedosto);
-    }
-    if (!fullPath.startsWith("/")) {
-      fullPath = "/" + fullPath;
-    }
-    return { __typename: "KunnallinenLadattuTiedosto", tiedosto: fullPath, nimi, tuotu, tila, jarjestys, uuid, kunta };
-  }
-}
-
-export function adaptKunnallisetLadatutTiedostotToApi(
-  tiedostot: KunnallinenLadattuTiedosto[] | undefined | null,
-  projektiPath: PathTuple
-): Array<API.KunnallinenLadattuTiedosto> | undefined | null {
-  return tiedostot
-    ?.map((tiedosto) => adaptKunnallinenLadattuTiedostoToAPI(projektiPath, tiedosto, false) as API.KunnallinenLadattuTiedosto)
     .sort(jarjestaTiedostot);
 }
