b7994cf96eecca733570a5dd75b8d1e7e3c0a826
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-04-17 13:56:11 +0300

    Korjaa path nimi

--- backend/src/HyvaksymisEsitys/paths.ts
+++ backend/src/HyvaksymisEsitys/paths.ts
@@ -1,5 +1,5 @@
 // Always start path without slash. Always end path in slash.
-export const JULKAISTU_HYVAKSYMISESITYS_PATH = "hyvasymisesitys/";
+export const JULKAISTU_HYVAKSYMISESITYS_PATH = "hyvaksymisesitys/";
 export const MUOKATTAVA_HYVAKSYMISESITYS_PATH = "muokattava_hyvaksymisesitys/";
 export function getYllapitoPathForProjekti(oid: string) {
   return `yllapito/tiedostot/projekti/${oid}/`;
