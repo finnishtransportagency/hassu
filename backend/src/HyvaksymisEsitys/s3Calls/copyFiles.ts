5c654bc6dfa49f612f1eb85107ab8a294bb506b2
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-04-17 17:26:05 +0300

    Lisää kommentteja

--- backend/src/HyvaksymisEsitys/s3Calls/copyFiles.ts
+++ backend/src/HyvaksymisEsitys/s3Calls/copyFiles.ts
@@ -4,6 +4,12 @@ import { log } from "../../logger";
 import { getS3Client } from "../../aws/client";
 import { getYllapitoPathForProjekti, joinPath } from "../paths";
 
+/**
+ * Kopioi tiedoston annetusta sourceFile-polusta targetFile-polkuun
+ *
+ * @param sourceFile polku tiedostoon; polun alussa ylläpitobucketin nimi
+ * @param targetFile polku tiedostoon; polun alussa ylläpitobucketin nimi
+ */
 async function copyYllapitoFile(sourceFile: string, targetFile: string): Promise<void> {
   const yllapitoBucketName = config.yllapitoBucketName;
   if (!sourceFile.includes(yllapitoBucketName) || !targetFile.includes(yllapitoBucketName)) {
@@ -20,6 +26,14 @@ async function copyYllapitoFile(sourceFile: string, targetFile: string): Promise
   await getS3Client().send(new CopyObjectCommand(params));
 }
 
+/**
+ * Kopioi yhden vaiheen tiedostot toiselle vaiheelle
+ *
+ * @param oid Projetkin oid
+ * @param vaihePrefixFrom from-vaiheen kansio s3:ssa
+ * @param vaihePrefixTo to-vaiheen kansio s3:ssa
+ * @param files kopioitavat tiedostot; tiedossa nimi ja avain, jonka alla ne ovat vaiheessa
+ */
 export async function copyFilesFromVaiheToAnother(
   oid: string,
   vaihePrefixFrom: string,
