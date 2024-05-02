1f1e27df53e8e9b1dc1e5eb8a4358fddf91b0cf0
Valhe Kouneli <valhe.kouneli@gmail.com>
2024-05-02 12:33:45 +0300

    feat: testeja hyvaksymisesitykseen2 (#1134)
    * fileServicen uploads-tallennuksia hoitava funktio käyttää adaptFileName-funktiota
    
    * Testaa tallenna hyväksymisesitys persistointi
    
    * Päivitä snapshot
    
    * Lisää testi tallennukselle ja korjaa sen toteutusta. Muokkaa testidataa sopivammaksi.
    
    ---------
    
    Co-authored-by: Valhe Kouneli <valhe.kouneli@cgi.com>

--- backend/integrationtest/HyvaksymisEsitys/TEST_HYVAKSYMISESITYS_INPUT.ts
+++ backend/integrationtest/HyvaksymisEsitys/TEST_HYVAKSYMISESITYS_INPUT.ts
@@ -4,15 +4,15 @@ const TEST_HYVAKSYMISESITYS_INPUT: API.HyvaksymisEsitysInput = {
   poistumisPaiva: "2033-01-01",
   hyvaksymisEsitys: [
     {
-      tiedosto: "hyvaksymisEsitys.png",
-      nimi: "hyvaksymisEsitys.png",
+      tiedosto: "hyvaksymisEsitys/hyvaksymisEsitys_aoa_.png",
+      nimi: "hyvaksymisEsitys äöå .png",
       uuid: "hyvaksymis-esitys-uuid",
     },
   ],
   suunnitelma: [
     {
       dokumenttiOid: "suunnitelmaDokumenttiOid",
-      nimi: "suunnitelma.png",
+      nimi: "suunnitelma äöå .png",
       uuid: "suunnitelma-uuid",
     },
   ],
@@ -26,43 +26,44 @@ const TEST_HYVAKSYMISESITYS_INPUT: API.HyvaksymisEsitysInput = {
   },
   muistutukset: [
     {
-      tiedosto: "muistutukset.png",
-      nimi: "muistutukset.png",
+      kunta: 1,
+      tiedosto: "muistutukset/muistutukset_aoa_.png",
+      nimi: "muistutukset äöå .png",
       uuid: "muistutukset-esitys-uuid",
     },
   ],
   lausunnot: [
     {
-      tiedosto: "lausunnot.png",
-      nimi: "lausunnot.png",
+      tiedosto: "lausunnot/lausunnot_aoa_.png",
+      nimi: "lausunnot äöå .png",
       uuid: "lausunnot-esitys-uuid",
     },
   ],
   maanomistajaluettelo: [
     {
-      tiedosto: "maanomistajaluettelo.png",
-      nimi: "maanomistajaluettelo.png",
+      tiedosto: "maanomistajaluettelo/maanomistajaluettelo_aoa_.png",
+      nimi: "maanomistajaluettelo äöå .png",
       uuid: "maanomistajaluettelo-esitys-uuid",
     },
   ],
   kuulutuksetJaKutsu: [
     {
-      tiedosto: "kuulutuksetJaKutsu.png",
-      nimi: "kuulutuksetJaKutsu.png",
+      tiedosto: "kuulutuksetJaKutsu/kuulutuksetJaKutsu_aoa_.png",
+      nimi: "kuulutuksetJaKutsu äöå .png",
       uuid: "kuulutuksetJaKutsu-esitys-uuid",
     },
   ],
   muuAineistoVelhosta: [
     {
       dokumenttiOid: "muuAineistoVelhostaDokumenttiOid",
-      nimi: "muuAineistoVelhosta.png",
+      nimi: "muuAineistoVelhosta äöå .png",
       uuid: "muuAineistoVelhosta-uuid",
     },
   ],
   muuAineistoKoneelta: [
     {
-      tiedosto: "muuAineistoKoneelta.png",
-      nimi: "muuAineistoKoneelta.png",
+      tiedosto: "muuAineistoKoneelta/muuAineistoKoneelta_aoa_.png",
+      nimi: "muuAineistoKoneelta äöå .png",
       uuid: "muuAineistoKoneelta-esitys-uuid",
     },
   ],
@@ -70,3 +71,12 @@ const TEST_HYVAKSYMISESITYS_INPUT: API.HyvaksymisEsitysInput = {
 };
 
 export default TEST_HYVAKSYMISESITYS_INPUT;
+
+export const INPUTIN_LADATUT_TIEDOSTOT: { nimi: string; uuid: string; filename: string }[] = [
+  { nimi: "hyvaksymisEsitys äöå .png", uuid: "hyvaksymisEsitys", filename: "hyvaksymisEsitys_aoa_.png" },
+  { nimi: "muistutukset äöå .png", uuid: "muistutukset", filename: "muistutukset_aoa_.png" },
+  { nimi: "lausunnot äöå .png", uuid: "lausunnot", filename: "lausunnot_aoa_.png" },
+  { nimi: "maanomistajaluettelo äöå .png", uuid: "maanomistajaluettelo", filename: "maanomistajaluettelo_aoa_.png" },
+  { nimi: "kuulutuksetJaKutsu äöå .png", uuid: "kuulutuksetJaKutsu", filename: "kuulutuksetJaKutsu_aoa_.png" },
+  { nimi: "muuAineistoKoneelta äöå .png", uuid: "muuAineistoKoneelta", filename: "muuAineistoKoneelta_aoa_.png" },
+];
