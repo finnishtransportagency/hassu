d59f2a8aaa947e6710b979378a47c8d53f308763
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-05-08 11:08:03 +0300

    Uusi testi tallennukselle

--- backend/integrationtest/HyvaksymisEsitys/tallenna.test.ts
+++ backend/integrationtest/HyvaksymisEsitys/tallenna.test.ts
@@ -22,8 +22,13 @@ import { expect } from "chai";
 import { setupLocalDatabase } from "../util/databaseUtil";
 import { omit } from "lodash";
 import MockDate from "mockdate";
-import TEST_HYVAKSYMISESITYS, { TEST_HYVAKSYMISESITYS2, TEST_HYVAKSYMISESITYS_FILES2 } from "./TEST_HYVAKSYMISESITYS";
+import TEST_HYVAKSYMISESITYS, {
+  TEST_HYVAKSYMISESITYS2,
+  TEST_HYVAKSYMISESITYS_FILES,
+  TEST_HYVAKSYMISESITYS_FILES2,
+} from "./TEST_HYVAKSYMISESITYS";
 import { IllegalAccessError, IllegalArgumentError } from "hassu-common/error";
+import { adaptFileName } from "../../src/tiedostot/paths";
 
 describe("Hyväksymisesityksen tallentaminen", () => {
   const userFixture = new UserFixture(userService);
@@ -113,6 +118,148 @@ describe("Hyväksymisesityksen tallentaminen", () => {
         ({ filename, uuid: avain }) => `yllapito/tiedostot/projekti/Testi1/muokattava_hyvaksymisesitys/${avain}/${filename}`
       ).sort()
     );
+  });
+
+  it("osaa pitää, lisätä ja poistaa tiedostoja samaan aikaan", async () => {
+    userFixture.loginAsAdmin();
+    // Luodaan DB:ssä olevan hyväksymisesityksen data
+    const muokattavaHyvaksymisEsitys = {
+      ...TEST_HYVAKSYMISESITYS,
+      muuAineistoVelhosta: [
+        {
+          dokumenttiOid: "muuAineistoVelhostaDokumenttiOid",
+          nimi: "muuAineistoVelhosta äöå .png",
+          uuid: "muuAineistoVelhosta-uuid",
+          lisatty: "2022-01-02T02:00:00+02:00",
+        },
+        // Poistetaan alla oleva tiedosto myöhemmin
+        {
+          dokumenttiOid: "muuAineistoVelhostaDokumenttiOid2",
+          nimi: "muuAineistoVelhosta äöå 2.png",
+          uuid: "muuAineistoVelhosta-uuid2",
+          lisatty: "2022-01-02T02:00:00+02:00",
+        },
+      ],
+      muuAineistoKoneelta: [
+        {
+          nimi: "muuAineistoKoneelta äöå .png",
+          uuid: "muuAineistoKoneelta-esitys-uuid",
+          lisatty: "2022-01-02T02:00:00+02:00",
+        },
+        // Poistetaan alla oleva tiedosto myöhemmin
+        {
+          nimi: "muuAineistoKoneelta äöå 2.png",
+          uuid: "muuAineistoKoneelta-esitys-uuid2",
+          lisatty: "2022-01-02T02:00:00+02:00",
+        },
+      ],
+      tila: API.HyvaksymisTila.MUOKKAUS,
+    };
+    const projektiBefore = {
+      oid,
+      versio: 2,
+      muokattavaHyvaksymisEsitys,
+    };
+    // Asetetaan DB:ssä olevan projektin data
+    await insertProjektiToDB(projektiBefore);
+    // Luodaan DB:ssä olevassa projektissa viitatut tiedostot
+    await Promise.all(
+      TEST_HYVAKSYMISESITYS_FILES.map(async ({ path }) => {
+        const fullpath = `yllapito/tiedostot/projekti/${oid}/muokattava_hyvaksymisesitys/${path}`;
+        await insertYllapitoFileToS3(fullpath);
+      })
+    );
+    const fullpathMuuVelho2 = `yllapito/tiedostot/projekti/${oid}/muokattava_hyvaksymisesitys/muuAineistoVelhosta/${adaptFileName(
+      "muuAineistoVelhosta äöå 2.png"
+    )}`;
+    await insertYllapitoFileToS3(fullpathMuuVelho2);
+    const fullpathMuuKoneelta2 = `yllapito/tiedostot/projekti/${oid}/muokattava_hyvaksymisesitys/muuAineistoKoneelta/${adaptFileName(
+      "muuAineistoKoneelta äöå 2.png"
+    )}`;
+    await insertYllapitoFileToS3(fullpathMuuKoneelta2);
+
+    // Luodaan inputissa viitatut tiedostot uploads-kansioon
+    await Promise.all(INPUTIN_LADATUT_TIEDOSTOT.map(({ nimi, uuid }) => insertUploadFileToS3(uuid, nimi)));
+    await insertUploadFileToS3("upload-uuid", "tiedostonimi.png");
+    // Luodaan inputin data
+    const muokattavaHyvaksymisEsitysInput: API.HyvaksymisEsitysInput = {
+      ...TEST_HYVAKSYMISESITYS_INPUT,
+      muuAineistoVelhosta: [
+        {
+          dokumenttiOid: "muuAineistoVelhostaDokumenttiOid",
+          nimi: "muuAineistoVelhosta äöå .png",
+          uuid: "muuAineistoVelhosta-uuid", // Sama kuin ennen
+        },
+        {
+          dokumenttiOid: "muuAineistoVelhostaDokumenttiOid3",
+          nimi: "muuAineistoVelhosta äöå 3.png",
+          uuid: "muuAineistoVelhosta-uuid3", // Uusi
+        },
+      ],
+      muuAineistoKoneelta: [
+        {
+          nimi: "muuAineistoKoneelta äöå .png",
+          uuid: "muuAineistoKoneelta-esitys-uuid", // Sama kuin ennen
+        },
+        {
+          tiedosto: "upload-uuid/tiedostonimi.png",
+          nimi: "muuAineistoKoneelta äöå 3.png",
+          uuid: "muuAineistoKoneelta-esitys-uuid3", // Uusi
+        },
+      ],
+    };
+    // Asetetaan päiväys päivää myöhemmäksi kuin DB:ssä olevian tiedostojen ladattu-ajat.
+    MockDate.set("2022-01-03");
+    await tallennaHyvaksymisEsitys({ oid, versio: 2, muokattavaHyvaksymisEsitys: muokattavaHyvaksymisEsitysInput });
+    // Haetaan tallentamisen jälkeen S3:ssa olevat tiedostot
+    const files = await getYllapitoFilesUnderPath(`yllapito/tiedostot/projekti/${oid}`);
+    // Poistettujen tiedostojen tulisi olla poistunut S3:sta, ja uusi LadattuTiedosto on lisätty sinne.
+    expect(files.sort()).to.eql(
+      [
+        `yllapito/tiedostot/projekti/Testi1/muokattava_hyvaksymisesitys/muuAineistoKoneelta/${adaptFileName(
+          "muuAineistoKoneelta äöå 3.png"
+        )}`,
+        ...TEST_HYVAKSYMISESITYS_FILES.map(({ path }) => `yllapito/tiedostot/projekti/Testi1/muokattava_hyvaksymisesitys/${path}`),
+      ].sort()
+    );
+    // Varmistetaan, että DynamoDB:ssä on oikeat tiedostot mainittuna
+    const projektiAfter = await getProjektiFromDB(oid);
+    const expectedMuokattavaHyvaksymisEsitys = {
+      ...TEST_HYVAKSYMISESITYS,
+      muuAineistoVelhosta: [
+        {
+          dokumenttiOid: "muuAineistoVelhostaDokumenttiOid",
+          nimi: "muuAineistoVelhosta äöå .png",
+          uuid: "muuAineistoVelhosta-uuid",
+          lisatty: "2022-01-02T02:00:00+02:00",
+        },
+        // Tähän tiedostoon on viite Dynamossa, vaikka sitä ei olisi S3:ssa vielä
+        {
+          dokumenttiOid: "muuAineistoVelhostaDokumenttiOid3",
+          nimi: "muuAineistoVelhosta äöå 3.png",
+          uuid: "muuAineistoVelhosta-uuid3",
+          lisatty: "2022-01-03T02:00:00+02:00",
+        },
+      ],
+      muuAineistoKoneelta: [
+        {
+          nimi: "muuAineistoKoneelta äöå .png",
+          uuid: "muuAineistoKoneelta-esitys-uuid",
+          lisatty: "2022-01-02T02:00:00+02:00",
+        },
+        {
+          nimi: "muuAineistoKoneelta äöå 3.png",
+          uuid: "muuAineistoKoneelta-esitys-uuid3",
+          lisatty: "2022-01-03T02:00:00+02:00",
+        },
+      ],
+    };
+    expect(projektiAfter.muokattavaHyvaksymisEsitys).to.eql({
+      ...omit(expectedMuokattavaHyvaksymisEsitys, ["muokkaaja", "vastanottajat"]),
+      muokkaaja: "theadminuid",
+      tila: API.HyvaksymisTila.MUOKKAUS,
+      vastaanottajat: [{ sahkoposti: "vastaanottaja@sahkoposti.fi" }],
+    });
   });
 
   it("onnistuu, jos ei ole muokattavaa eikä julkaistua hyväksymisesitystä", async () => {
