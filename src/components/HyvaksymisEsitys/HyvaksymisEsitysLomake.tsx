59f073f46b966c3a10d911b5e2eb2e7766bc8724
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-05-14 13:39:17 +0300

    Hyväksymisesityksen lomakkeen perustaminen

--- src/components/HyvaksymisEsitys/HyvaksymisEsitysLomake.tsx
+++ src/components/HyvaksymisEsitys/HyvaksymisEsitysLomake.tsx
@@ -1,5 +1,26 @@
-import { HyvaksymisEsityksenTiedot } from "@services/api";
+import { HyvaksymisEsityksenTiedot, TallennaHyvaksymisEsitysInput } from "@services/api";
+import { useMemo } from "react";
+import { FormProvider, UseFormProps, useForm } from "react-hook-form";
+import getDefaultValuesForForm from "./getDefaultValuesForForm";
 
 export default function HyvaksymisEsitysLomake({ hyvaksymisEsityksenTiedot }: { hyvaksymisEsityksenTiedot: HyvaksymisEsityksenTiedot }) {
-  return <></>;
+  const defaultValues: TallennaHyvaksymisEsitysInput = useMemo(
+    () => getDefaultValuesForForm(hyvaksymisEsityksenTiedot),
+    [hyvaksymisEsityksenTiedot]
+  );
+
+  const formOptions: UseFormProps<TallennaHyvaksymisEsitysInput> = {
+    //resolver: yupResolver(hyvaksymisEsitysSchema, { abortEarly: false, recursive: true }),
+    defaultValues,
+    mode: "onChange",
+    reValidateMode: "onChange",
+    //context: { hyvaksymisEsityksenTiedot, validationMode },
+  };
+
+  const useFormReturn = useForm<TallennaHyvaksymisEsitysInput>(formOptions);
+  return (
+    <FormProvider {...useFormReturn}>
+      <form></form>
+    </FormProvider>
+  );
 }
