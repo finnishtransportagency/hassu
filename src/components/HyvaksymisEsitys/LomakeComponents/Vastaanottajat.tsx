bd02cc2117112afcddc71fe10a96077e043ef762
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-05-17 13:34:20 +0300

    Vastaanottajat korjaus

--- src/components/HyvaksymisEsitys/LomakeComponents/Vastaanottajat.tsx
+++ src/components/HyvaksymisEsitys/LomakeComponents/Vastaanottajat.tsx
@@ -1,30 +1,40 @@
 import Button from "@components/button/Button";
+import IconButton from "@components/button/IconButton";
 import TextInput from "@components/form/TextInput";
 import SectionContent from "@components/layout/SectionContent";
 import { TallennaHyvaksymisEsitysInput } from "@services/api";
 import { ReactElement } from "react";
-import { Controller, useFormContext } from "react-hook-form";
+import { useFieldArray, useFormContext } from "react-hook-form";
 
 export default function Vastaanottajat(): ReactElement {
-  const { control, watch, setValue } = useFormContext<TallennaHyvaksymisEsitysInput>();
-  const vastaanottajat = watch("muokattavaHyvaksymisEsitys.vastaanottajat");
+  const { control, register } = useFormContext<TallennaHyvaksymisEsitysInput>();
+  const { fields, remove, append } = useFieldArray({ name: "muokattavaHyvaksymisEsitys.vastaanottajat", control });
 
   return (
     <SectionContent>
       <h3 className="vayla-subtitle">Hyväksymisesityksen vastaanottajat</h3>
 
-      {(vastaanottajat?.length ? vastaanottajat : [""]).map((_vastaanottaja, index) => (
-        <Controller
-          key={index}
-          control={control}
-          name={`muokattavaHyvaksymisEsitys.vastaanottajat.${index}`}
-          render={({ field }) => <TextInput label="Sähköpostiosoite" {...field} />}
-        />
+      {fields.map((field, index) => (
+        <div key={field.id}>
+          <TextInput
+            label="Sähköpostiosoite"
+            {...register(`muokattavaHyvaksymisEsitys.vastaanottajat.${index}.sahkoposti`, { shouldUnregister: true })}
+          />
+          {!!index && (
+            <IconButton
+              type="button"
+              onClick={() => {
+                remove(index);
+              }}
+              icon="trash"
+            />
+          )}
+        </div>
       ))}
       <Button
         onClick={(event) => {
           event.preventDefault();
-          setValue("muokattavaHyvaksymisEsitys.vastaanottajat", (vastaanottajat ?? []).concat([""]));
+          append({ sahkoposti: "" });
         }}
         type="button"
         id="lisaa_uusi_vastaanottaja"
