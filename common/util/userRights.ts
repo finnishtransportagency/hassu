38a5d7676056bf16cdc900de7f5b4431f7c9f811
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-05-21 12:29:37 +0300

    luo lintterin avuksi tyyppi. Uudet fragment typesit

--- common/util/userRights.ts
+++ common/util/userRights.ts
@@ -14,16 +14,23 @@ export function userHasAccessToProjekti({
   return !!kayttaja?.uid && !!projekti?.kayttoOikeudet?.some(({ kayttajatunnus }) => kayttaja.uid === kayttajatunnus);
 }
 
+/**
+ * Tämä tyyppi on luotu, koska lintterin kanssa oli jotain ihme häikkää
+ */
+type GenKayttoOikeus = {
+  kayttajatunnus: string;
+  tyyppi: string;
+};
 export function userIsProjectManagerOrSubstitute({
   kayttaja,
   projekti,
 }: {
   kayttaja?: NykyinenKayttaja;
   projekti?: Pick<Projekti, "kayttoOikeudet"> | Pick<DBProjekti, "kayttoOikeudet">;
-}) {
+}): boolean {
   return (
     !!kayttaja?.uid &&
-    !!projekti?.kayttoOikeudet?.find(
+    !!(projekti?.kayttoOikeudet as GenKayttoOikeus[] | undefined | null)?.find(
       ({ kayttajatunnus, tyyppi }) =>
         kayttaja.uid === kayttajatunnus && (tyyppi === KayttajaTyyppi.PROJEKTIPAALLIKKO || tyyppi === KayttajaTyyppi.VARAHENKILO)
     )
