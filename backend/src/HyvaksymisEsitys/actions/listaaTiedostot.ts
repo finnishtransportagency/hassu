5b5acdb3e65943c0e6cfe019314308b71af1e20c
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-05-22 15:49:28 +0300

    vastuuorganisaatio = suunnittelusta vastaava viranomainen

--- backend/src/HyvaksymisEsitys/actions/listaaTiedostot.ts
+++ backend/src/HyvaksymisEsitys/actions/listaaTiedostot.ts
@@ -48,7 +48,7 @@ export default async function listaaHyvaksymisEsityksenTiedostot({
       aineistopaketti,
       suunnitelmanNimi: projekti.velho!.nimi,
       asiatunnus: projekti.velho!.asiatunnusVayla,
-      vastuuorganisaatio: projekti.velho!.toteuttavaOrganisaatio,
+      vastuuorganisaatio: projekti.velho!.suunnittelustaVastaavaViranomainen,
       laskutustiedot: adaptLaskutustiedotToAPI(hyvaksymisEsitys.laskutustiedot),
       poistumisPaiva: hyvaksymisEsitys.poistumisPaiva,
       projektipaallikonYhteystiedot: adaptProjektiKayttajaJulkinen(projari),
