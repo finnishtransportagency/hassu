4bd134bd41dec0dc09cc552578786b7f91bc6841
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-05-22 14:45:33 +0300

    TODO-otsikoita hyväksymisesityksen aineistosivulle

--- src/components/HyvaksymisEsitys/AineistoPage.tsx
+++ src/components/HyvaksymisEsitys/AineistoPage.tsx
@@ -5,9 +5,11 @@ import { KunnallinenLadattavaTiedosto, LadattavaTiedosto, ProjektiKayttajaJulkin
 import { formatDate } from "hassu-common/util/dateUtils";
 import DownloadIcon from "@mui/icons-material/Download";
 import ButtonLink from "@components/button/ButtonLink";
-import { H1, H2 } from "@components/Headings";
+import { H1, H2, H3, H4 } from "@components/Headings";
 import Notification, { NotificationType } from "@components/notification/Notification";
 import SuunnittelmaLadattavatTiedostotAccordion from "@components/LadattavatTiedostot/SuunnitelmaAccordion";
+import { Stack } from "@mui/material";
+import SectionContent from "@components/layout/SectionContent";
 
 type Props = {
   projektinNimi?: string | null;
@@ -40,16 +42,47 @@ export default function HyvaksymisEsitysAineistoPage(props: Props): ReactElement
           välilehteen yksi kerrallaan.
         </Notification>
       )}
-      <p>
-        Huomioi, että tämä sisältö on tarkasteltavissa <b>{formatDate(poistumisPaiva)}</b> asti, jonka jälkeen sisältö poistuu näkyvistä.
-      </p>
-      <Section noDivider>
-        {suunnitelma?.length && <H2>Suunnitelma</H2>}
+      <Section>
+        <p>
+          Huomioi, että tämä sisältö on tarkasteltavissa <b>{formatDate(poistumisPaiva)}</b> asti, jonka jälkeen sisältö poistuu näkyvistä.
+        </p>
+        <SectionContent>
+          <H4>Pyydetään kiireellistä käsittelyä: //TODO</H4>
+        </SectionContent>
+        <SectionContent>
+          <H4>Lisätietoa vastaanottajalle</H4>
+          <p>TODO</p>
+        </SectionContent>
+        <SectionContent>
+          <H4>Laskutustiedot hyväksymismaksua varten</H4>
+          <Stack>TODO</Stack>
+        </SectionContent>
+        <SectionContent>
+          <H4>Yhteystiedot</H4>
+          <p>TODO</p>
+        </SectionContent>
+      </Section>
+
+      <Section>
+        <H2>Hyväksymisesityksen aineisto</H2>
+        <H3>Hyväksymisesitys</H3>
+        <div>TODO</div>
+        <H3>Suunnitelma</H3>
         <SuunnittelmaLadattavatTiedostotAccordion
           kategoriat={aineistoKategoriat.listKategoriat()}
           aineistot={suunnitelma}
           esikatselu={!!props.esikatselu}
         />
+      </Section>
+      <Section>
+        <H2>Vuorovaikutus</H2>
+        <H3>TODO Muistutukset</H3>
+        <H3>TODO Lausunnot</H3>
+        <H3>TODO Maanomistajaluettelo</H3>
+        <H3>TODO Kuulutukset ja kutsu vuorovaikutukseen</H3>
+      </Section>
+      <Section>
+        <H2>TODO Muu tekninen aineisto</H2>
       </Section>
       {aineistopaketti && (
         <Section noDivider>
