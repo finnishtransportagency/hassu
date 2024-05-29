a23af4da0c47a21d0742642bbe266661cb0b6972
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-05-29 16:57:34 +0300

    hyv.es. linkin katselussa pitää olla aineistokäännökset

--- i18n.js
+++ i18n.js
@@ -27,6 +27,7 @@ module.exports = {
     "/yllapito/projekti/[oid]/suunnittelu": ["suunnittelu"],
     "/yllapito/projekti/[oid]/suunnittelu/vuorovaikuttaminen": ["suunnittelu"],
     "/yllapito/projekti/[oid]/hyvaksymisesitys": ["aineisto"],
+    "/suunnitelma/[oid]/hyvaksymisesitys": ["aineisto"],
   },
   ignoreRoutes: ["/api/"],
   loadLocaleFrom: (lang, ns) =>
