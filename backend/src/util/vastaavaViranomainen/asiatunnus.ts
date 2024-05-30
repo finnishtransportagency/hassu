594a2ec96174f116df4d1492e15fd1728b19550f
tomi korkalainen <tomi.korkalainen@cgi.com>
2024-05-30 13:28:24 +0300

    löyhennä asiatunnuksessa käytettyä tyypitystä

--- backend/src/util/vastaavaViranomainen/asiatunnus.ts
+++ backend/src/util/vastaavaViranomainen/asiatunnus.ts
@@ -1,7 +1,9 @@
 import * as API from "hassu-common/graphql/apiModel";
 import { Velho } from "../../database/model/common";
 
-export const vastaavanViranomaisenAsiatunnus = (velho: Velho | API.Velho) =>
+export const vastaavanViranomaisenAsiatunnus = (
+  velho: Pick<Velho, "suunnittelustaVastaavaViranomainen" | "asiatunnusELY" | "asiatunnusVayla">
+) =>
   velho.suunnittelustaVastaavaViranomainen === API.SuunnittelustaVastaavaViranomainen.VAYLAVIRASTO
     ? velho.asiatunnusVayla
     : velho.asiatunnusELY;
