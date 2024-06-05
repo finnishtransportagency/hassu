8053c78404e6a2e343e6fd710d1f3bbfc1a0600f
Petri Hakala <92077410+hakalap@users.noreply.github.com>
2024-06-05 15:57:46 +0300

    fix: Korjaa tuotannon sso logout (#1191)

--- src/pages/api/slo.ts
+++ src/pages/api/slo.ts
@@ -1,23 +1,9 @@
-import { SSM } from "@aws-sdk/client-ssm";
 import { NextApiRequest, NextApiResponse } from "next";
-
-const ssm = new SSM({ region: "eu-west-1" });
-
-async function getParameter(name: string, envVariable: string): Promise<string> {
-  if (process.env[envVariable]) {
-    return process.env[envVariable] as string;
-  }
-  const value = (await ssm.getParameter({ Name: name, WithDecryption: true })).Parameter?.Value;
-  if (value) {
-    return value;
-  }
-  throw new Error("Getting parameter " + name + " failed");
-}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const currenturl = new URL(req.headers.referer!);
   const keycloakClientId = process.env.KEYCLOAK_CLIENT_ID!;
-  const redirect_uri = new URL(await getParameter("KeycloakDomain", "KEYCLOAK_DOMAIN"));
+  const redirect_uri = new URL(process.env.KEYCLOAK_DOMAIN!);
   redirect_uri.pathname = "/keycloak/auth/realms/suomifi/protocol/openid-connect/logout";
   redirect_uri.searchParams.set("client_id", keycloakClientId);
   redirect_uri.searchParams.set("post_logout_redirect_uri", currenturl.toString());
