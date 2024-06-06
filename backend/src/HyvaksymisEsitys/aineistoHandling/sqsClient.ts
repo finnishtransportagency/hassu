70e69e5db62e6a22d347cc8b87fc797a0f89d1a9
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-06-06 16:20:37 +0300

    sqsClient on luokka

--- backend/src/HyvaksymisEsitys/aineistoHandling/sqsClient.ts
+++ backend/src/HyvaksymisEsitys/aineistoHandling/sqsClient.ts
@@ -2,10 +2,22 @@ import { SQS, SendMessageRequest } from "@aws-sdk/client-sqs";
 import { produce } from "../../aws/produce";
 import { log } from "../../logger";
 import { SqsEvent } from "./sqsEvent";
+import { config } from "../../config";
 
-export const getSQS = (): SQS => {
-  return produce<SQS>("sqs-hyvaksymisesitys", () => new SQS({ region: "eu-west-1" }));
-};
+export class SqsClient {
+  static getSQS = (): SQS => {
+    return produce<SQS>("sqs-hyvaksymisesitys", () => new SQS({ region: "eu-west-1" }));
+  };
+
+  static async addEventToSqsQueue(params: SqsEvent, retry?: boolean) {
+    const messageParams = createMessageParams(params, retry);
+    if (messageParams) {
+      log.info("addEventToHyvaksymisesitysSqsQueue", { messageParams });
+      const result = await SqsClient.getSQS().sendMessage(messageParams);
+      log.info("addEventToHyvaksymisesitysSqsQueue", { result });
+    }
+  }
+}
 
 function createMessageParams(params: SqsEvent, retry?: boolean) {
   if (retry) {
@@ -23,17 +35,8 @@ function createMessageParams(params: SqsEvent, retry?: boolean) {
   const messageParams: SendMessageRequest = {
     MessageGroupId: params.oid,
     MessageBody: JSON.stringify({ timestamp: Date.now(), ...params }),
-    QueueUrl: "", //TODO: config.hyvaksymisEsitysSqsUrl,
+    QueueUrl: config.hyvaksymisesitysSqsUrl,
     DelaySeconds: retry ? 60 : undefined,
   };
   return messageParams;
-}
-
-export async function addEventToSqsQueue(params: SqsEvent, retry?: boolean) {
-  const messageParams = createMessageParams(params, retry);
-  if (messageParams) {
-    log.info("addEventToSqsQueue", { messageParams });
-    const result = await getSQS().sendMessage(messageParams);
-    log.info("addEventToSqsQueue", { result });
-  }
 }
