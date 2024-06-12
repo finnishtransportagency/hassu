9c61dc7435b2d7b2560b2d110a0173515ee22d29
Valhe Kouneli <valhe.kouneli@cgi.com>
2024-06-12 16:25:06 +0300

    Näytä hyväksymisesitystiedostolle tuotu-aikaleima

--- src/components/HyvaksymisEsitys/LomakeComponents/HyvaksymisEsitysTiedosto.tsx
+++ src/components/HyvaksymisEsitys/LomakeComponents/HyvaksymisEsitysTiedosto.tsx
@@ -48,6 +48,7 @@ export default function HyvaksymisEsitysTiedosto({ tiedostot }: { tiedostot?: La
           registerNimi={registerNimi}
           ladattuTiedosto
           noHeaders
+          showTuotu
         />
       )}
       <input
