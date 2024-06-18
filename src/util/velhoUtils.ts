ac7fcb0e792aa2f9c41494491bc1b620c0c35b53
Ville Ruuskanen <81217553+vilruus@users.noreply.github.com>
2024-06-18 11:31:19 +0300

    Fix: HASSUYP-347 Kuulutuksen ja kutsun lähettäminen/hyväksyntä vaativat velhon kunnan ja maakunnan.  (#1205)
    * added project notification
    
    * vuorovaikutus
    
    * removed unused imports
    
    * removed unused imports
    
    * lisätty piste
    
    * formatted

---
+++ src/util/velhoUtils.ts
@@ -0,0 +1,9 @@
+import { Projekti } from "hassu-common/graphql/apiModel";
+
+export const isKuntatietoMissing = (projekti: Projekti) => {
+  return !projekti.velho.maakunnat || !projekti.velho.kunnat;
+};
+
+export const getVelhoUrl = (oid: Projekti["oid"]) => {
+  return process.env.NEXT_PUBLIC_VELHO_BASE_URL + "/projektit/oid-" + oid;
+};
