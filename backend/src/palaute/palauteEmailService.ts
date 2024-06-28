9e39c33644ecc6bbc3208563dd842a42f0342da3
Petri Hakala <92077410+hakalap@users.noreply.github.com>
2024-06-28 10:59:33 +0300

    fix: HASSUYP-119: Kysymyksen tai palautteen jätöstä kansalaiselle vahvistusviesti sähköpostiin (#1228)

--- backend/src/palaute/palauteEmailService.ts
+++ backend/src/palaute/palauteEmailService.ts
@@ -2,7 +2,10 @@ import { projektiDatabase } from "../database/projektiDatabase";
 import { DBProjekti } from "../database/model/projekti";
 import { createNewFeedbackAvailableEmail } from "../email/emailTemplates";
 import { emailClient } from "../email/email";
-import { log } from "../logger";
+import { auditLog, log } from "../logger";
+import { Palaute } from "../database/model";
+import { nyt } from "../util/dateUtil";
+import { Kieli } from "hassu-common/graphql/apiModel";
 
 class PalauteEmailService {
   async sendEmailsToPalautteidenVastaanottajat(projekti: DBProjekti) {
@@ -17,6 +20,51 @@ class PalauteEmailService {
           }
         }
       }
+    }
+  }
+
+  async sendEmailToFeedbackSender(projekti: DBProjekti, palaute: Palaute) {
+    const vastaanotettu = nyt().format("DD.MM.YYYY HH:mm");
+    const ruotsiksi = projekti.kielitiedot?.ensisijainenKieli === Kieli.RUOTSI || projekti.kielitiedot?.toissijainenKieli === Kieli.RUOTSI;
+    const liite = palaute.liite ? palaute.liite.substring(palaute.liite.lastIndexOf("/") + 1) : "";
+    let text = `Palaute vastaanotettu
+${vastaanotettu}
+Nimi
+${palaute.etunimi ?? ""} ${palaute.sukunimi ?? ""}
+Sähköposti
+${palaute.sahkoposti ?? ""}
+Puhelinnumero
+${palaute.puhelinnumero ?? ""}
+
+Suunnitelman nimi
+${projekti.velho?.nimi ?? ""}
+Palaute
+${palaute.kysymysTaiPalaute ?? ""}
+${liite ? "Sähköpostiviestin liite\n" + liite : ""}
+`
+    if (ruotsiksi) {
+      text += text = `
+Respons mottagen
+${vastaanotettu}
+Namn
+${palaute.etunimi ?? ""} ${palaute.sukunimi ?? ""}
+E-post
+${palaute.sahkoposti ?? ""}
+Telefonnummer
+${palaute.puhelinnumero ?? ""}
+
+Planens namn
+${projekti.kielitiedot?.projektinNimiVieraskielella ?? ""}
+Respons
+${palaute.kysymysTaiPalaute ?? ""}
+${liite ? "Bifogad fil\n" + liite : ""}
+`
+    }
+    if (palaute.sahkoposti) {
+      await emailClient.sendEmail({ subject: `Vahvistus palautteen jättämisestä Valtion liikenneväylien suunnittelu -järjestelmän kautta${ruotsiksi ? " / Bekräftelse av att respons lämnats via systemet Planering av statens trafikleder" : ""}`, text, to: palaute.sahkoposti });
+      auditLog.info("Palautteen antajalle lähetetty sähköpostikuittaus osoitteeseen " + palaute.sahkoposti);
+    } else {
+      auditLog.info("Palautteen antajalle ei lähetetty sähköpostikuittausta");
     }
   }
 
