50a1d11d5318ebbe0e30cdeb6c1113168c2a762e
Ville Ruuskanen <81217553+vilruus@users.noreply.github.com>
2024-08-02 14:46:56 +0300

    feat: Hassuyp-132 virkamiehen projektitaulukkoon hovertyylit (#1249)
    * added hover style for project table
    
    * changes only to virkamies projekti table
    
    * removed commented code
    
    * table takes css object as meta attribute for row customization purposes

--- src/components/table/HassuTable.tsx
+++ src/components/table/HassuTable.tsx
@@ -199,7 +199,6 @@ export function BodyContent<T>(
     }
 ) {
   const actualRows = props.table.getRowModel().rows;
-
   const rowVirtualizer = props?.virtualizer;
 
   const virtualRows = rowVirtualizer?.getVirtualItems();
@@ -354,6 +353,7 @@ export const BasicRow = forwardRef(BasicRowWithoutStyles);
 function BasicRowWithoutStyles<T>({ row, table, gridTemplateColumns, index }: RowProps<T>, ref: ForwardedRef<HTMLDivElement>) {
   const meta = table.options.meta;
   const href = meta?.rowHref?.(row);
+  const additionalStyles = meta?.customRowStyles;
   const onClick: React.MouseEventHandler<HTMLDivElement> | undefined = useMemo(() => {
     return meta?.rowOnClick ? (event) => meta.rowOnClick?.(event, row) : undefined;
   }, [meta, row]);
@@ -396,6 +396,15 @@ function BasicRowWithoutStyles<T>({ row, table, gridTemplateColumns, index }: Ro
     [row.id, originalIndex, onDragAndDrop]
   );
 
+  const bodyTrWrapperStyles = {
+    boxShadow: isDragging
+      ? "inset -16px 0px 14px -8px #FFFFFF, inset 16px 0px 14px -8px #FFFFFF, inset 0px 11px 8px -10px #999999, inset 0px -11px 8px -10px #999999"
+      : undefined,
+    borderBottom: !isDragging ? "2px #49c2f1 solid" : "unset",
+    zIndex: isDragging ? 1 : "unset",
+    backgroundColor: !isDragging && index % 2 ? "#F8F8F8" : "#FFFFFF",
+  };
+
   return (
     <TableDragConnectSourceContext.Provider value={dragRef}>
       <ConditionalWrapper
@@ -411,14 +420,7 @@ function BasicRowWithoutStyles<T>({ row, table, gridTemplateColumns, index }: Ro
             previewRef(node);
             typeof ref === "function" && ref(node);
           }}
-          sx={{
-            boxShadow: isDragging
-              ? "inset -16px 0px 14px -8px #FFFFFF, inset 16px 0px 14px -8px #FFFFFF, inset 0px 11px 8px -10px #999999, inset 0px -11px 8px -10px #999999"
-              : undefined,
-            borderBottom: !isDragging ? "2px #49c2f1 solid" : "unset",
-            zIndex: isDragging ? 1 : "unset",
-            backgroundColor: !isDragging && index % 2 ? "#F8F8F8" : "#FFFFFF",
-          }}
+          sx={!additionalStyles ? bodyTrWrapperStyles : { ...bodyTrWrapperStyles, ...additionalStyles }}
           data-index={index}
           onClick={onClick}
           as={href ? "a" : undefined}
