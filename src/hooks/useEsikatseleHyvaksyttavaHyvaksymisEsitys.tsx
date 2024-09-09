146a6f0282ae617bbff370af8cd79cd9f905ba37
tomi korkalainen <tomi.korkalainen@cgi.com>
2024-09-09 09:35:08 +0300

    feat: HASSUYP-457 Hyväksymisesityksen esikatselu hyväksymisnäkymään

---
+++ src/hooks/useEsikatseleHyvaksyttavaHyvaksymisEsitys.tsx
@@ -0,0 +1,25 @@
+import useSWR from "swr";
+import { apiConfig } from "@services/api";
+import { useRouter } from "next/router";
+import useApi from "./useApi";
+import { API } from "@services/api/commonApi";
+
+export function useEsikatseleHyvaksyttavaHyvaksymisEsitys() {
+  const api = useApi();
+  const { query } = useRouter();
+  const oid = typeof query.oid === "string" ? query.oid : undefined;
+
+  const esikatseleHyvaksymisEsitysLoader = getEsikatseleHyvaksymisEsitysLoader(api);
+
+  console.log("bong");
+  console.log("oid:" + oid);
+
+  return useSWR([apiConfig.esikatseleHyvaksyttavaHyvaksymisEsityksenTiedostot.graphql, oid], esikatseleHyvaksymisEsitysLoader);
+}
+
+const getEsikatseleHyvaksymisEsitysLoader = (api: API) => async (_query: string, oid: string) => {
+  if (!oid) {
+    return null;
+  }
+  return await api.esikatseleHyvaksyttavaHyvaksymisEsityksenTiedostot(oid);
+};
