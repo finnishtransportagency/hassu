645eadb28f5f00977d67d83b2a0906b212320b2c
tomi korkalainen <tomi.korkalainen@cgi.com>
2024-09-27 13:14:44 +0300

    muuta päivämäärävertailu isBefore -> 'isNotAfter' tyyppiseksi

--- backend/src/HyvaksymisEsitys/aineistoNewIsReady.ts
+++ backend/src/HyvaksymisEsitys/aineistoNewIsReady.ts
@@ -2,5 +2,5 @@ import dayjs from "dayjs";
 import { AineistoNew } from "../database/model/common";
 
 export function aineistoNewIsReady(aineisto: AineistoNew, aineistoHandledAt: string | undefined | null): boolean {
-  return !!aineistoHandledAt && !!aineisto.lisatty && dayjs(aineisto.lisatty).isBefore(dayjs(aineistoHandledAt));
+  return !!aineistoHandledAt && !!aineisto.lisatty && !dayjs(aineisto.lisatty).isAfter(dayjs(aineistoHandledAt));
 }
