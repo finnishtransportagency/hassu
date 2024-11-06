affca5422b6170edf06f814db12f1ee385308741
tomi korkalainen <tomi.korkalainen@cgi.com>
2024-11-06 14:30:58 +0200

    sulje Popper esc-painalluksella

--- src/components/projekti/tiedottaminen/ViimeisinLahetysHeader.tsx
+++ src/components/projekti/tiedottaminen/ViimeisinLahetysHeader.tsx
@@ -1,23 +1,40 @@
-import React, { useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { ClickAwayListener, IconButton, Popper, styled, SvgIcon } from "@mui/material";
 import ContentSpacer from "@components/layout/ContentSpacer";
 import { Stack } from "@mui/system";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faEnvelope, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import Notification from "@components/notification/Notification";
 
 export function ViimeisinLahetysHeader() {
   const [open, setOpen] = React.useState(false);
 
-  const handleClick = () => {
+  const handleClick = useCallback(() => {
     setOpen((previousOpen) => !previousOpen);
-  };
+  }, []);
 
-  const handleTooltipClose = () => {
+  const handleTooltipClose = useCallback(() => {
     setOpen(false);
-  };
+  }, []);
+
   const buttonRef = useRef<HTMLButtonElement>(null);
 
   const id = open ? "viimeisin-lahetysaika-tooltip" : undefined;
+
+  useEffect(() => {
+    if (!open) {
+      return undefined;
+    }
+    function handleKeyDown(nativeEvent: KeyboardEvent) {
+      if (nativeEvent.key === "Escape") {
+        handleTooltipClose();
+      }
+    }
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleTooltipClose, open]);
 
   return (
     <Stack rowGap={2} columnGap={2} alignItems="end" direction="row">
@@ -31,25 +48,27 @@ export function ViimeisinLahetysHeader() {
           </IconButton>
           {/* @ts-ignore */}
           <NotificationPopper id={id} anchorEl={buttonRef.current} disablePortal={true} open={open}>
-            <ContentSpacer gap={2}>
-              <b>Selitteet</b>
-              <SeliteLista>
-                <dt>-</dt>
-                <dd>Ei tiedoituksia</dd>
-                <dt>
-                  <FontAwesomeIcon icon={faEnvelope} color="#207a43" />
-                </dt>
-                <dd>Tiedotettu</dd>
-                <dt>
-                  <FontAwesomeIcon icon={faCheck} color="#207a43" />
-                </dt>
-                <dd>Tiedotettu toisesta kiinteistöstä tai muistutuksesta</dd>
-                <dt>
-                  <FontAwesomeIcon icon={faTriangleExclamation} color="#f10e0e" />
-                </dt>
-                <dd>Tiedottaminen epäonnistunut</dd>
-              </SeliteLista>
-            </ContentSpacer>
+            <Notification>
+              <ContentSpacer gap={2}>
+                <b>Selitteet</b>
+                <SeliteLista>
+                  <dt>-</dt>
+                  <dd>Ei tiedoituksia</dd>
+                  <dt>
+                    <FontAwesomeIcon icon={faEnvelope} color="#207a43" />
+                  </dt>
+                  <dd>Tiedotettu</dd>
+                  <dt>
+                    <FontAwesomeIcon icon={faCheck} color="#207a43" />
+                  </dt>
+                  <dd>Tiedotettu toisesta kiinteistöstä tai muistutuksesta</dd>
+                  <dt>
+                    <FontAwesomeIcon icon={faTriangleExclamation} color="#f10e0e" />
+                  </dt>
+                  <dd>Tiedottaminen epäonnistunut</dd>
+                </SeliteLista>
+              </ContentSpacer>
+            </Notification>
           </NotificationPopper>
         </div>
       </ClickAwayListener>
@@ -57,15 +76,7 @@ export function ViimeisinLahetysHeader() {
   );
 }
 const NotificationPopper = styled(Popper)(({ theme }) => ({
-  borderColor: "#999999",
-  borderStyle: "solid",
-  borderWidth: "1px",
-  borderRadius: "0px",
   color: theme.palette.text.primary,
-  backgroundColor: "#F8F8F8",
-  padding: theme.spacing(5),
-  fontSize: "1rem",
-  lineHeight: 1.5,
   maxWidth: "100%",
   zIndex: theme.zIndex.tooltip,
   fontWeight: 400,
@@ -74,6 +85,7 @@ const NotificationPopper = styled(Popper)(({ theme }) => ({
     pointerEvents: "none",
   },
 }));
+
 const SeliteLista = styled("dl")(({ theme }) => ({
   display: "grid",
   gridTemplateColumns: "min-content 1fr",
