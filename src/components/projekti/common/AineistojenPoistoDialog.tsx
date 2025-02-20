e743ed20687a1ee9d261a8c79b352d1e1d235c97
riikkanaCGI <riikka.naamanka@cgi.com>
2025-02-20 13:03:56 +0200

    poistodialogin vaihtoehtoiset tekstit

--- src/components/projekti/common/AineistojenPoistoDialog.tsx
+++ src/components/projekti/common/AineistojenPoistoDialog.tsx
@@ -3,17 +3,40 @@ import Button from "@components/button/Button";
 import HassuDialog from "@components/HassuDialog";
 import { DialogActions, DialogContent } from "@mui/material";
 
+type AineistoTyyppi = "esittelyaineistot" | "suunnitelmaluonnokset";
 interface Props {
   dialogiOnAuki: boolean;
   onClose: () => void;
   onAccept: () => void;
+  aineistoTyyppi?: AineistoTyyppi;
 }
 
-export default function AineistojenPoistoDialog({ dialogiOnAuki, onClose, onAccept }: Props): ReactElement {
+export default function AineistojenPoistoDialog({ dialogiOnAuki, onClose, onAccept, aineistoTyyppi }: Props): ReactElement {
+  const getDialogContent = () => {
+    if (aineistoTyyppi === "esittelyaineistot") {
+      return (
+        <>
+          Olet poistamassa kaikki <strong>esittelyaineistot</strong>. Haluatko varmasti poistaa kaikki?
+        </>
+      );
+    } else if (aineistoTyyppi === "suunnitelmaluonnokset") {
+      return (
+        <>
+          Olet poistamassa kaikki <strong>suunnitelmaluonnokset</strong>. Haluatko varmasti poistaa kaikki?
+        </>
+      );
+    }
+    return (
+      <>
+        Olet poistamassa kaikki <strong>Projektivelhosta tuodut aineistot</strong>. Haluatko varmasti poistaa kaikki?
+      </>
+    );
+  };
+
   return (
-    <HassuDialog open={dialogiOnAuki} title="Vahvista kaikkien aineistojen poistaminen" onClose={onClose} maxWidth="sm">
+    <HassuDialog open={dialogiOnAuki} title="Vahvista aineistojen poistaminen" onClose={onClose} maxWidth="sm">
       <DialogContent>
-        <p>Olet poistamassa kaikki Velhosta tuodut aineistot. Haluatko varmasti poistaa kaikki?</p>
+        <p>{getDialogContent()}</p>
       </DialogContent>
       <DialogActions>
         <Button primary type="button" id="poista-kaikki-aineistot" onClick={onAccept}>
