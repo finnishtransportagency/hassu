3f618c1e203c57f20f013514ba8b773c7f0ac26c
tomi korkalainen <tomi.korkalainen@cgi.com>
2025-03-31 14:21:46 +0300

    fix: tee kovakoodattu versio haePalauteKyselyTiedot-toteutukseta

--- src/util/haePalauteKyselyTiedot.tsx
+++ src/util/haePalauteKyselyTiedot.tsx
@@ -23,26 +23,16 @@ export interface PalauteKyselyAvoinna extends PalauteKyselyTiedot {
 }
 
 export const haePalauteKyselyTiedot = (): PalauteKyselyAvoinna => {
-  // const envParam = process.env.NEXT_PUBLIC_PALAUTE_KYSELY_TIEDOT;
-  try {
-    // const palauteKyselyTiedot = JSON.parse(envParam);
-    const palauteKyselyTiedot = {
-      startDate: "2025-03-28",
-      endDate: "2025-04-30",
-      href: "https://link.webropolsurveys.com/S/93F78A20D9689AB2",
-    };
-    if (isValidPalautekyselyObject(palauteKyselyTiedot)) {
-      const NOW = today().format("YYYY-MM-DD");
-      const isActive = !(dayjs(NOW).isBefore(palauteKyselyTiedot.startDate) || dayjs(NOW).isAfter(palauteKyselyTiedot.endDate));
-      return {
-        isActive,
-        href: palauteKyselyTiedot.href,
-        startDate: palauteKyselyTiedot.startDate,
-        endDate: palauteKyselyTiedot.endDate,
-      };
+  const envParam = process.env.NEXT_PUBLIC_PALAUTE_KYSELY_TIEDOT;
+  if (envParam) {
+    try {
+      const palauteKyselyTiedot = JSON.parse(envParam);
+      if (isValidPalautekyselyObject(palauteKyselyTiedot)) {
+        return createPalauteKyselyAvoinna(palauteKyselyTiedot);
+      }
+    } catch (error) {
+      // unable to parse envParam, return the 'default' values below
     }
-  } catch (error) {
-    // unable to parse envParam, return the 'default' values below
   }
   return {
     isActive: false,
@@ -51,3 +41,23 @@ export const haePalauteKyselyTiedot = (): PalauteKyselyAvoinna => {
     endDate: "",
   };
 };
+
+export const haehardCodedPalauteKyselyTiedot = (): PalauteKyselyAvoinna => {
+  const palauteKyselyTiedot = {
+    startDate: "2025-03-28",
+    endDate: "2025-04-30",
+    href: "https://link.webropolsurveys.com/S/93F78A20D9689AB2",
+  };
+  return createPalauteKyselyAvoinna(palauteKyselyTiedot);
+};
+
+function createPalauteKyselyAvoinna(palauteKyselyTiedot: { startDate: string; endDate: string; href: string }) {
+  const NOW = today().format("YYYY-MM-DD");
+  const isActive = !(dayjs(NOW).isBefore(palauteKyselyTiedot.startDate) || dayjs(NOW).isAfter(palauteKyselyTiedot.endDate));
+  return {
+    isActive,
+    href: palauteKyselyTiedot.href,
+    startDate: palauteKyselyTiedot.startDate,
+    endDate: palauteKyselyTiedot.endDate,
+  };
+}
