049c8f151092de726a2c955077b9726528947f83
tomi korkalainen <tomi.korkalainen@cgi.com>
2025-03-31 14:09:13 +0300

    fix: Kovakoodaa palauteKyselyTiedot

--- src/util/haePalauteKyselyTiedot.tsx
+++ src/util/haePalauteKyselyTiedot.tsx
@@ -23,24 +23,26 @@ export interface PalauteKyselyAvoinna extends PalauteKyselyTiedot {
 }
 
 export const haePalauteKyselyTiedot = (): PalauteKyselyAvoinna => {
-  const envParam = process.env.NEXT_PUBLIC_PALAUTE_KYSELY_TIEDOT;
-  console.log(envParam);
-  if (envParam) {
-    try {
-      const palauteKyselyTiedot = JSON.parse(envParam);
-      if (isValidPalautekyselyObject(palauteKyselyTiedot)) {
-        const NOW = today().format("YYYY-MM-DD");
-        const isActive = !(dayjs(NOW).isBefore(palauteKyselyTiedot.startDate) || dayjs(NOW).isAfter(palauteKyselyTiedot.endDate));
-        return {
-          isActive,
-          href: palauteKyselyTiedot.href,
-          startDate: palauteKyselyTiedot.startDate,
-          endDate: palauteKyselyTiedot.endDate,
-        };
-      }
-    } catch (error) {
-      // unable to parse envParam, return the 'default' values below
+  // const envParam = process.env.NEXT_PUBLIC_PALAUTE_KYSELY_TIEDOT;
+  try {
+    // const palauteKyselyTiedot = JSON.parse(envParam);
+    const palauteKyselyTiedot = {
+      startDate: "2025-03-28",
+      endDate: "2025-04-30",
+      href: "https://link.webropolsurveys.com/S/93F78A20D9689AB2",
+    };
+    if (isValidPalautekyselyObject(palauteKyselyTiedot)) {
+      const NOW = today().format("YYYY-MM-DD");
+      const isActive = !(dayjs(NOW).isBefore(palauteKyselyTiedot.startDate) || dayjs(NOW).isAfter(palauteKyselyTiedot.endDate));
+      return {
+        isActive,
+        href: palauteKyselyTiedot.href,
+        startDate: palauteKyselyTiedot.startDate,
+        endDate: palauteKyselyTiedot.endDate,
+      };
     }
+  } catch (error) {
+    // unable to parse envParam, return the 'default' values below
   }
   return {
     isActive: false,
