97093f5a9d320a6bc34ec10561cba62cbcf2ae89
tomi korkalainen <tomi.korkalainen@cgi.com>
2025-03-31 12:25:43 +0300

    fix: lisää try catch kyselytietojen päättelyyn

--- src/util/haePalauteKyselyTiedot.tsx
+++ src/util/haePalauteKyselyTiedot.tsx
@@ -3,11 +3,11 @@ import dayjs from "dayjs";
 
 function isValidPalautekyselyObject(obj: any): obj is { startDate: string; endDate: string; href: string } {
   return (
-    typeof obj === 'object' &&
+    typeof obj === "object" &&
     obj !== null &&
-    typeof obj.startDate === 'string' &&
-    typeof obj.endDate === 'string' &&
-    typeof obj.href === 'string'
+    typeof obj.startDate === "string" &&
+    typeof obj.endDate === "string" &&
+    typeof obj.href === "string"
   );
 }
 
@@ -15,33 +15,37 @@ export interface PalauteKyselyTiedot {
   href: string | undefined;
   startDate: string;
   endDate: string;
-};
+}
 
 export interface PalauteKyselyAvoinna extends PalauteKyselyTiedot {
   href: string | undefined;
   isActive: boolean;
-};
+}
 
 export const haePalauteKyselyTiedot = (): PalauteKyselyAvoinna => {
   const envParam = process.env.NEXT_PUBLIC_PALAUTE_KYSELY_TIEDOT;
+  console.log(envParam);
   if (envParam) {
-    const palauteKyselyTiedot = JSON.parse(envParam);
-    if (isValidPalautekyselyObject(palauteKyselyTiedot)) {
-      const NOW = today().format("YYYY-MM-DD");
-      const isActive = !(dayjs(NOW).isBefore(palauteKyselyTiedot.startDate) || dayjs(NOW).isAfter(palauteKyselyTiedot.endDate));
-      return {
-        isActive,
-        href: palauteKyselyTiedot.href,
-        startDate: palauteKyselyTiedot.startDate, 
-        endDate: palauteKyselyTiedot.endDate
+    try {
+      const palauteKyselyTiedot = JSON.parse(envParam);
+      if (isValidPalautekyselyObject(palauteKyselyTiedot)) {
+        const NOW = today().format("YYYY-MM-DD");
+        const isActive = !(dayjs(NOW).isBefore(palauteKyselyTiedot.startDate) || dayjs(NOW).isAfter(palauteKyselyTiedot.endDate));
+        return {
+          isActive,
+          href: palauteKyselyTiedot.href,
+          startDate: palauteKyselyTiedot.startDate,
+          endDate: palauteKyselyTiedot.endDate,
+        };
       }
+    } catch (error) {
+      // unable to parse envParam, return the 'default' values below
     }
   }
   return {
     isActive: false,
     href: undefined,
-    startDate: '', 
-    endDate: ''
+    startDate: "",
+    endDate: "",
   };
 };
-
