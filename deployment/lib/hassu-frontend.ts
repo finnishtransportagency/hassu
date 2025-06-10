505137ac153a41c8c9750206882c99fd4396bb2a
Jani Aaltonen <jani.aaltonen@cgi.com>
2025-06-10 10:15:29 +0300

    nimetään yhdenmukaisesti

--- deployment/lib/hassu-frontend.ts
+++ deployment/lib/hassu-frontend.ts
@@ -335,7 +335,7 @@ export class HassuFrontendStack extends Stack {
         Fn.importValue("nextImageCachePolicyId")
       ) as CachePolicy;
 
-      const nextStaticsCachePolicy = CachePolicy.fromCachePolicyId(
+      const nextJsStaticsCachePolicy = CachePolicy.fromCachePolicyId(
         this,
         "nextJsStaticsCachePolicy",
         Fn.importValue("nextStaticsCachePolicyId")
@@ -363,7 +363,7 @@ export class HassuFrontendStack extends Stack {
           "/_next/*": {
             ...commonNextBehaviourOptions,
             allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
-            cachePolicy: nextStaticsCachePolicy,
+            cachePolicy: nextJsStaticsCachePolicy,
           },
           //"static/*": {
           //  ...commonNextBehaviourOptions,
@@ -373,7 +373,7 @@ export class HassuFrontendStack extends Stack {
           "/assets/*": {
             ...commonNextBehaviourOptions,
             allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
-            cachePolicy: nextStaticsCachePolicy,
+            cachePolicy: nextJsStaticsCachePolicy,
           },
           "/api/*": {
             ...commonNextBehaviourOptions,
