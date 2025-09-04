7ee792ae4d591ec4c941d8af3455c81647df620e
riikkanaCGI <riikka.naamanka@cgi.com>
2025-09-04 10:14:57 +0300

    tiedotteen leveyden korjaus

--- src/components/projekti/common/TiedoteNotification.tsx
+++ src/components/projekti/common/TiedoteNotification.tsx
@@ -127,7 +127,7 @@ export const TiedoteNotification = () => {
     <Notification
       sx={{
         whiteSpace: "pre-wrap",
-        width: "90%",
+        width: "80%",
         margin: "10px auto 10px",
         position: "relative",
         paddingRight: "40px",
