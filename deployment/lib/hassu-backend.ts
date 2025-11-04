4399b9441ce1d20132c676dc520aa87ac78ed61f
Jani Aaltonen <jani.aaltonen@cgi.com>
2025-11-04 10:52:35 +0200

    nosta AWS-Parameters-and-Secrets-Lambda-Extension versio 18 -> 21

--- deployment/lib/hassu-backend.ts
+++ deployment/lib/hassu-backend.ts
@@ -93,7 +93,7 @@ export class HassuBackendStack extends Stack {
       LayerVersion.fromLayerVersionArn(
         this,
         "paramLayer",
-        "arn:aws:lambda:eu-west-1:015030872274:layer:AWS-Parameters-and-Secrets-Lambda-Extension:18"
+        "arn:aws:lambda:eu-west-1:015030872274:layer:AWS-Parameters-and-Secrets-Lambda-Extension:21"
       ),
     ];
   }
