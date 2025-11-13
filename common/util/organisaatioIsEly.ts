a9988a6b52b2a3d013e30b295ccb9a606c1889ae
riikkanaCGI <riikka.naamanka@cgi.com>
2025-11-13 12:21:38 +0200

    Valtuushallinnasta tulevan organisaation tarkistaminen

--- common/util/organisaatioIsEly.ts
+++ common/util/organisaatioIsEly.ts
@@ -4,8 +4,8 @@ export function organisaatioIsEly(organisaatio: string | undefined | null): bool
   return organisaatio?.toUpperCase() === ELY_ORGANISAATIO_STRING;
 }
 
-export const EVK_ORGANISAATIO_STRING = "EVK";
+export const EVK_ORGANISAATIO_STRING = "Elinvoimakeskus";
 
 export function organisaatioIsEvk(organisaatio: string | undefined | null): boolean {
-  return organisaatio?.toUpperCase() === EVK_ORGANISAATIO_STRING;
+  return organisaatio?.toUpperCase() === EVK_ORGANISAATIO_STRING.toUpperCase();
 }
