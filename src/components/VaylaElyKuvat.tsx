fa2002728d7d78d638dc6ca20687201a195e7310
riikkanaCGI <riikka.naamanka@cgi.com>
2025-11-26 14:08:07 +0200

    evk-logo ei oikeuksia -sivulle

--- src/components/VaylaElyKuvat.tsx
+++ src/components/VaylaElyKuvat.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { experimental_sx as sx, styled } from "@mui/material";
+import { isEvkAktivoitu } from "common/util/isEvkAktivoitu";
 
 export default function VaylaElyKuvat() {
   return (
     <KuvaContainer>
       <Img src="/assets/vayla_alla_fi_sv_rgb.png" alt="Väylävirasto logo" sx={{ maxHeight: "117px" }} />
-      <Img src="/assets/ely_alla_fi_sv_rgb.png" alt="ELY logo" sx={{ maxHeight: "91px" }} />
+      {isEvkAktivoitu() ? (
+        <Img src="/assets/evk_footer_fi_sv.png" alt="Elinvoimakeskus logo" sx={{ maxHeight: "91px" }} />
+      ) : (
+        <Img src="/assets/ely_alla_fi_sv_rgb.png" alt="ELY logo" sx={{ maxHeight: "91px" }} />
+      )}
     </KuvaContainer>
   );
 }
