3dfb62a46ea2993ccd542cb02f21ef108edfc7e5
riikkanaCGI <riikka.naamanka@cgi.com>
2026-01-22 15:25:38 +0200

    lisaa alkuperainenPaatos muokkaustilan validointiin

--- backend/src/projekti/validator/validateMuokkaustilaAllowsInput.ts
+++ backend/src/projekti/validator/validateMuokkaustilaAllowsInput.ts
@@ -21,9 +21,12 @@ export function validateMuokkaustilaAllowsInput(
     throw new IllegalArgumentError("Adminin on avattava uudelleenkuulutus voidaksesi muokata migroitua vaihetta.");
   } else if (muokkausTila === MuokkausTila.AINEISTO_MUOKKAUS) {
     Object.keys(input).forEach((key) => {
-      const allowedInputKeys: (keyof NahtavillaoloVaiheInput | keyof HyvaksymisPaatosVaiheInput)[] = ["aineistoNahtavilla"];
+      const allowedInputKeys: (keyof NahtavillaoloVaiheInput | keyof HyvaksymisPaatosVaiheInput)[] = [
+        "aineistoNahtavilla",
+        "alkuperainenPaatos",
+      ];
       if (!allowedInputKeys.includes(key as keyof NahtavillaoloVaiheInput | keyof HyvaksymisPaatosVaiheInput)) {
-        throw new IllegalArgumentError(`Et voi muokata arvoa ${key}, koka projekti on aineistomuokkaustilassa`);
+        throw new IllegalArgumentError(`Et voi muokata arvoa ${key}, koska projekti on aineistomuokkaustilassa`);
       }
     });
   }
