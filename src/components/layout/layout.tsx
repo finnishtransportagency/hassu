6263c387866f65caa56fb289123267b41f6cad8f
Kai Klemola <kai.klemola@cgi.com>
2026-02-17 14:56:44 +0200

    ymparistomuuttujien debuggausta

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -45,7 +45,7 @@ export default function Layout({ children }: Props): ReactElement {
   ) {
     return (
       <div className="min-h-screen relative flex flex-col">
-        {env !== "prod" && <NotificationBar />}
+        {env === "dev" && <NotificationBar />}
 
         <Container sx={{ marginBottom: "110px", marginTop: "50px" }}>
           <main>{children}</main>
@@ -57,7 +57,7 @@ export default function Layout({ children }: Props): ReactElement {
 
   return (
     <div className="min-h-screen relative flex flex-col">
-      {env !== "prod" && <NotificationBar />}
+      {env === "dev" && <NotificationBar />}
       <Header />
       <div style={{ minWidth: "90%", margin: "10px auto 10px" }}>
         <TiedoteNotification />
