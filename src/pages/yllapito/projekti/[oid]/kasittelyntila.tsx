232adb3082e4f80832c97bea926bfd3ca647dba6
klemokai <kai.klemola@cgi.com>
2026-03-10 12:46:18 +0200

    feat: HASSUYP-508 - Hallinnollisen statuksen asetus automaatiolla Kasittelyn tila sivulla (#1542)

--- src/pages/yllapito/projekti/[oid]/kasittelyntila.tsx
+++ src/pages/yllapito/projekti/[oid]/kasittelyntila.tsx
@@ -286,7 +286,13 @@ function KasittelyntilaPageContent({ projekti, projektiLoadError, reloadProjekti
     [api, reloadProjekti, reset, showTallennaProjektiMessage]
   );
 
-  const onSubmit = useCallback((data: KasittelynTilaFormValues) => withLoadingSpinner(save(data)), [save, withLoadingSpinner]);
+  const onSubmit = useCallback(
+    (data: KasittelynTilaFormValues) => {
+      asetaKasittelynTilaAutomaatiolla(data, defaultValues);
+      return withLoadingSpinner(save(data));
+    },
+    [defaultValues, save, withLoadingSpinner]
+  );
 
   useEffect(() => {
     reset(defaultValues);
@@ -744,3 +750,47 @@ const FormGroupWithLabel = styled(FormGroup)(() => ({
     marginBottom: "0.5rem",
   },
 }));
+
+const asetaKasittelynTilaAutomaatiolla = (data: KasittelynTilaFormValues, defaults: KasittelynTilaFormValues) => {
+  const asianumeroChanged =
+    (data.kasittelynTila?.hyvaksymispaatos?.asianumero ?? null) !== defaults?.kasittelynTila?.hyvaksymispaatos?.asianumero;
+  const hyvaksymisPvmChanged =
+    (data.kasittelynTila?.hyvaksymispaatos?.paatoksenPvm ?? null) !== defaults.kasittelynTila?.hyvaksymispaatos?.paatoksenPvm;
+  if (asianumeroChanged && hyvaksymisPvmChanged) {
+    data.kasittelynTila ??= {};
+    data.kasittelynTila.suunnitelmanTila = "suunnitelman-tila/sutil04";
+  }
+
+  const lainvoimaAlkaenChanged = (data.kasittelynTila?.lainvoimaAlkaen ?? null) !== defaults.kasittelynTila?.lainvoimaAlkaen;
+  const lainvoimaPaatyenChanged = (data.kasittelynTila?.lainvoimaPaattyen ?? null) !== defaults.kasittelynTila?.lainvoimaPaattyen;
+  if (lainvoimaAlkaenChanged && lainvoimaPaatyenChanged) {
+    data.kasittelynTila ??= {};
+    data.kasittelynTila.suunnitelmanTila = "suunnitelman-tila/sutil05";
+  }
+
+  const liikenteeseenluovutusOsittainChanged =
+    (data.kasittelynTila?.liikenteeseenluovutusOsittain ?? null) !== defaults.kasittelynTila?.liikenteeseenluovutusOsittain;
+  if (liikenteeseenluovutusOsittainChanged) {
+    data.kasittelynTila ??= {};
+    data.kasittelynTila.suunnitelmanTila = "suunnitelman-tila/sutil06";
+  }
+
+  const liikenteeseenluovutusKokonaanChanged =
+    (data.kasittelynTila?.liikenteeseenluovutusKokonaan ?? null) !== defaults.kasittelynTila?.liikenteeseenluovutusKokonaan;
+  if (liikenteeseenluovutusKokonaanChanged) {
+    data.kasittelynTila ??= {};
+    data.kasittelynTila.suunnitelmanTila = "suunnitelman-tila/sutil07";
+  }
+
+  const valitustenMaaraChanged = data.kasittelynTila?.valitustenMaara !== null && defaults.kasittelynTila?.valitustenMaara === null;
+  if (valitustenMaaraChanged) {
+    data.kasittelynTila ??= {};
+    data.kasittelynTila.suunnitelmanTila = "suunnitelman-tila/sutil08";
+  }
+
+  const toimitusKaynnistynytChanged = (data.kasittelynTila?.toimitusKaynnistynyt ?? null) !== defaults.kasittelynTila?.toimitusKaynnistynyt;
+  if (toimitusKaynnistynytChanged) {
+    data.kasittelynTila ??= {};
+    data.kasittelynTila.suunnitelmanTila = "suunnitelman-tila/sutil14";
+  }
+};
