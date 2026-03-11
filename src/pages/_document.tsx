7a34dc7aab8ea295a2d389d53a449480128a16c7
Jani Aaltonen <jani.aaltonen@cgi.com>
2026-03-11 14:03:01 +0200

    vaihdetaan env.js latausskripti 'tavalliseksi' synkrooniseksi skriptiksi. Lataus headissa, jolloin on valmis ennen react skriptejä. Ei hydration mismatchia

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { css, Global } from "@emotion/react";
-import Script from "next/script";
 
 const StyledDocument = () => (
   <Html>
     <Head>
       {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-      <Script src="/assets/__env.js" strategy="beforeInteractive" />
+      <script src="/assets/__env.js" />
     </Head>
     <body>
       <Main />
