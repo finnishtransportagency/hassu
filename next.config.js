ae4df571ae1adb85efcc398cdba77c39236cb4bd
Jani Aaltonen <jani.aaltonen@cgi.com>
2026-03-18 10:18:49 +0200

    poista tarpeettomat importit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 const nextTranslate = require("next-translate-plugin");
-const { BaseConfig } = require("./common/BaseConfig");
 const CopyPlugin = require("copy-webpack-plugin");
-const dotenv = require("dotenv");
-const fs = require("fs");
 
 // Middleware poistaa /frontend prefixin ennen kuin redirectit ajetaan
 const lyhytOsoiteRedirects = [
