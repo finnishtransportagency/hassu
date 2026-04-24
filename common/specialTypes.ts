33072a832ef252933e2bcfdfc4a48d30a91efb18
Kai Klemola <kai.klemola@cgi.com>
2026-04-24 13:11:51 +0300

    HASSUYP-750 - Eslint - korjauksia

--- common/specialTypes.ts
+++ common/specialTypes.ts
@@ -1,6 +1,7 @@
+// Contains code generated or recommended by Amazon Q
 export type DeepReadonly<T> = T extends (infer R)[] ? DeepReadonlyArray<R> : T extends object ? DeepReadonlyObject<T> : T;
 
-interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
+type DeepReadonlyArray<T> = ReadonlyArray<DeepReadonly<T>>;
 
 type DeepReadonlyObject<T> = {
   readonly [P in keyof T]: DeepReadonly<T[P]>;
